2baaf245fd2c9471956c4a915f924b3a672edda1
Ned Schwartz <ned@theinterned.net>
2018-07-25 15:01:38 -0400

    Cleans up Ned story

--- sandboxes/src/Ned/Ned.story.js
+++ sandboxes/src/Ned/Ned.story.js
@@ -1,17 +1,15 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-import { Button } from '@storybook/react/demo';
 
 import styles from "./Ned.css";
 
-storiesOf('Button', module)
+storiesOf('Ned', module)
   .add('Type fun times', () => (
     <div className="foo">
       <p>You're rolling</p>
       <p>As you add stuff ... it just works.</p>
     </div>
+  ))
+  .add('Another fun time', () => (
+    <p>Another fun time</p>
   ));
