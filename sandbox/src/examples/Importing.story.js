641e193ce6801f75c90c7dac36c78a73bddd79b6
Ned Schwartz <ned@theinterned.net>
2018-07-25 23:31:42 -0400

    removes unecessary imported const

--- sandbox/src/examples/Importing.story.js
+++ sandbox/src/examples/Importing.story.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from "@nulogy/components";
 
-import styles from "./examples.scss";
+import "./examples.scss";
 
 storiesOf('Examples', module)
   .add('Importing stuff', () => (
