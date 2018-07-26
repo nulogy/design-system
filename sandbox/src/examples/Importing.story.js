e76841a12403b1d3718b53b45ed2d4eea736f4e9
Ned Schwartz <ned@theinterned.net>
2018-07-25 22:44:20 -0400

    Updates examples with sample scss usage.
    I'd like to put this in the `/examples` folder but that's in PR #10

--- sandbox/src/examples/Importing.story.js
+++ sandbox/src/examples/Importing.story.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from "@nulogy/components";
 
-import styles from "./examples.css";
+import styles from "./examples.scss";
 
 storiesOf('Examples', module)
   .add('Importing stuff', () => (
-    <div className="foo">
+    <div className='parent'>
       <p>Some styled text</p>
+      <p className='nested'>Some styled text</p>
       <Button>A @nulogy/components Button</Button>
     </div>
   ));
