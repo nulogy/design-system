29da7c0627179b4f2ab14251dcca521a9d47a7cc
nikola-nulogy <nikolap@nulogy.com>
2018-07-27 15:31:52 -0400

    Corrects incorrect BEM syntax

--- sandbox/src/Nikola/Bgrs.story.js
+++ sandbox/src/Nikola/Bgrs.story.js
@@ -4,44 +4,43 @@ import { storiesOf } from '@storybook/react';
 import styles from "./Bgrs.scss";
 
 storiesOf('Backgrounds', module)
-  .add('Type fun times', () => (
+  .add('V.1', () => (
     <div class="bgrs">
-
       <nav class="nav-bar"></nav>
       <div class="content-wrapper">
         <aside class="sidebar"></aside>
         <main class="content">
-            <ol class="drop-down drop-down__dark">
-              <li class="drop-down--item"><a class="drop-down--link" href="">Item 1</a></li>
-              <li class="drop-down--item"><a class="drop-down--link" href="">Item 1</a></li>
+            <ol class="drop-down drop-down--dark">
+              <li class="drop-down__item"><a class="drop-down__link" href="">Item 1</a></li>
+              <li class="drop-down__item"><a class="drop-down__link" href="">Item 1</a></li>
             </ol>
-            <ol class="drop-down drop-down__light">
-              <li class="drop-down--item"><a class="drop-down--link" href="">Item 1</a></li>
-              <li class="drop-down--item"><a class="drop-down--link" href="">Item 1</a></li>
+            <ol class="drop-down drop-down--light">
+              <li class="drop-down__item"><a class="drop-down__link" href="">Item 1</a></li>
+              <li class="drop-down__item"><a class="drop-down__link" href="">Item 1</a></li>
             </ol>
-            <ol class="drop-down drop-down__blue">
-              <li class="drop-down--item"><a class="drop-down--link" href="">Item 1</a></li>
-              <li class="drop-down--item"><a class="drop-down--link" href="">Item 1</a></li>
+            <ol class="drop-down drop-down--blue">
+              <li class="drop-down__item"><a class="drop-down__link" href="">Item 1</a></li>
+              <li class="drop-down__item"><a class="drop-down__link" href="">Item 1</a></li>
             </ol>
         </main>
       </div>
-      <button class="button button__float"><i class="fas fa-plus"></i></button>
+      <button class="button button--float"><i class="fas fa-plus"></i></button>
       <section class="modal">
-        <div class="modal--box">
-          <h1 class="modal--title"></h1>
-          <div class="modal--content"></div>
+        <div class="modal__box">
+          <h1 class="modal__title"></h1>
+          <div class="modal__content"></div>
           <nav class="action-set">
-            <button class="action-set--button action-set--button__destructive"></button>
-            <button class="action-set--button action-set--button__positive"></button>
+            <button class="action-set__button action-set__button--destructive"></button>
+            <button class="action-set__button action-set__button--positive"></button>
           </nav>
         </div>
       </section>
       <section class="fly-in">
-        <div class="fly-in--type"></div>
-        <div class="fly-in--content"></div>
+        <div class="fly-in__type"></div>
+        <div class="fly-in__content"></div>
         <nav class="action-set">
-          <button class="action-set--button action-set--button__destructive"></button>
-          <button class="action-set--button action-set--button__positive"></button>
+          <button class="action-set__button action-set__button--destructive"></button>
+          <button class="action-set__button action-set__button--positive"></button>
         </nav>
       </section>
     </div>
