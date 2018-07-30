4249fc78f9d7900242d828a8776cfff16f47da85
nikola-nulogy <nikolap@nulogy.com>
2018-07-30 15:55:57 -0400

    Adds Nu7 colour exploration to sandbox

--- sandbox/src/Nikola/Bgrs.story.js
+++ sandbox/src/Nikola/Bgrs.story.js
@@ -47,5 +47,138 @@ storiesOf('Backgrounds', module)
 
   ))
   .add('Another fun time', () => (
-    <p>Another fun time</p>
+    <section class="color-set color-set__nu6">
+      <h1>Nu 7</h1>
+      <p class="notification notification--fail"><span class="notification__title">You are offline</span><span class="notification__description">Do this to fix...</span></p>
+      <div class="wrapper">
+        <aside>
+            <nav>
+              <ol class="nav">
+                <li><a class="nav--item" href="">Link</a></li>
+                <li><a class="nav--item nav--item__active" href="">Active Link</a></li>
+                <li><a class="nav--item" href="">Link</a></li>
+                <li><a class="nav--item" href="">Link</a></li>
+              </ol>
+            </nav>
+        </aside>
+        <div class="main">
+          <div class="block block--button-set">
+            <button class="button button__primary">Primary Button</button>
+            <button class="button button__secondary">Secondary</button>
+            <button class="button button__secondary">Button</button>
+            <button class="button button__secondary__outlined">Secondary Button</button>
+            <button class="button button__disabled">Another Button</button>
+            <button class="button button__tertiary">Tertiary</button>
+          </div>
+          <div class="block block--button-set block--button-set__alt">
+            <button class="button button__primary">Primary Button</button>
+            <button class="button button__secondary">Secondary</button>
+            <button class="button button__secondary">Button</button>
+            <button class="button button__secondary__outlined">Secondary Button</button>
+            <button class="button button__disabled">Another Button</button>
+            <button class="button button__tertiary">Tertiary</button>
+          </div>
+          <div class="block block--button-set block--button-set__pass">
+            <button class="button button__primary">Primary Button</button>
+            <button class="button button__secondary">Secondary</button>
+            <button class="button button__secondary">Button</button>
+            <button class="button button__secondary__outlined">Secondary Button</button>
+            <button class="button button__disabled">Another Button</button>
+            <button class="button button__tertiary">Tertiary</button>
+          </div>
+          <div class="block block--button-set block--button-set__fail">
+            <button class="button button__primary">Primary Button</button>
+            <button class="button button__secondary">Secondary</button>
+            <button class="button button__secondary">Button</button>
+            <button class="button button__secondary__outlined">Secondary Button</button>
+            <button class="button button__disabled">Another Button</button>
+            <button class="button button__tertiary">Tertiary</button>
+          </div>
+
+          <div class="block block--card">
+            <div class="card-img">
+              <i class="fas fa-grin"></i>
+            </div>
+            <div class="card--inset">
+              <h2>Card Title</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare metus sapien, eu aliquet augue egestas eget. Ut nec nunc non risus semper auctor sed in tortor. Proin nec tellus erat. In hac habitasse platea dictumst. In semper magna eu viverra pulvinar. Aenean facilisis lorem consectetur nisl ultricies, quis pharetra ligula tincidunt.</p>
+              <ol class="actions">
+                <li><a class="action-item action-item__positive">Something</a></li>
+                <li><a class="action-item action-item__destructive">Something else</a></li>
+              </ol>
+            </div>
+          </div>
+
+          <div class="block block--notification">
+            <h2>NotificationTitle</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare metus sapien, eu aliquet augue egestas eget. Ut nec nunc non risus semper auctor sed in tortor. Proin nec tellus erat. In hac habitasse platea dictumst. In semper magna eu viverra pulvinar. Aenean facilisis lorem consectetur nisl ultricies, quis pharetra ligula tincidunt.</p>
+          </div>
+
+          <div class="block block--table">
+            <table>
+              <thead>
+                <tr>
+                  <th>khsdhs</th>
+                  <th>khsdhs</th>
+                  <th>khsdhs</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><button class="button button__primary">I am a button</button></td>
+                  <td>khsdhs</td>
+                  <td class="status status__pass"><span class="status--status-indicator"></span>Pass</td>
+                </tr>
+                <tr>
+                  <td><button class="button button__primary">I am a button</button></td>
+                  <td>khsdhs</td>
+                  <td class="status status__fail"><span class="status--status-indicator"></span>Fail</td>
+                </tr>
+                <tr>
+                  <td><button class="button button__primary">I am a button</button></td>
+                  <td>khsdhs</td>
+                  <td class="status status__pass"><span class="status--status-indicator"></span>Pass</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div class="block block--form">
+            <ol>
+              <li>
+                <label>Label 1</label>
+                <input type="text" />
+              </li>
+              <li>
+                <label>Label 2</label>
+                <input type="text" />
+              </li>
+              <li>
+                <label>Label 3</label>
+                <input type="text" />
+              </li>
+              <li class="val-status val-status__error">
+                <label>Label 4</label>
+                <input type="text" />
+                <div class="val">You have entered wrong thing. Try this ...</div>
+              </li>
+            </ol>
+          </div>
+          <div class="block block--tinies">
+            <span class="pill pill__solid__pass">Pass</span>
+            <span class="pill pill__solid__fail">Fail</span>
+            <span class="pill pill__inverted__pass">Open</span>
+            <span class="pill pill__inverted__fail">Closed</span>
+          </div>
+        </div>
+      </div>
+      <section class="fly-in">
+        <div class="fly-in__type"></div>
+        <div class="fly-in__content">Some text</div>
+        <nav class="action-set">
+          <button class="action-set__button action-set__button--destructive"></button>
+          <button class="action-set__button action-set__button--positive"></button>
+        </nav>
+      </section>
+    </section>
   ));
