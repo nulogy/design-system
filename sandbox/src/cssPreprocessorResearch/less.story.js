c9f5112283cd040ba56bc543c609cfd09a17c261
Ned Schwartz <ned@theinterned.net>
2018-08-02 19:11:43 -0400

    makes it clear whcih sotry is less and which is sass

--- sandbox/src/cssPreprocessorResearch/less.story.js
+++ sandbox/src/cssPreprocessorResearch/less.story.js
@@ -5,7 +5,7 @@ import './less.less';
 storiesOf('CSS preprocessors', module)
   .add('Less', () => (
     <div className="less">
-      <p className="local_vars">This text is styled with local vars</p>
+      <p className="local_vars">This text is styled with local JS vars using Less.</p>
 
       <p>This solution uses <a href="https://github.com/tompascall/js-to-styles-var-loader">js-to-styles-var-loader</a> to enable loading of JS variables into less.</p>
       
--- sandbox/src/cssPreprocessorResearch/sass.story.js
+++ sandbox/src/cssPreprocessorResearch/sass.story.js
@@ -5,7 +5,7 @@ import './sass.scss';
 storiesOf('CSS preprocessors', module)
   .add('Sass', () => (
     <div className="sass">
-      <p className="local_vars">This text is styled with local vars</p>
+      <p className="local_vars">This text is styled with local JS vars using Sass.</p>
 
       <p>This solution uses <a href="https://github.com/tompascall/js-to-styles-var-loader">js-to-styles-var-loader</a> to enable loading of JS variables into sass.</p>
       
