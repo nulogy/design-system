4ed69a21e6b2266007e2477ae15bbdfeaf226205
nikola-nulogy <nikolap@nulogy.com>
2018-08-03 17:31:03 -0400

    Extracts common map row

--- docs/catalog/routes.js
+++ docs/catalog/routes.js
@@ -30,33 +30,25 @@ const toRGB = hex => {
   return rgb.join(', ')
 }
 
+const mapRow = ([colourName, hex]) => ({
+  Token: `colour.${colourName}`,
+  Alias: null,
+  HEX: hex,
+  RGB: toRGB(hex)
+})
+
 const baseRows = Object.entries(colour)
   .filter(([_, value]) => typeof value === 'string')
-  .map(([colourName, hex]) => ({
-    Token: `colour.${colourName}`,
-    Alias: null,
-    HEX: hex,
-    RGB: toRGB(hex)
-  }));
-
+  .map(mapRow);
 
 const scaleRows = Object.entries(colour)
   .filter(([_, value]) => typeof value !== 'string')
   .reduce((result, [scaleName, scale]) => ([
     ...result,
-    ...Object.entries(scale).map(([colourName, hex]) => ({
-      Token: 'colour.neutral.100',
-      Alias: null,
-      HEX: colour.neutral['100'],
-      RGB: '255, 255, 255'
-    }))
+    ...Object.entries(scale).map(mapRow)
   ]), [])
 
 const colourTokenTableRows = [ ...baseRows, ...scaleRows]
-
-
-
-
 
 const colourImports = {
   neutral: mapColours('neutral', { 100: '(white)', 900: '(black)'}),
