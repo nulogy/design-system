e31f89e79d439a8a0bd2ebb511493d7793e098cb
nikola-nulogy <nikolap@nulogy.com>
2018-08-13 15:54:41 -0400

    Cleans up button shared styles

--- components/src/Button/sharedStyles.js
+++ components/src/Button/sharedStyles.js
@@ -3,21 +3,34 @@ import styled from 'styled-components';
 import { css } from 'styled-components';
 import { borderRadius, colour, space, font, corner, shadow } from '@nulogy/tokens';
 
+
 const sharedStyles = css`
+  box-sizing: border-box;
   border: none;
   outline: none;
+  cursor: pointer;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  font-size: ${font.size.small}px;
-  font-weight: ${font.weight.bold};
-  padding: ${space.x1};
-  border-radius: ${borderRadius};
-  transition: .2s;
-  cursor: pointer;
-  position: relative; top: 0px; left: 0px; // for animation
+
+  font-size: ${font.size.medium}px;
+  font-weight: ${font.weight.medium};
+  padding: ${space.x2} ${space.x3};
+  border-radius: ${corner.small};
+  transition: .1s  ease-in-out;
+
+  &:hover{
+    transform: scale(1.02);
+    box-shadow: ${shadow.close};
+  }
+
+  &:focus{
+    transform: scale(1);
+    box-shadow: ${shadow.pressed};
+  }
+
   &:active {
-    top: 1px;
-    left: 1px;
+    transform: scale(1);
+    box-shadow: ${shadow.pressed};
   }
 `
 
