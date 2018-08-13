9bc91b169a8e43f40bd0e036ce2a213053b921c8
nikola-nulogy <nikolap@nulogy.com>
2018-08-13 15:55:38 -0400

    Clears Default button styles

--- components/src/Button/Button.js
+++ components/src/Button/Button.js
@@ -2,17 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import { borderRadius, colour, space, font, corner, shadow } from '@nulogy/tokens';
 import sharedStyles from './sharedStyles';
+import textCrop from './textCrop';
 
 const Button = styled.button`
   ${sharedStyles}
-  background-color: ${colour.neutral.x400};
-  color: ${colour.neutral.x800};
-  &:hover, &:focus {
-    box-shadow: 0 5px 15px rgba(0, 0, 0, .2);
-    background-color: ${colour.neutral.x500};
-    color: ${colour.black};
+
+  background-color: ${colour.neutral.x300};
+  color: ${colour.neutral.x700};
+
+  &:hover{
+    background-color: ${colour.neutral.x400};
+    color: ${colour.neutral.x800};
   }
 `;
 
 export default Button;
-
