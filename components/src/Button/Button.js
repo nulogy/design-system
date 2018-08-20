c423fd87b4192d1c5b8ad567c80b77748c8e03cb
nikola-nulogy <nikolap@nulogy.com>
2018-08-20 17:29:27 -0400

    Moves shared styles into button

--- components/src/Button/Button.js
+++ components/src/Button/Button.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { colour, space, font } from '@nulogy/tokens';
-import sharedStyles from './sharedStyles';
+import { colour, space, font, corner, shadow } from '@nulogy/tokens';
 
 const type = ({ type = 'button' }) => (({
   button: css`
@@ -17,36 +16,60 @@ const type = ({ type = 'button' }) => (({
   submit: css`
     background-color: ${colour.blue.base};
     color: ${colour.white};
-    
+    border-color: ${colour.blue.base};
+
     &:hover{
       background-color: ${colour.blue.x700};
     }
   `
 })[type]);
 
-const size = ({ size }) => (({
+const size = ({ size = 'medium' }) => (({
   large: css`
     font-size: ${font.size.large}px;
     font-weight: ${font.weight.medium};
-    padding: ${space.x1} ${space.x2};
+  `,
+
+  medium: css`
+    font-size: ${font.size.small}px;
+    font-weight: ${font.weight.medium};
   `,
 
   small: css`
     font-size: ${font.size.smaller}px;
     font-weight: ${font.weight.normal};
-    padding: ${space.x1} ${space.x2};
   `
 })[size]);
 
 const Button = styled.button`
-  ${sharedStyles}
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.3;
+  padding: ${space.x1} ${space.x2};
+  border-radius: ${corner.small};
+  transition: .1s  ease-in-out;
+
   ${ size }
   ${ type }
+
+  &:hover{
+    box-shadow: ${shadow.close};
+  }
+
+  &:active {
+    box-shadow: none;
+    outline: none; // test this
+    transform: scale(0.98);
+    transition: .05s  ease-in;
+  }
 
   &:disabled {
     background-color: ${colour.neutral.x300};
     color: ${colour.neutral.x500};
     pointer-events: none;
+    border-color: ${colour.neutral.x300};
 
     &:hover{
       box-shadow: none;
--- components/src/Button/sharedStyles.js
+++ components/src/Button/sharedStyles.js
@@ -8,30 +8,10 @@ const sharedStyles = css`
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 
-  border: none;
-  cursor: pointer;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
 
-  font-size: ${font.size.small}px;
-  font-weight: ${font.weight.medium};
-  line-height: 1.3; // Adjust when introduce cropTool
-  padding: ${space.x1} ${space.x2};
-  border-radius: ${corner.small};
-  transition: .1s  ease-in-out;
 
-  &:hover{
-    box-shadow: ${shadow.close};
-  }
 
-  &:active {
-    box-shadow: none;
-    outline: none; // test this
-    transform: scale(0.98);
-    transition: .05s  ease-in;
-  }
 `
 
 export default sharedStyles;
