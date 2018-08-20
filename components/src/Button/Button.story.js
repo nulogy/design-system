ef9c04cbd2afa74d615630b3e1b75819fa3c7fd0
Ned Schwartz <ned@theinterned.net>
2018-08-20 13:09:53 -0400

    Removes unused Button types

--- components/src/Button/Button.story.js
+++ components/src/Button/Button.story.js
@@ -7,26 +7,16 @@ import PrimaryButton from "./PrimaryButton";
 import ApprovalButton from "./ApprovalButton";
 import DangerButton from "./DangerButton";
 import DisabledButton from "./DisabledButton";
-import LinkButton from "./LinkButton";
-import CreateButton from "./CreateButton";
-import ActionButton from "./ActionButton";
-import TriggerButton from "./TriggerButton";
-import GhostButton from "./GhostButton";
 import SmallButton from "./SmallButton";
 import LargeButton from "./LargeButton";
-import FullWidthButton from "./FullWidthButton";
 
 storiesOf('Button', module)
   .add('Default', () => <Button onClick={action('clicked')}>Default Button</Button>)
   .add('Primary', () => <PrimaryButton onClick={action('clicked')}>Primary Button</PrimaryButton>)
+  .add('With an icon', () => <Button onClick={action('clicked')}>➕</Button>)
+  .add('With an icon and text', () => <Button onClick={action('clicked')}>✏️ Edit this thing</Button>)
   .add('Approval', () => <ApprovalButton onClick={action('clicked')}>Approval Button</ApprovalButton>)
   .add('Danger', () => <DangerButton onClick={action('clicked')}>Danger Button</DangerButton>)
   .add('Disabled', () => <DisabledButton disabled onClick={action('clicked')}>Disabled Button</DisabledButton>)
-  .add('Link', () => <LinkButton onClick={action('clicked')}>Link Button</LinkButton>)
-  .add('Create', () => <CreateButton onClick={action('clicked')}>+</CreateButton>)
-  .add('Action', () => <ActionButton onClick={action('clicked')}>+</ActionButton>)
-  .add('Trigger', () => <TriggerButton onClick={action('clicked')}>Trigger Button</TriggerButton>)
-  .add('Ghost', () => <GhostButton onClick={action('clicked')}>Ghost Button</GhostButton>)
   .add('Small', () => <SmallButton onClick={action('clicked')}>Small Button</SmallButton>)
   .add('Large', () => <LargeButton onClick={action('clicked')}>Large Button</LargeButton>)
-  .add('FullWidth', () => <FullWidthButton onClick={action('clicked')}>Full Width Button</FullWidthButton>);
