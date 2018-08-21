07ebd81aa6e657fbb311cf139a10d9b7c8ec6a62
nikola-nulogy <nikolap@nulogy.com>
2018-08-21 16:07:08 -0400

    Fixes Aproval Buttons pseudo states

--- components/src/Button/ApprovalButton.js
+++ components/src/Button/ApprovalButton.js
@@ -4,18 +4,18 @@ import { borderRadius, colour, space, font, radius, shadow } from '@nulogy/token
 import Button from './Button';
 
 const ApprovalButton = styled(Button)`
-  --colour-bg: ${colour.green.base};
-  background-color: var(--colour-bg);
-  border-color: var(--colour-bg);
+  background-color: ${colour.green.base};
+  border-color: ${colour.green.base};
   color: ${colour.white};
 
   &:hover{
-    --colour-bg: ${colour.green['700']};
+    background-color: ${colour.green['700']};
+    border-color: ${colour.green['700']};
     color:  ${colour.white};
   }
 
   &:active{
-    --colour-bg: ${colour.green['700']};
+    background-color: ${colour.green.base};
   }
 `
 
