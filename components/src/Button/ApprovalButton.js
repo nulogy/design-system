e8ed6a6063737946c4a3134411a530aaaad5e5c7
nikola-nulogy <nikolap@nulogy.com>
2018-08-21 15:37:02 -0400

    Removes shared styles from Buttons

--- components/src/Button/ApprovalButton.js
+++ components/src/Button/ApprovalButton.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import sharedStyles from './sharedStyles';
 import { borderRadius, colour, space, font, radius, shadow } from '@nulogy/tokens';
 
 const ApprovalButton = styled.button`
-  ${sharedStyles}
 
   background-color: ${colour.green.base};
   color: ${colour.white};
--- components/src/Button/DangerButton.js
+++ components/src/Button/DangerButton.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import sharedStyles from './sharedStyles';
 import { borderRadius, colour, space, font, radius, shadow } from '@nulogy/tokens';
 
 const DangerButton = styled.button`
-  ${sharedStyles}
 
   background-color: ${colour.red.base};
   color: ${colour.white};
