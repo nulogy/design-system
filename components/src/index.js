0244f47cd0453608c543c07f07ecfe0e2ca07589
Ned Schwartz <ned@theinterned.net>
2018-08-24 18:52:00 -0400

    Simplifies componets/index

--- components/src/index.js
+++ components/src/index.js
@@ -1,4 +1,1 @@
-export { default as ApprovalButton } from "./Button/ApprovalButton";
-export { default as Button } from "./Button/Button";
-export { default as DangerButton } from "./Button/DangerButton";
-export { default as LinkButton } from "./Button/LinkButton";
+export * from "./Button";
