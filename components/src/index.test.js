3d6b423383edf576d63cbc295a827f6f692e9ca5
Ned Schwartz <ned@theinterned.net>
2018-08-27 15:39:16 -0400

    adds test for components main exports

--- components/src/index.test.js
+++ components/src/index.test.js
@@ -1,3 +1,35 @@
-it('runs tests', () => {
-  expect(true).toBeTruthy();
+import {
+  Button,
+  ApprovalButton,
+  DangerButton,
+  LinkButton,
+} from './';
+
+describe('Button', () => {
+  it('matches snapshot', () => {
+    expect(Button).toBeDefined();
+    expect(Button).toMatchSnapshot();
+  });
 });
+
+describe('ApprovalButton', () => {
+  it('matches snapshot', () => {
+    expect(ApprovalButton).toBeDefined();
+    expect(ApprovalButton).toMatchSnapshot();
+  });
+});
+
+describe('DangerButton', () => {
+  it('matches snapshot', () => {
+    expect(DangerButton).toBeDefined();
+    expect(DangerButton).toMatchSnapshot();
+  });
+});
+
+describe('LinkButton', () => {
+  it('matches snapshot', () => {
+    expect(LinkButton).toBeDefined();
+    expect(LinkButton).toMatchSnapshot();
+  });
+});
+  
