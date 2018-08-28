180e67c4d410f3065c81940808eb40aebe65e316
Ned Schwartz <ned@theinterned.net>
2018-08-28 12:57:39 -0400

    NDS-225: removes default export from Table and adds a bit of styling

--- components/src/Tables/Table.js
+++ components/src/Tables/Table.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Table = styled.table`
-  border: 1px solid;
+const borderColour = 'lightgrey';
+
+
+export const Cell = styled.td`
+  border-bottom: 1px solid ${borderColour};
+  padding: 1rem;
 `;
 
 export const Row = styled.tr``;
 
-export const Cell = styled.td`
-  border-bottom: 1px solid;
+export const Table = styled.table`
+  border-collapse: collapse;
+  border-top: 1px solid ${borderColour};
 `;
-
-export default Table;
