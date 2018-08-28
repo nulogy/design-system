357063e3e2e9052a6705dab5486d4f2722815d98
Ned Schwartz <ned@theinterned.net>
2018-08-28 12:50:50 -0400

    NDS-225: adds abasic Table, Row and Cell

---
+++ components/src/Tables/Table.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Table = styled.table`
+  border: 1px solid;
+`;
+
+export const Row = styled.tr``;
+
+export const Cell = styled.td`
+  border-bottom: 1px solid;
+`;
+
+export default Table;
---
+++ components/src/Tables/Table.story.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Table, { Row, Cell } from './Table';
+
+storiesOf('Table', module)
+  .add('default', () => (
+    <Table>
+      <Row>
+        <Cell>Row 1, Cell 1</Cell>
+        <Cell>Row 1, Cell 2</Cell>
+        <Cell>Row 1, Cell 3</Cell>
+      </Row>
+      <Row>
+        <Cell>Row 2, Cell 1</Cell>
+        <Cell>Row 2, Cell 2</Cell>
+        <Cell>Row 2, Cell 3</Cell>
+      </Row>
+      <Row>
+        <Cell>Row 3, Cell 1</Cell>
+        <Cell>Row 3, Cell 2</Cell>
+        <Cell>Row 3, Cell 3</Cell>
+      </Row>
+    </Table>
+  ));
