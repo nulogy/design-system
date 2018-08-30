16f955415d59417a7231b658e0224f4bc35930b8
nikola-nulogy <nikolap@nulogy.com>
2018-08-30 11:41:43 -0400

    Reorganizes Table stroy to include read only and edit mode tables

--- components/src/Tables/Table.story.js
+++ components/src/Tables/Table.story.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Table, Row, Cell, Body, Header, HeaderCell } from './';
+import { Table, Row, Cell, ActionCell, Body, Header, HeaderCell } from './';
+import LinkButton from ".././Button/LinkButton";
 
-storiesOf('Table', module)
-  .add('default', () => (
+storiesOf('Table / Inventory Statuses Table', module)
+  .add('Read Only', () => (
     <Table>
       <Header>
         <Row>
-          <HeaderCell>Header Row 1, Header Cell 1</HeaderCell>
-          <HeaderCell>Header Row 1, Header Cell 2</HeaderCell>
-          <HeaderCell>Header Row 1, Header Cell 3</HeaderCell>
+          <HeaderCell>Name</HeaderCell>
+          <HeaderCell>Key</HeaderCell>
+          <HeaderCell>Integration Key</HeaderCell>
         </Row>
       </Header>
       <Body>
@@ -27,6 +28,38 @@ storiesOf('Table', module)
           <Cell>Row 3, Cell 1</Cell>
           <Cell>Row 3, Cell 2</Cell>
           <Cell>Row 3, Cell 3</Cell>
+        </Row>
+      </Body>
+    </Table>
+  ))
+  .add('Edit Mode', () => (
+    <Table>
+      <Header>
+        <Row>
+          <HeaderCell>Name</HeaderCell>
+          <HeaderCell>Key</HeaderCell>
+          <HeaderCell>Integration Key</HeaderCell>
+          <HeaderCell></HeaderCell>
+        </Row>
+      </Header>
+      <Body>
+        <Row>
+          <Cell>Row 1, Cell 1</Cell>
+          <Cell>Row 1, Cell 2</Cell>
+          <Cell>Row 1, Cell 3</Cell>
+          <ActionCell><LinkButton>+</LinkButton></ActionCell>
+        </Row>
+        <Row>
+          <Cell>Row 2, Cell 1</Cell>
+          <Cell>Row 2, Cell 2</Cell>
+          <Cell>Row 2, Cell 3</Cell>
+          <ActionCell></ActionCell>
+        </Row>
+        <Row>
+          <Cell>Row 3, Cell 1</Cell>
+          <Cell>Row 3, Cell 2</Cell>
+          <Cell>Row 3, Cell 3</Cell>
+          <ActionCell></ActionCell>
         </Row>
       </Body>
     </Table>
