2724586429a1ad51515b9dc2b22d389a104850bf
nikola-nulogy <nikolap@nulogy.com>
2018-09-07 12:31:06 -0400

    Makes table themable

--- components/src/Tables/Table.js
+++ components/src/Tables/Table.js
@@ -1,87 +1,122 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import tokens from '@nulogy/tokens';
+import QuietButton from '../Button/QuietButton';
 
 const defaultProps = { theme: tokens };
 
-const lineHeight = name => font.lineHeight[name] * font.size.medium;
+const lineHeight = theme => name => theme.font.lineHeight[name] * theme.font.size.medium;
+
+const defaultProps = {
+  theme: tokens
+}
 
 export const Cell = styled.td`
-  font-size: ${font.size.small}px;
-  line-height: ${lineHeight('smaller') / font.size.small};
-  padding: ${space.x2}px ${space.x1}px;
-  &:first-child{
-    padding-left: ${space.x2}px;
-  }
-  &:last-child{
-    padding-right: ${space.x2}px;
-    width:72px; // not the right place
-  }
+  ${ ({ theme }) => css`
+    font-size: ${theme.font.size.small}px;
+    line-height: ${lineHeight(theme)('smaller') / theme.font.size.small};
+    padding: ${theme.space.x2}px ${theme.space.x1}px;
+    &:first-child{
+      padding-left: ${theme.space.x2}px;
+    }
+    &:last-child{
+      padding-right: ${theme.space.x2}px;
+      width:72px; // not the right place
+    }
+  `}
 `;
 
 Cell.defaultProps = defaultProps;
 
 export const ActionCell = styled(Cell)`
-  padding: ${space.x1}px ${space.x2}px ${space.x1}px ${space.x1}px;
-  display: flex;
-  justify-content: flex-end;
-  & > button:not(:last-child) {
-    margin-right: ${space.half}px;
-  }
+  ${ ({ theme }) => css`
+    padding: ${theme.space.x1}px ${theme.space.x2}px ${theme.space.x1}px ${theme.space.x1}px;
+    display: flex;
+    justify-content: flex-end;
+    & > button:not(:last-child) {
+      margin-right: ${theme.space.half}px;
+    }
+  `}
 `;
+
+ActionCell.defaultProps = defaultProps;
 
 export const Button = styled(QuietButton)`
-  padding: ${space.half}px;
-  border-width: 0;
-  border-radius: 50%;
-  transition: none;
-  &:hover{
-    background-color: ${colour.blue[300]};
-    transform: scale(1.25);
-    svg{
-      transform: scale(0.8);
+  ${ ({ theme }) => css`
+    padding: ${theme.space.half}px;
+    border-width: 0;
+    border-radius: 50%;
+    transition: none;
+    &:hover{
+      background-color: ${theme.colour.blue[300]};
+      transform: scale(1.25);
+      svg{
+        transform: scale(0.8);
+      }
     }
-  }
+  `}
 `;
 
+Button.defaultProps = defaultProps;
+
 export const Row = styled.tr`
-  &:nth-child(odd){
-    background-color: ${colour.blue['200']};
-  }
+  ${ ({ theme }) => css`
+    &:nth-child(odd){
+      background-color: ${theme.colour.blue['200']};
+    }
+  `}
 `;
+
+Row.defaultProps = defaultProps;
 
 export const Header = styled.thead`
   text-align: left;
 `;
 
 export const HeaderCell = styled(Cell.withComponent('th'))`
-  font-weight: bold;
-  background-color: ${colour.white}; // sloppy
+  ${ ({ theme }) => css`
+    font-weight: bold;
+    background-color: ${theme.colour.white}; // sloppy
+  `}
 `;
+
+HeaderCell.defaultProps = defaultProps;
 
 export const CreateRow = styled.tr`
-  background-color: ${colour.blue[800]};
+  ${ ({ theme }) => css`
+    background-color: ${theme.colour.blue[800]};
+  `}
 `;
+
+CreateRow.defaultProps - defaultProps;
 
 export const InputCell = styled(HeaderCell.withComponent('th'))`
-  font-weight: ${font.weight.normal};
-  padding: ${space.x1}px;
-  position: relative;
+  ${  ({ theme }) => css`
+    font-weight: ${theme.font.weight.normal};
+    padding: ${theme.space.x1}px;
+    position: relative;
+  `}
 `;
 
+InputCell.defaultProps = defaultProps;
+
 export const TextInput = styled.input`
-  box-sizing: border-box;
-  height:32px; // Temporary
-  width:100%;
-  border: none;
-  background: ${colour.blue[300]};
-  font-size: ${font.size.small}px;
-  padding: ${space.half}px ${space.x1}px;
-  border-radius: ${radius.small}px;
-  &:focus{
-    background: ${colour.white};
-  }
+  ${ ({ theme }) => css`
+    box-sizing: border-box;
+    height:32px; // Temporary
+    width:100%;
+    border: none;
+    background: ${theme.colour.blue[300]};
+    font-size: ${theme.font.size.small}px;
+    padding: ${theme.space.half}px ${theme.space.x1}px;
+    border-radius: ${theme.radius.small}px;
+    &:focus{
+      background: ${theme.colour.white};
+    }
+  `}
 `;
+
+TextInput.defaultProps = defaultProps;
 
 export const Body = styled.tbody``;
 
