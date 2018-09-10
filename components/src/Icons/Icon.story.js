299def3f60fdbfbb9ccf1b791aa3a73dc0048e45
Ned Schwartz <ned@theinterned.net>
2018-09-10 18:04:59 -0400

    NDS-196: Adds storybook test cases from GO

--- components/src/Icons/Icon.story.js
+++ components/src/Icons/Icon.story.js
@@ -2,11 +2,54 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Icon, { names } from './Icon';
 
-storiesOf('Icon', module)
-  .add('Default', () => (
+const IconCode = ({ name }) => (
+  <code>
+    &lt;Icon name="<b>{name}</b>" /&gt;
+  </code>
+);
+
+storiesOf("Icon", module)
+  .add("Default icons", () => (
     <React.Fragment>
-      { names.map(iconName => 
-        <Icon name={iconName} key={iconName}/>
-      )}
+      {names.map(iconName => (
+        <p key={iconName}>
+          <Icon name={iconName} />
+          <IconCode name={iconName} />
+        </p>
+      ))}
     </React.Fragment>
-  ));
+  ))
+  .add("Size tracks surrounding text", () => (
+    <React.Fragment>
+      {[1, 2, 3, 4].map(size => (
+        <p style={{ fontSize: `${size}em` }} key={size}>
+          @{size}em: &nbsp;
+          {names.map(iconName => <Icon name={iconName} key={iconName} />)}
+        </p>
+      ))}
+    </React.Fragment>
+  ))
+  .add("Inherits parent colours", () => (
+    <React.Fragment>
+      {["red", "orange", "yellow", "green", "blue", "purple"].map(color => (
+        <div style={{ color }} key={color}>
+        {names.map(iconName => <Icon name={iconName} key={iconName} />)}
+        </div>
+      ))}
+    </React.Fragment>
+  ))
+  // .add("Accepts a className", () => (
+  //   <Icon name="late" className="storybookTestClass" />
+  // ))
+  // .add("Just the svg files", () => (
+  //   <React.Fragment>
+  //     {Object.keys(Icons).map(key => {
+  //       const IconValue = Icons[key];
+  //       return (
+  //         <p key={key}>
+  //           <IconValue /> <code>{key}.icon.svg</code>
+  //         </p>
+  //       );
+  //     })}
+  //   </React.Fragment>
+  // ));
