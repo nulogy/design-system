fb539a7893e8afc5b6cd9cab58fb1723eb89d5d1
nikola-nulogy <nikolap@nulogy.com>
2018-09-25 10:09:15 -0400

    Replaces Icons svgs with Icon components

--- components/src/Tables/Table.story.js
+++ components/src/Tables/Table.story.js
@@ -77,16 +77,10 @@ storiesOf('Table/primitives', module)
           <Cell>1</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
@@ -96,16 +90,10 @@ storiesOf('Table/primitives', module)
           <Cell>2</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
@@ -115,16 +103,10 @@ storiesOf('Table/primitives', module)
           <Cell>3</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
@@ -146,10 +128,7 @@ storiesOf('Table/primitives', module)
           <Cell><TemporaryTextInput></TemporaryTextInput></Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z" fill="#EBEBEB"/>
-              </svg>
+              <Icon name="save" />
             </Button>
           </ActionCell>
         </CreateRow>
@@ -161,16 +140,10 @@ storiesOf('Table/primitives', module)
           <Cell>1</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
@@ -180,16 +153,10 @@ storiesOf('Table/primitives', module)
           <Cell>2</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
@@ -199,16 +166,10 @@ storiesOf('Table/primitives', module)
           <Cell>3</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
@@ -241,16 +202,10 @@ storiesOf('Table/primitives', module)
           <Cell>Row 1, Cell 3</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
@@ -260,16 +215,10 @@ storiesOf('Table/primitives', module)
           <Cell>Row 2, Cell 3</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
@@ -279,16 +228,10 @@ storiesOf('Table/primitives', module)
           <Cell>Row 3, Cell 3</Cell>
           <ActionCell>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="edit" />
             </Button>
             <Button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="#607180"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-              </svg>
+              <Icon name="delete" />
             </Button>
           </ActionCell>
         </Row>
