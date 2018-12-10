2a545bdc1cdb8ba40005818559606e7d50b1045a
Matt Dunn <mattd@nulogy.com>
2018-12-10 09:06:05 -0500

    adds storybook addons and styled-system

--- components/.storybook/config.js
+++ components/.storybook/config.js
@@ -1,7 +1,18 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { configure, addDecorator } from '@storybook/react';
+import theme from '../src/theme';
+
+import '../../css/src/nds-dev.css';
 
 function loadStories() {
   require('../src/Link/Link.story.js');
 }
+
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    {story()}
+  </ThemeProvider>
+))
 
 configure(loadStories, module);
