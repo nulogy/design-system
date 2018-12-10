da4c4cf051888fafb43b4ba5683e8489c1f9c86e
Matt Dunn <mattd@nulogy.com>
2018-12-10 14:51:12 -0500

    removes tokens build folder from gitignore

---
+++ tokens/build/exports.js
@@ -0,0 +1,50 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 06 Dec 2018 14:48:11 GMT
+ */
+
+export const color_base_black = "#011e38";
+export const color_base_black_blue = "#122b47";
+export const color_base_dark_blue = "#00438f";
+export const color_base_blue = "#216beb";
+export const color_base_light_blue = "#e1ebfa";
+export const color_base_dark_grey = "#434d59";
+export const color_base_grey = "#c0c8d1";
+export const color_base_light_grey = "#e4e7eb";
+export const color_base_white_grey = "#f0f2f5";
+export const color_base_white = "#ffffff";
+export const color_base_yellow = "#ffbb00";
+export const color_base_green = "#008059";
+export const color_base_light_green = "#e6faf0";
+export const color_base_red = "#cc1439";
+export const color_base_light_red = "#faebec";
+export const font_family_base = "'IBM Plex Sans', sans";
+export const font_family_mono = "'IBM Plex Mono', monospace";
+export const radius_background_small = "2px";
+export const radius_background_medium = "4px";
+export const radius_background_large = "6px";
+export const shadow_box_pressed = "0 1px 4px 0 rgba(3,16,26, 0.175)";
+export const shadow_box_close = "0 2px 12px 0 rgba(3,16,26, 0.2)";
+export const shadow_box_middle = "0 3px 12px 0 rgba(3,16,26, 0.25)";
+export const shadow_box_far = "0 6px 15px 0 rgba(3,16,26, 0.33)";
+export const size_font_smaller = "12px";
+export const size_font_small = "14px";
+export const size_font_medium = "16px";
+export const size_font_large = "18px";
+export const size_font_larger = "26px";
+export const size_font_largest = "36px";
+export const size_font_base = "16px";
+export const size_font_page_title = "36px";
+export const size_font_section_title = "26px";
+export const size_font_subsection_title = "18px";
+export const size_padding_half = "4px";
+export const size_padding_x_1 = "8px";
+export const size_padding_x_2 = "16px";
+export const size_padding_x_3 = "24px";
+export const size_padding_x_4 = "32px";
+export const size_padding_x_6 = "48px";
+export const size_padding_x_8 = "64px";
+export const size_padding_base = "16px";
+export const weight_font_normal = "400";
+export const weight_font_medium = "500";
+export const weight_font_bold = "700";
