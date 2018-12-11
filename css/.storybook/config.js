f703ff075e0265ef28e4fe67884af940abc3ae69
Matt Dunn <mattd@nulogy.com>
2018-12-11 15:58:32 -0500

    adds button

--- css/.storybook/config.js
+++ css/.storybook/config.js
@@ -1,8 +1,8 @@
 import { configure } from '@storybook/html';
 import '../src/nds-dev.css';
 
-
 function loadStories() {
+  require('../src/scss/components/_buttons.story.js');
   require('../src/scss/components/_type.story.js');
   require('../src/scss/utilities/_type.story.js');
 }
