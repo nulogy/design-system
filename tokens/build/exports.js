3dfa3190db6f7094f240270597658bdac86119f8
Matt Dunn <mattd@nulogy.com>
2018-12-12 11:45:23 -0500

    updates tokens

--- tokens/build/exports.js
+++ tokens/build/exports.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Thu, 06 Dec 2018 14:48:11 GMT
+ * Generated on Wed, 12 Dec 2018 15:58:24 GMT
  */
 
 export const color_base_black = "#011e38";
@@ -13,20 +13,16 @@ export const color_base_grey = "#c0c8d1";
 export const color_base_light_grey = "#e4e7eb";
 export const color_base_white_grey = "#f0f2f5";
 export const color_base_white = "#ffffff";
-export const color_base_yellow = "#ffbb00";
 export const color_base_green = "#008059";
 export const color_base_light_green = "#e6faf0";
 export const color_base_red = "#cc1439";
 export const color_base_light_red = "#faebec";
+export const color_base_yellow = "#ffbb00";
 export const font_family_base = "'IBM Plex Sans', sans";
 export const font_family_mono = "'IBM Plex Mono', monospace";
 export const radius_background_small = "2px";
 export const radius_background_medium = "4px";
-export const radius_background_large = "6px";
-export const shadow_box_pressed = "0 1px 4px 0 rgba(3,16,26, 0.175)";
-export const shadow_box_close = "0 2px 12px 0 rgba(3,16,26, 0.2)";
-export const shadow_box_middle = "0 3px 12px 0 rgba(3,16,26, 0.25)";
-export const shadow_box_far = "0 6px 15px 0 rgba(3,16,26, 0.33)";
+export const shadow_box_small = "0 1px 1px rgba(27,31,35,0.1)";
 export const size_font_smaller = "12px";
 export const size_font_small = "14px";
 export const size_font_medium = "16px";
