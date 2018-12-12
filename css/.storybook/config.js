33964ce0449d573467afa283f89d1355a673b38c
Matt Dunn <mattd@nulogy.com>
2018-12-12 15:36:58 -0500

    makes css stories load automatically

--- css/.storybook/config.js
+++ css/.storybook/config.js
@@ -1,22 +1,10 @@
 import { configure } from '@storybook/html';
 import '../src/nds-dev.css';
 
-function loadStories() {
-  require('../src/scss/components/_buttons.story.js');
-  require('../src/scss/components/_type.story.js');
-  require('../src/scss/utilities/_borders.story.js');
-  require('../src/scss/utilities/_colors.story.js');
-  require('../src/scss/utilities/_layout.story.js');
-  require('../src/scss/utilities/_shadows.story.js');
-  require('../src/scss/utilities/_space.story.js');
-  require('../src/scss/utilities/_type.story.js');
-}
-
 configure(loadStories, module);
 
-// maybe this better? it automatic.
-// configureStorybook(require.context(
-//     "../src",       // path where stories live
-//     true,           // recursive?
-//     /\.story.js$/, // story files match this pattern
-//   ));
+configureStorybook(require.context(
+    "../src/scss/",  // path where stories live
+    true,           // recursive?
+    /\.story.js$/, // story files match this pattern
+  ));
