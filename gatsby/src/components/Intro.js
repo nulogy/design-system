5eacbe35cdc32c18c9d5578275b14fec6a8acf75
Matt Dunn <mattd@nulogy.com>
2018-12-27 10:15:48 -0500

    adds Intro component

---
+++ gatsby/src/components/Intro.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Intro = styled.p`
+    font-size: 21.333px;
+    line-height: 32px;
+    max-width: 720px;
+    color: #434d59;
+    padding: 24px 0;
+`
+  
+export default Intro
+
