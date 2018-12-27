34264a874932e09c4735d02e1b8db3a1152fe916
Matt Dunn <mattd@nulogy.com>
2018-12-27 10:15:59 -0500

    adds Nav components

---
+++ gatsby/src/components/nav.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Nav = styled.ul`
+`
+
+const NavItem = styled.li`
+    margin: 24px;
+
+    a {
+        color: rgb(33, 107, 235);
+        text-decoration: none;
+    }    
+`
+
+export { NavItem, Nav }
+
