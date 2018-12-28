d5c890d650d1e8250d7f55c1315812ca551f8727
Matt Dunn <mattd@nulogy.com>
2018-12-28 14:50:02 -0500

    adds theme as default prop to headings

--- components/src/Type/Headings.js
+++ components/src/Type/Headings.js
@@ -6,21 +6,21 @@ Title.defaultProps = {
   fontSize: 4,
   lineHeight: 1.04, 
   fontWeight: 0,
-  m: 0
+  theme: theme
 }
 
 const SectionTitle = Text.withComponent('h1')
 SectionTitle.defaultProps = {
   fontSize: 3,
   lineHeight: 1.23, 
-  m: 0
+  theme: theme
 }
 
 const SubsectionTitle = Text.withComponent('h1')
 SubsectionTitle.defaultProps = {
   fontSize: 2,
   lineHeight: 1.33,
-  m: 0
+  theme: theme
 }
 
 export { Title, SectionTitle, SubsectionTitle }
