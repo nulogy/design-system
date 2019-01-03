1c6949a2338c8994a24678a9f31c01b849d483bc
Matt Dunn <mattd@nulogy.com>
2019-01-03 09:15:36 -0500

    loads new stories

--- components/.storybook/config.js
+++ components/.storybook/config.js
@@ -7,15 +7,21 @@ import Box from '../src/Box/Box';
 import '../../css/src/nds-dev.css';
 
 function loadStories() {
-  require('../src/Link/Link.story.js');
   require('../src/Box/Box.story.js');
+  require('../src/Button/Button.story.js');  
   require('../src/Flex/Flex.story.js');
-  require('../src/Type/Type.story.js');
+  require('../src/Type/Headings.story.js');
+  require('../src/Link/Link.story.js');
+  require('../src/Type/Text.story.js');
 }
+
+// addDecorator(withInfo)
 
 addDecorator((story) => (
   <ThemeProvider theme={theme}>
-    <Box p={1}>{story()}</Box>
+    <Box p={2}>
+      {story()}
+    </Box>
   </ThemeProvider>
 ))
 
