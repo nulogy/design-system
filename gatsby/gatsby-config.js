754b1435183085874f8108e4079de30861dfcef5
Matt Dunn <mattd@nulogy.com>
2019-01-03 09:20:19 -0500

    updates gatsby config description

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nulogy Design System`,
+    description: `Documentation for the Nulogy Design System`,
+    author: `Nulogy`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
