ebae1ef6044f269b00e13b071c0db3c53aa676b3
Matt Dunn <mattd@nulogy.com>
2019-01-03 09:14:29 -0500

    adds stories

--- components/src/Flex/Flex.story.js
+++ components/src/Flex/Flex.story.js
@@ -3,13 +3,12 @@ import { storiesOf } from '@storybook/react';
 import Box from '../Box/Box';
 import Flex from './Flex';
 
-storiesOf('Layout/Flex', module)
+storiesOf('Flex', module)
   .add('Default', () => (
-    <Box bg='black' maxWidth={1040}>
+    <Box bg='black' p={4} maxWidth={1040}>
       <Flex>
-        <Box width={1/2} p={4} m={2} color='white' bg='blackBlue'>
-        </Box>
-        <Box width={1/2} p={8} m={2} bg='lightGreen'>Box</Box>
+        <Box width={1/2} p={4} m={2} color='white' bg='blackBlue'>Box</Box>
+        <Box width={1/2} p={4} m={2} color='white' bg='darkBlue'>Box</Box>
       </Flex>
     </Box>   
   )) 
---
+++ components/src/Type/Headings.story.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Text from './Text';
+import {Title, SectionTitle, SubsectionTitle} from './Headings';
+
+storiesOf('Headings', module)
+  .add('Title', () => (
+      <Title>Title</Title>
+  ))
+  .add('Section Title', () => (
+      <SectionTitle>Section Title</SectionTitle>
+  ))  
+  .add('Subsection Title', () => (
+    <SubsectionTitle>SubsectionTitle</SubsectionTitle>
+  ))
+  .add('With a custom margin', () => (
+    <React.Fragment>
+      <Title mb={6}>Title</Title>
+      <Text>Lorem ipsum</Text>
+    </React.Fragment>
+))    
+;
