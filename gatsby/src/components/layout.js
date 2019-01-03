fe7d72f1d724425ac5064fb391138ce8cd311ae0
Matt Dunn <mattd@nulogy.com>
2019-01-03 09:23:06 -0500

    updates layout to one column, hiding nav for now

--- gatsby/src/components/layout.js
+++ gatsby/src/components/layout.js
@@ -1,44 +1,63 @@
-// TO-DO: https://www.npmjs.com/package/gatsby-plugin-layout
-
 import React from 'react'
-import { NavItem, Nav, NavLink} from '../components/Nav'
+// import { NavItem, Nav, NavLink} from '../components/Nav'
 import styled, {ThemeProvider} from 'styled-components'
-import { Flex } from '../../../components/src/Flex'
-import { Box } from '../../../components/src/Box'
+import {Box, Flex} from '@nulogy/components'
 import theme from '../../../components/src/theme'
 import '../../../css/dist/nds.css';
 
 import logo from '../images/nulogy.svg'
 
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1250px;
+`
+
+Container.defaultProps = {
+  theme: theme
+}
+
 export default ({ children }) => (
-  <Flex style={{borderTop: '32px solid #011E38', paddingTop: '64px'}}>
-    <Box p={`0 48px 64px 48px`} width={1/4} color='white' bg='blackBlue'>
-      <a href="/"><img src={logo} alt="Logo" width="112px" /></a>
-      <Nav>
-        <NavItem>
-          <a href="#">Style</a>
-          <Nav>
-            <NavItem><a href="/style/colour">Colour</a></NavItem>
-            <NavItem><a href="#">Typography</a></NavItem>
-            <NavItem><a href="#">Spacing</a></NavItem>
-            <NavItem><a href="#">Iconography</a></NavItem>
-          </Nav>
-        </NavItem>
-        <NavItem>
-          <a href="#">Components</a>
-          <Nav>
-            <NavItem><a href="#">Buttons</a></NavItem>
-            <NavItem><a href="#">Form</a></NavItem>
-            <NavItem><a href="#">Table</a></NavItem>
-          </Nav>
-        </NavItem>
-        <NavItem><a href="#">Tokens</a></NavItem>
-        <NavItem><a href="#">Designers Guide</a></NavItem>
-        <NavItem><a href="#">Developers Guide</a></NavItem>
-      </Nav>
+  <ThemeProvider theme={theme}>
+  <Box>
+    <Box bg='blackBlue' align='center' p={3}>
+    <Box pl={4} pr={4} maxWidth={1000} m='auto'>
+    <Flex>
+      <Box width={1/2}>
+        <a href="/"><img src={logo} alt="Logo" width="56px" /></a>
+      </Box>
+      <Box width={1/2} textAlign='right'>
+      <ul>
+        <li style={{'display': 'inline'}}><a style={{'color': '#fff', paddingRight: '24px', 'textDecoration': 'none'}} href="/style/colour">Visual Style</a></li>
+        <li style={{'display': 'inline'}}><a style={{'color': '#fff', paddingRight: '24px', 'textDecoration': 'none'}} href="/components">Components</a></li>
+        <li style={{'display': 'inline'}}><a a style={{'color': '#fff', paddingRight: '24px', 'textDecoration': 'none'}} href="/tokens">Tokens</a></li>
+        <li style={{'display': 'inline'}}><a a style={{'color': '#fff', paddingRight: '24px', 'textDecoration': 'none'}} href="/resources">Resources</a></li>
+      </ul>
+      </Box>
+      </Flex>
+      </Box>
     </Box>
-    <Box p={`48px 48px 48px 0`} width={3/4}>
-    {children}
+      <Box pt={6} pl={4} pr={4} maxWidth={1000} m='auto'>
+        {children}
+      </Box>
     </Box>
-  </Flex>
+  </ThemeProvider>
 )
+
+
+// <Nav>
+// <NavItem>
+//   <a href="#">Visual Style</a>
+//   <Nav>
+//     <NavItem><a href="/style/colour">Colour</a></NavItem>
+//     <NavItem><a href="/style/typography">Typography</a></NavItem>
+//     <NavItem><a href="/style/spacing">Spacing</a></NavItem>
+//     <NavItem><a href="/style/iconography">Iconography</a></NavItem>
+//   </Nav>
+// </NavItem>
+// <NavItem><a href="#">Components</a>
+// </NavItem>
+// <NavItem><a href="/tokens">Tokens</a></NavItem>
+// <NavItem><a href="/guides/designers">Designers Guide</a></NavItem>
+// <NavItem><a href="/guides/developers">Developers Guide</a></NavItem>
+// </Nav>
