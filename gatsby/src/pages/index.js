04df39cad7c14c4fcdde9a3193682fd6815e1a26
Matt Dunn <mattd@nulogy.com>
2019-01-03 09:21:36 -0500

    updates pages to use actual nulogy components

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import {Box, Flex, Text, Title, SectionTitle} from '@nulogy/components';
+import {Button, Box, Flex, Text, Title, SectionTitle} from '@nulogy/components';
 import Intro from '../components/Intro'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <section>
-        <h1>Nulogy Design System</h1>
+    <Box bg='whiteGrey' p={5} borderRadius={1} mb={6}>
+        <Title mb={3}>Nulogy Design System</Title>
         <Intro>The Nulogy Design System is a collection of Visual Guidelines and UI Components that will allow designers and developers to quickly create consistent experiences for our customers using established best practices.</Intro>
-    </section>
+    </Box>
 
-    <div>
-        <h2>Visual Style</h2>
-        <p>Learn about the style that makes up Nulogy applications; including logo usage, typography, our colour system, iconography and spacing.</p>
-        <a href="#">Learn how to design for Nulogy</a>
-    </div>
+    <Flex>
+        <Box boxShadow={0} mb={3} pr={3}>
+            <SectionTitle mb={3}>Visual Style</SectionTitle>
+            <Text mb={3}>Learn about the style that makes up Nulogy applications; including logo usage, typography, our colour system, iconography and spacing.</Text>
+            <Button href="/style/colour">Learn how to design for Nulogy</Button>
+        </Box>
 
-    <div>
-        <h2>Components</h2>
-        <p>Built using React, components are tested interface design patterns designed to ensure a consistent experience for our users.</p>
-        <a href="#">Use our components</a>
-    </div> 
+        <Box boxShadow={1} mb={3} pl={3}>
+            <SectionTitle mb={3}>Components</SectionTitle>
+            <Text mb={3}>Built using React, components are tested interface design patterns designed to ensure a consistent experience for our users.</Text>
+            <Button href="components">Use our components</Button>
+        </Box>
+    </Flex> 
   </Layout>
 )
 
