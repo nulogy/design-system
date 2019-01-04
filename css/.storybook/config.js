4d85584dfab26526c580e8084ed3e72a39ec66b0
Matt Dunn <mattd@nulogy.com>
2019-01-04 08:30:06 -0500

    manually loads css stories

--- css/.storybook/config.js
+++ css/.storybook/config.js
@@ -1,10 +1,16 @@
 import { configure } from '@storybook/html';
 import '../src/nds-dev.css';
 
-configure(loadStories, module);
+  function loadStories() {
+    require('../src/scss/components/_buttons.story.js');
+    require('../src/scss/components/_type.story.js');
+    require('../src/scss/utilities/_borders.story.js');
+    require('../src/scss/utilities/_colors.story.js');
+    require('../src/scss/utilities/_helpers.story.js');
+    require('../src/scss/utilities/_layout.story.js');
+    require('../src/scss/utilities/_shadows.story.js');
+    require('../src/scss/utilities/_space.story.js');
+    require('../src/scss/utilities/_type.story.js');
+  }
 
-configureStorybook(require.context(
-    "../src/scss/",  // path where stories live
-    true,           // recursive?
-    /\.story.js$/, // story files match this pattern
-  ));
+  configure(loadStories, module);
