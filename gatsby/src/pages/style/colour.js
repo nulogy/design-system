b13bab659d1aa9554fe4fdf171d705c0a682f176
Matt Dunn <mattd@nulogy.com>
2019-01-07 13:57:25 -0500

    fixes some inconsistency in structure in colours.js

--- gatsby/src/pages/style/colour.js
+++ gatsby/src/pages/style/colour.js
@@ -1,10 +1,9 @@
-import React from "react"
-import {Box, Flex, Text, Title, SectionTitle} from '@nulogy/components';
-import * as tokens from '@nulogy/tokens';
+import React from 'react'
+import {Box, Flex, Text, Title, SectionTitle} from '@nulogy/components'
 import {Layout, Intro, DocSection} from '../../components'
+import * as tokens from '@nulogy/tokens'
 
 const Palette = props => (
-    // <Box width={{md: 1/3, lg: 1/3}} mb={"24px"}>
     <Box width={1/3} mb={3}>
         <Box mr={2} pt={6} pb={6} mb={1} bg={props.colour} borderRadius={1}></Box>
         <Text mb={0}>{props.name}</Text>
@@ -18,7 +17,8 @@ export default () => (
             <Title>Colour</Title>   
             <Intro>Colours are used to set a visual tone, communicate meaning, and create a cohesive experience between Nulogy products and the physical environment. We are committed to complying with <a href="https://webaim.org/resources/contrastchecker/">WCAG AA</a> contrast ratios.</Intro>
         </DocSection>
-        <Box mb={3}>
+        
+        <DocSection mb={3}>
             <SectionTitle>Text & Interactive Colours</SectionTitle>
             <Flex>
                 <Palette name="Black" colour={tokens.color_base_black}></Palette>
@@ -27,9 +27,9 @@ export default () => (
                 <Palette name="Blue" colour={tokens.color_base_blue}></Palette>
                 <Palette name="Light Blue" colour={tokens.color_base_light_blue}></Palette>
             </Flex>
-        </Box>
+        </DocSection>
 
-        <Box mb={3}>
+        <DocSection mb={3}>
             <SectionTitle>UI & Background Colours</SectionTitle>
             <Flex>
                 <Palette name="Dark Grey" colour={tokens.color_base_dark_grey}></Palette>
@@ -38,14 +38,15 @@ export default () => (
                 <Palette name="White Grey" colour={tokens.color_base_white_grey}></Palette>
                 <Palette name="White" colour={tokens.color_base_white}></Palette>
             </Flex>
-        </Box>
-        <Box mb={3}>
+        </DocSection>
+
+        <DocSection mb={3}>
             <SectionTitle>Contextual Colours</SectionTitle>
             <Flex>
                 <Palette name="Green" colour={tokens.color_base_green}></Palette>
                 <Palette name="Red" colour={tokens.color_base_red}></Palette>
                 <Palette name="Yellow" colour={tokens.color_base_yellow}></Palette>
             </Flex>
-        </Box>
+        </DocSection>
     </Layout>
 )
