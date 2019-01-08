292e8dda8b130d8f519b66df87674d051babd3b4
Matt Dunn <mattd@nulogy.com>
2019-01-08 10:10:38 -0500

    removes unused reference in layout.js

--- gatsby/src/pages/style/spacing.js
+++ gatsby/src/pages/style/spacing.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Box, Text, Title, SectionTitle} from '@nulogy/components'
-import {Layout, Intro, DocSection} from '../../components'
+import {Layout, Intro, DocSection, CheckList} from '../../components'
 import anatomy from '../../images/spacing-anatomy.png'
 import scale from '../../images/spacing-scale.svg'
 
@@ -12,31 +12,24 @@ export default () => (
         </DocSection>
 
         <DocSection>
+            <SectionTitle>Scale</SectionTitle>
             <Text>Nulogy uses a scale based on 8px with modifiers from half (4px) to 8x (64px).</Text>
-            <img src={anatomy} alt="Spacing example" style={{width: '50%'}} />
+            <img src={scale} alt="Spacing scale" />
         </DocSection>
 
         <DocSection>
-            <img src={scale} alt="Spacing example" />
+            <SectionTitle>Example</SectionTitle>
+            <img src={anatomy} alt="Spacing example" style={{width: '50%'}} /> 
         </DocSection>
 
         <DocSection>
             <SectionTitle>How to choose spacing</SectionTitle>
-            <Text>There are two important factors to consider when choosing spacing: size and relatedness</Text>
+            <Text>There are two important factors to consider when choosing spacing: <em>size</em> and <em>relatedness</em></Text>
             <Box mb={2}>
-            <Text mb={0}>Use less spacing inside smaller elements or between functionally related elements.</Text>
-            <Text>Use more spacing inside larger elements or between less functionally related elements</Text>
+            <CheckList>Use less spacing inside smaller elements or between functionally related elements.</CheckList>
+            <CheckList>Use more spacing inside larger elements or between less functionally related elements</CheckList>
             </Box>
-            <Text>Note: half should mostly be used for spacing related items within an element, e.g a button’s text and it’s icon.</Text>
-        </DocSection>
-
-        <DocSection>
-            <SectionTitle>Usage</SectionTitle>
-            <ul>
-                <li><a href="/">Tokens</a></li>
-                <li><a href="/">CSS classes</a></li>
-                <li><a href="/">React storybook</a></li>
-            </ul>
+            <Text fontSize={0}><em>Note: half should mostly be used for spacing related items within an element, e.g a button’s text and it’s icon.</em></Text>
         </DocSection>
     </Layout>
 )
