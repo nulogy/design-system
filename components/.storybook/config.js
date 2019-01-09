088a4672ed78988af5f294b0eb454952123090bb
nikola-nulogy <nikolap@nulogy.com>
2019-01-09 14:41:04 -0500

    Adds background grid styles to storybook

--- components/.storybook/config.js
+++ components/.storybook/config.js
@@ -5,15 +5,21 @@ import theme from '../src/theme';
 import Box from '../src/Box/Box';
 import '../../css/src/nds-dev.css';
 // import ThemeProvider from '../src/ThemeProvider/ThemeProvider';
+import withStyles from "@sambego/storybook-styles";
 
 function loadStories() {
   require('../src/Box/Box.story.js');
-  require('../src/Button/Button.story.js');  
+  require('../src/Button/Button.story.js');
   require('../src/Flex/Flex.story.js');
   require('../src/Type/Headings.story.js');
   require('../src/Link/Link.story.js');
   require('../src/Type/Text.story.js');
 }
+
+addDecorator(withStyles({
+  backgroundSize: '8px 8px',
+  backgroundImage: 'linear-gradient(to right, transparent 7px, #E4E7EB 1px), linear-gradient(to bottom, transparent 7px, #E4E7EB 1px)'
+}));
 
 addDecorator((story) => (
   <ThemeProvider theme={theme}>
