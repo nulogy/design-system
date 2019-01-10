f1f125b0d12789d14a94e0b4614fc63a03781d42
nikola-nulogy <nikolap@nulogy.com>
2019-01-10 12:15:58 -0500

    Adds article story

--- components/src/Type/Text.story.js
+++ components/src/Type/Text.story.js
@@ -18,21 +18,73 @@ storiesOf('Text', module)
     <Text mb={4}>Default text</Text>
     <Text>Default text</Text>
     </React.Fragment>
-<<<<<<< HEAD
   ))
   .add('Set to inline', () => (
     <React.Fragment>
       <Text inline mr={2}>Default text</Text>
       <Text inline>Default text</Text>
     </React.Fragment>
-  ));
-=======
-))  
-.add('Set to inline', () => (
+  ))
+.add('Article', () => (
   <React.Fragment>
-    <Text inline mr={2}>Default text</Text>
-    <Text inline>Default text</Text>
+    <Title>Nunc vitae nisl vestibulum vitae nisl vestibulum vitae nisl vestibulum</Title>
+    <SectionTitle>Donec leo felis vitae nisl vestibulum vitae nisl vestibulum vitae nisl vestibulum</SectionTitle>
+    <Text>
+      Nunc tempor eget mauris id facilisis. Morbi convallis mauris at
+      fermentum gravida. Nunc lacinia a odio eu rutrum. Etiam in libero
+      vestibulum, lobortis mi fermentum, pharetra lacus. Aliquam commodo
+      molestie dolor, vel tristique orci efficitur eu. Nullam eleifend
+      malesuada. Nam luctus blandit dignissim. Mauris eu odio tristique,
+      lorem quis, lobortis nulla. Interdum et malesuada fames ac ante ipsum
+      primis in faucibus. Nunc quis lacus felis. Ut convallis rhoncus orci.
+      Maecenas sit amet leo dui. Integer semper porta dignissim.
+    </Text>
+    <SubsectionTitle>Two Paragraphs and a Long Titile that Hopefully wraps. Maybe now? How About Now? Now? Now? Now?</SubsectionTitle>
+    <Text>
+      Porttitor urna sit amet, congue nulla. Etiam in posuere nibh. Nam
+      pellentesque, lacus id elementum posuere, neque purus ullamcorper nunc,
+      consequat mi velit eget mi. Duis ipsum augue, pulvinar ullamcorper
+      fringilla in, dignissim congue velit.
+    </Text>
+    <SectionTitle>Donec leo felis</SectionTitle>
+    <SubsectionTitle>Nunc lacinia a odio eu rutrum</SubsectionTitle>
+    <Text>
+        Nunc tempor eget mauris id facilisis. Morbi convallis mauris at
+        fermentum gravida. Nunc lacinia a odio eu rutrum. Etiam in libero
+        vestibulum, lobortis mi fermentum, pharetra lacus. Aliquam commodo
+        molestie dolor, vel tristique orci efficitur eu. Nullam eleifend
+        malesuada. Nam luctus blandit dignissim. Mauris eu odio tristique,
+        lorem quis, lobortis nulla. Interdum et malesuada fames ac ante ipsum
+        primis in faucibus. Nunc quis lacus felis. Ut convallis rhoncus orci.
+        Maecenas sit amet leo dui. Integer semper porta dignissim.
+    </Text>
+    <Text>Nunc id arcu sagittis, volutpat
+      sit amet, accumsan diam. Pellentesque luctus, nulla a ornare semper,
+      dui mollis nisi, vel lacinia neque velit eget sapien. Etiam sodales
+      dolor, vel dictum libero cursus ac. Nam vulputate tempor mauris vel.
+      Nam tristique metus et dignissim pretium. Aliquam erat volutpat.
+    </Text>
+    <SubsectionTitle>This is small text (14px) with medium(default) line height (24px).</SubsectionTitle>
+    <Text fontSize={0}>
+      Porttitor urna sit amet, congue nulla. Etiam in posuere nibh. Nam
+      pellentesque, lacus id elementum posuere, neque purus ullamcorper nunc,
+      consequat mi velit eget mi. Duis ipsum augue, pulvinar ullamcorper
+      fringilla in, dignissim congue velit. Nunc id arcu sagittis, volutpat
+      sit amet, accumsan diam. Pellentesque luctus, nulla a ornare semper,
+      dui mollis nisi, vel lacinia neque velit eget sapien. Etiam sodales
+      dolor, vel dictum libero cursus ac. Nam vulputate tempor mauris vel.
+      Nam tristique metus et dignissim pretium. Aliquam erat volutpat.
+    </Text>
+    <SubsectionTitle>This is small text (14px) with small line height (16px). Reserved for buttons, inputs, maybe tables ...</SubsectionTitle>
+    <Text fontSize={0}>
+      Porttitor urna sit amet, congue nulla. Etiam in posuere nibh. Nam
+      pellentesque, lacus id elementum posuere, neque purus ullamcorper nunc,
+      consequat mi velit eget mi. Duis ipsum augue, pulvinar ullamcorper
+      fringilla in, dignissim congue velit. Nunc id arcu sagittis, volutpat
+      sit amet, accumsan diam. Pellentesque luctus, nulla a ornare semper,
+      dui mollis nisi, vel lacinia neque velit eget sapien. Etiam sodales
+      dolor, vel dictum libero cursus ac. Nam vulputate tempor mauris vel.
+      Nam tristique metus et dignissim pretium. Aliquam erat volutpat.
+    </Text>
   </React.Fragment>
-))
-;
->>>>>>> updates story names to new format
+));
