6348c1988b79173d236cc9ae7d7d3abb2ce9baa8
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-14 15:37:19 -0500

    solves merge conflics

--- components/.storybook/config.js
+++ components/.storybook/config.js
@@ -1,24 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { configure, addDecorator } from '@storybook/react';
 import theme from '../src/theme';
 import Box from '../src/Box/Box';
-import ThemeProvider from '../src/ThemeProvider/ThemeProvider';
+import '../../css/src/nds-dev.css';
+// import ThemeProvider from '../src/ThemeProvider/ThemeProvider';
 import withStyles from "@sambego/storybook-styles";
 
 const req = require.context('../src', true, /\.story\.js$/);
 
 function loadStories() {
-<<<<<<< HEAD
-  require('../src/Box/Box.story.js');
-  require('../src/Button/Button.story.js');
-  require('../src/Flex/Flex.story.js');
-  require('../src/Type/Headings.story.js');
-  require('../src/Link/Link.story.js');
-  require('../src/Type/Text.story.js');
-  require('../src/Icon/Icon.story.js');
-=======
   req.keys().forEach(filename => req(filename));
->>>>>>> master
 }
 
 addDecorator(withStyles({
