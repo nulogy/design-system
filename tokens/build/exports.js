781baa3121d8c7c2aaa0f61e7e204a2beaebc5a4
Matt Dunn <mattd@nulogy.com>
2019-01-15 11:30:41 -0500

    adds unitless lineHeight values to tokens

--- tokens/build/exports.js
+++ tokens/build/exports.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Tue, 15 Jan 2019 15:35:23 GMT
+ * Generated on Tue, 15 Jan 2019 16:21:21 GMT
  */
 
 export const color_base_black = "#011e38";
@@ -20,6 +20,11 @@ export const color_base_light_red = "#faebec";
 export const color_base_yellow = "#ffbb00";
 export const font_family_base = "'IBM Plex Sans', sans";
 export const font_family_mono = "'IBM Plex Mono', monospace";
+export const line_height_base = "24";
+export const line_height_compressed = "16";
+export const line_height_page_title = "48";
+export const line_height_section_title = "32";
+export const line_height_subsection_title = "24";
 export const radius_background_small = "2px";
 export const radius_background_medium = "4px";
 export const shadow_box_small = "0 1px 1px rgba(27,31,35,0.1)";
@@ -33,11 +38,6 @@ export const size_font_base = "16px";
 export const size_font_page_title = "46px";
 export const size_font_section_title = "26px";
 export const size_font_subsection_title = "18px";
-export const size_line_height_base = "24px";
-export const size_line_height_compressed = "16px";
-export const size_line_height_page_title = "48px";
-export const size_line_height_section_title = "32px";
-export const size_line_height_subsection_title = "24px";
 export const size_padding_half = "4px";
 export const size_padding_x_1 = "8px";
 export const size_padding_x_2 = "16px";
