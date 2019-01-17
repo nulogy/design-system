6339381f1233d39f04da6d101f65ec1855422064
nikola-nulogy <nikolap@nulogy.com>
2019-01-17 15:28:44 -0500

    Styles Iconic button

--- components/src/Button/IconicButton.js
+++ components/src/Button/IconicButton.js
@@ -1,27 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-//import { color, space, width, maxWidth, borderRadius, textAlign } from 'styled-system'
 import theme from '../theme'
 import Icon from '../Icon/Icon.js';
 import Text from '../Type/Text.js';
 import Flex from '../Flex/Flex.js';
 
+const labelVisibility = props => {
+    switch (props.labelVisibility) {
+      case 'visible':
+        return {
+          display: 'flex',
+          alignItems: 'center'
+        }
+      case 'hidden':
+        return {
+          display: 'block'
+        }
+      default:
+        return {
+          display: 'flex',
+          alignItems: 'center'
+        }
+    }
+  }
+
 const Wrapper = styled.button`
   background: transparent;
   border: none;
+  position: relative;
   padding: ${theme.space[1]} ${theme.space[0]};
-  display: flex;
-  align-items: center;
   color: ${theme.colors['darkBlue']};
-  cursor: ${props => props.disabled ? 'arrow' : 'pointer'}};
+  cursor: ${props => props.disabled ? 'arrow' : 'pointer'};
+
+  ${labelVisibility}
+
   ${Icon} {
     border-radius: 50%;
+    transition: .2s;
+    min-width: 32px;
   }
   ${Text} {
+    display: ${props => props.label && props.labelVisibility == 'visible' ? 'block' : 'none'};
+    position: ${props => props.labelVisibility == 'hidden' ? 'absolute' : null};
+    font-size: ${props => props.labelVisibility == 'hidden' ? props.theme.fontSizes[0] : null};
+    font-weight: ${props => props.labelVisibility == 'visible' ? '600' : '400'};
+    line-height: ${props => props.labelVisibility == 'hidden' ? props.theme.colors['smallTextCompressed'] : null};
+    text-align: left;
+    padding: ${props => props.labelVisibility == 'hidden' ? props.theme.space[1] : null};
+    z-index: ${props => props.labelVisibility == 'hidden' ? '10' : null};
+    top: ${props => props.labelVisibility == 'hidden' ? '40px' : null};
+    left: ${props => props.labelVisibility == 'hidden' ? '-12px' : null}; // hard coded, needs function
+    border-radius: ${props => props.labelVisibility == 'hidden' ? props.theme.radii[0] : null};
+    background: ${props => props.labelVisibility == 'hidden' ? props.theme.colors['lightBlue'] : null};
+    pointer-events: ${props => props.labelVisibility == 'hidden' ? 'none' : null};
   }
   &:hover{
     ${Icon} {
         background ${theme.colors['lightBlue']};
+    }
+    ${Text} {
+      display: ${props => props.label && props.labelVisibility == 'hidden' ? 'block' : null};
     }
   }
   &:focus {outline: none;}
@@ -30,16 +69,32 @@ const Wrapper = styled.button`
       transform: scale(0.875); transition: .2s ease-in;}
     }
   }
-  &:disabled {opacity: .5;}
+  &:disabled {
+    opacity: .5;
+    &:hover, &:active {
+      ${Icon} {
+        background: none;
+        transform: none;
+      }
+      ${Text} {
+        display: ${props => props.label && props.labelVisibility == 'hidden' ? 'none' : null};
+      }
+    }
+  }
 `
 
-const IconicButton = ({ ...props }) => {
+const IconicButton = (props) => {
   return (
-    <Wrapper>
-      <Icon size={theme.space[5]} name='delete' mr={1} p={1} />
-      <Text mb={0} mr={1}>Delete</Text>
+    <Wrapper { ...props } >
+      <Icon size={theme.space[5]} name='delete' p={1} />
+      <Text mr={1} mb={0} ml={1}>{props.label}</Text>
     </Wrapper>
   )
+}
+
+IconicButton.propTypes = {
+  disabled: PropTypes.bool,
+  label: PropTypes.string
 }
 
 export default IconicButton
