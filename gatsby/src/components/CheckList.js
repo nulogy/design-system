80746df4327c5c724040ac331a3b9819eae1d9ca
Matt Dunn <mattd@nulogy.com>
2019-01-18 10:26:24 -0500

    fixes CheckList spacing

--- gatsby/src/components/CheckList.js
+++ gatsby/src/components/CheckList.js
@@ -4,14 +4,14 @@ const CheckList = styled.li`
     list-style: none;
     margin-left: ${props => props.theme.space[3]};
     vertical-align: middle;
-    &:not(:last-child) {margin-bottom: ${props => props.theme.space[3]};}
+    &:not(:last-child) {margin-bottom: ${props => props.theme.space[0]};}
     &:before {
         content: '✔️';
         margin-right: ${props => props.theme.space[1]};
         float: left;
         clear: both;
         position: relative; 
-        top: -2px;
+        top: -0px;
     }
 ` 
 
