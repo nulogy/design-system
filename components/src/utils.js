7cf2fb309aa76193562ea688cb2e9afdf4febd87
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-18 10:57:26 -0500

    cleanup utils.js

---
+++ components/src/utils.js
@@ -0,0 +1,7 @@
+export function getTextWidth(text, font) {
+    var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+    var context = canvas.getContext("2d");
+    context.font = font;
+    var metrics = context.measureText(text);
+    return metrics.width;
+}
