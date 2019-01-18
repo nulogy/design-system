76746a2150cd38c52fe505bea61f9ff2ca0835c9
nikola-nulogy <nikolap@nulogy.com>
2019-01-17 21:05:40 -0500

    Adds without label without labelVisibility prop story

--- components/src/Button/IconicButton.story.js
+++ components/src/Button/IconicButton.story.js
@@ -4,18 +4,20 @@ import IconicButton from './IconicButton';
 
 storiesOf('IconicButton', module)
   .add('With label', () => (
-    <IconicButton labelVisibility='visible'>Delete</IconicButton>
+    <IconicButton icon='delete' labelVisibility='visible'>Delete</IconicButton>
   ))
   .add('Without label', () => (
-    <IconicButton labelVisibility='hidden'>Delete</IconicButton>
+    <React.Fragment>
+      <IconicButton icon='edit' >Edit</IconicButton>
+      <IconicButton icon='delete' labelVisibility='hidden'>Delete</IconicButton>
+    </React.Fragment>
   ))
   .add('With a long label', () => (
-    <IconicButton labelVisibility='visible'>I am Iconic Button with really really really long label</IconicButton>
+    <IconicButton icon='delete' labelVisibility='visible'>I am Iconic Button with really really really long label</IconicButton>
   ))
   .add('Disabled', () => (
     <React.Fragment>
-      <IconicButton>Delete</IconicButton>
-      <IconicButton labelVisibility='visible' disabled >Delete</IconicButton>
-      <IconicButton labelVisibility='hidden' disabled >Delete</IconicButton>
+      <IconicButton icon='delete' labelVisibility='visible' disabled >Delete</IconicButton>
+      <IconicButton icon='delete' labelVisibility='hidden' disabled >Delete</IconicButton>
     </React.Fragment>
   ));
