b00db5a1682ef3d41325b43846378b030c46c2bf
Matt Dunn <mattd@nulogy.com>
2019-01-18 10:26:53 -0500

    adds intro field to markdown frontmatter

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -10,7 +10,7 @@ exports.createPages = ({ actions, graphql }) => {
   return graphql(`
     {
       allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
+        sort: { order: DESC, fields: [frontmatter___intro] }
         limit: 1000
       ) {
         edges {
