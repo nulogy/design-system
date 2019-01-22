5ca867a6387dcaffb3c86ddd0c81b0e1f98d39fb
nikola-nulogy <nikolap@nulogy.com>
2019-01-22 11:09:03 -0500

    Moves iconinc button variations to its own section and fixes styling

--- docs/src/pages/components/buttons.js
+++ docs/src/pages/components/buttons.js
@@ -15,65 +15,85 @@ export default () => (
 
         <DocSection>
             <Flex mb={4}>
-                <Flex width={2/5}  justifyContent='center' alignItems='center' mr={3} >
-                    <Button>Button</Button>
+                <Flex width={2/6}  justifyContent='center' alignItems='center' >
+                    <Button>Create project</Button>
                 </Flex>
-                <Box width={3/5}>
-                    <Text mb={0}>Buttons are used for actions that do not require any special emphasis and cover most cases.</Text>
+                <Box width={4/6}>
+                    <Text mb={0}><Text inline fontWeight={2} mb={0}>Buttons</Text> are used for actions that do not require any special emphasis and cover most cases.</Text>
                 </Box>
             </Flex>
             <Flex mb={4}>
-                <Flex width={2/5}  justifyContent='center' alignItems='center' mr={3} >
-                    <PrimaryButton>Primary button</PrimaryButton>
+                <Flex width={2/6}  justifyContent='center' alignItems='center'>
+                    <PrimaryButton>Edit project</PrimaryButton>
                 </Flex>
-                <Box width={3/5}>
-                    <Text mb={0}>Primary Buttons are used for the main action in a particular context. There is usually not more than one primary button per screen and not all of the screens require a Primary button.</Text>
+                <Box width={4/6}>
+                    <Text mb={0}><Text inline fontWeight={2} mb={0}>Primary Buttons</Text> are used for the main action in a particular context. There is usually not more than one primary button per screen and not all of the screens require a Primary button.</Text>
                 </Box>
             </Flex>
             <Flex mb={4}>
-                <Flex width={2/5} justifyContent='center' alignItems='center' mr={3}>
-                    <DangerButton>Danger button</DangerButton>
+                <Flex width={2/6} justifyContent='center' alignItems='center'>
+                    <DangerButton>Delete project</DangerButton>
                 </Flex>
-                <Box width={3/5}>
-                    <Text mb={0}>Danger Buttons are used for destructive actions such as deleting. They are most likely to appear in confirmation dialogs.</Text>
-                </Box>
-            </Flex>
-            <Flex mb={4}justifyContent='center' alignItems='center'>
-                <Flex width={2/5} justifyContent='center' mr={3}>
-                    <QuietButton>Quiet button</QuietButton>
-                </Flex>
-                <Box width={3/5}>
-                    <Text mb={0}>Quiet Buttons are used for less important actions such as “Cancel” or actions that are not directly related to the context of the page (e.g Learn more …). Quiet buttons are often paired with a Primary button.</Text>
+                <Box width={4/6}>
+                    <Text mb={0}><Text inline fontWeight={2} mb={0}>Danger Buttons</Text> are used for destructive actions such as deleting. They are most likely to appear in confirmation dialogs.</Text>
                 </Box>
             </Flex>
             <Flex mb={4}>
-              <Flex width={2/5} justifyContent='center' alignItems='center' flexDirection='column' mr={3} >
-                <IconicButton mb={5} icon='delete' labelVisibility='hover'>Iconic button</IconicButton>
-                <IconicButton icon='delete' labelVisibility='always'>Iconic button</IconicButton>
+                <Flex width={2/6} justifyContent='center' alignItems='center'>
+                    <QuietButton>Learn more</QuietButton>
+                </Flex>
+                <Box width={4/6}>
+                    <Text mb={0}><Text inline fontWeight={2} mb={0}>Quiet Buttons</Text> are used for less important actions such as “Cancel” or actions that are not directly related to the context of the page (e.g Learn more …). Quiet buttons are often paired with a Primary button.</Text>
+                </Box>
+            </Flex>
+            <Flex mb={4}>
+              <Flex width={2/6} justifyContent='center' alignItems='center' flexDirection='column'>
+                <IconicButton mb={5} icon='add' labelVisibility='hover'>Add project</IconicButton>
               </Flex>
-              <Box width={3/5}>
-                <Text mb={0}>Iconic Buttons are used for universally understood actions that can effectively be represented using icon. There are 2 variations of Iconic buttons: with permanently exposed label and with label exposed on hover.</Text>
+              <Box width={4/6}>
+                <Text mb={0}><Text inline fontWeight={2} mb={0}>Iconic Buttons</Text> are used for universally understood actions that can effectively be represented using icon.</Text>
               </Box>
             </Flex>
         </DocSection>
 
         <DocSection>
             <SectionTitle>Sizes</SectionTitle>
-            <Text>All buttons are available in three sizes and a full-width option.</Text>
-            <Button size="small" mr={4} mb={3}>Small button</Button>
-            <Button size="medium" mr={4} mb={3}>Medium button</Button>
-            <Button size="large" mb={3}>Large button</Button>
-            <Button fullWidth>Full width button</Button>
+            <Text mb='4'>Button, Primary Button, Danger Button and Quiet Button are available in three sizes and a full-width option.</Text>
+                <Button size="small" mb={2}>Read more</Button>
+                <Text fontSize='small'>Small</Text>
+                <Button size="medium" mb={2}>Add project</Button>
+                <Text fontSize='small'>Medium</Text>
+                <Button size="large" mb={2}>Create project</Button>
+                <Text fontSize='small'>Large</Text>
+                <Button fullWidth mb={2}>I agree</Button>
+                <Text fontSize='small'>Full-width</Text>
+        </DocSection>
+
+        <DocSection>
+          <SectionTitle>Variations</SectionTitle>
+          <Text>Iconic Buttons come in two variations.</Text>
+          <Flex justifyContent='space-around' alignItems='center'>
+            <Flex justifyContent='center' alignItems='center' flexDirection='column'>
+              <IconicButton icon='delete' labelVisibility='hover'>Delete</IconicButton>
+              <Text fontSize='small' mt={2}>Label exposed on hover</Text>
+            </Flex>
+            <Flex justifyContent='center' alignItems='center' flexDirection='column'>
+              <IconicButton icon='delete' labelVisibility='always'>Delete</IconicButton>
+              <Text fontSize='small' mt={2}>Permanently exposed label</Text>
+            </Flex>
+          </Flex>
         </DocSection>
 
         <DocSection>
             <SectionTitle>States</SectionTitle>
             <Text>All buttons can be disabled. When a button is disabled, it's greyed out and unable to be clicked.</Text>
-            <Button mr={4} mb={3} disabled>Create project</Button>
-            <PrimaryButton mr={4} mb={3} disabled>Create project</PrimaryButton>
-            <DangerButton mr={4} mb={3} disabled>Delete project</DangerButton>
-            <QuietButton disabled mr={4} mb={3}>Edit project</QuietButton>
-            <IconicButton icon='delete' labelVisibility='always' disabled mr={4} mb={3}>Iconic Button</IconicButton>
+            <Flex justifyContent='space-between' alignItems='center'>
+              <Button disabled>Create project</Button>
+              <PrimaryButton disabled>Create project</PrimaryButton>
+              <DangerButton disabled>Delete project</DangerButton>
+              <QuietButton disabled>Edit project</QuietButton>
+              <IconicButton icon='delete' labelVisibility='hover' disabled>Delete</IconicButton>
+            </Flex>
         </DocSection>
 
         <DocSection>
