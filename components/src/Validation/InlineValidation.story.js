5a4dcc3ecf92f9a72b85ad328f49cf5e39b6c752
nikola-nulogy <nikolap@nulogy.com>
2019-01-23 15:33:21 -0500

    Updates Inline Validation story copy

--- components/src/Validation/InlineValidation.story.js
+++ components/src/Validation/InlineValidation.story.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import InlineValidation from './InlineValidation';
 
-storiesOf('InlineValidation', module)
+storiesOf('Inline Validation', module)
   .add('Inline Validation', () => (
       <InlineValidation message='Something has gone wrong'>
-        <li>Something has gone wrong</li>
-        <li>String must be 3 characters long</li>
+        <li>Something has gone wrong.</li>
+        <li>Entry must be atleast 3 characters long</li>
         <li><a href=''>See here</a></li>
       </InlineValidation>
   ))
   .add('With custom style', () => (
-    <InlineValidation color='green' icon='check' message='Something has gone wrong'>
-      <li>Something has gone wrong</li>
-      <li>String must be 3 characters long</li>
-      <li><a href=''>See here</a></li>
+    <InlineValidation color='green' icon='check' message='Good job.'>
+      <li>Everything is as expected</li>
+      <li><a href='#'>Learn more</a></li>
     </InlineValidation>
   ))
   .add('Without list items', () => (
