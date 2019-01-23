213f7a4ce7cd3923342383fc8af2b6c173ee00a5
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-23 12:48:13 -0500

    adds help text and format text to label component

--- components/src/Label/Label.js
+++ components/src/Label/Label.js
@@ -10,19 +10,26 @@ const Label = (props) =>{
     return(
         <Box {...props}>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> adds help text and format text to label component
             <Text display='inline' mb={0} fontSize={theme.fontSizes[1]}>{props.children}</Text>
             <Text display='inline' mb={0} ml={2} fontSize='12px' color='darkGrey'>{props.requirementText}</Text>
             <Text mb={0} fontSize={theme.fontSizes[0]} lineHeight={theme.lineHeights.smallTextBase}>{props.helpText}</Text>
             <Text mb={0} fontSize='12px' lineHeight='16px' color='darkGrey'>{props.formatText}</Text>
+<<<<<<< HEAD
 =======
             <Text display='inline' mb={0} fontSize={theme.fontSizes[1]} color='black'>{props.children}</Text>
             <Text display='inline' mb={0} ml={2} fontSize={theme.fontSizes[0]} color='darkGrey'>{props.subtext}</Text>
 >>>>>>> adds Label component and changes proptype for display on Text component
+=======
+>>>>>>> adds help text and format text to label component
         </Box>
     )
 }
 
 Label.propTypes = {
+<<<<<<< HEAD
 <<<<<<< HEAD
     requirementText: PropTypes.oneOf(['(Optional)','(Required)']),
     helpText: PropTypes.string,
@@ -30,6 +37,11 @@ Label.propTypes = {
 =======
     subtext: PropTypes.oneOf(['(Optional)','(Required)'])
 >>>>>>> adds Label component and changes proptype for display on Text component
+=======
+    requirementText: PropTypes.oneOf(['(Optional)','(Required)']),
+    helpText: PropTypes.string,
+    formatText: PropTypes.string   
+>>>>>>> adds help text and format text to label component
 }
 
 export default Label
--- components/src/Label/Label.story.js
+++ components/src/Label/Label.story.js
@@ -7,6 +7,7 @@ storiesOf('Label', module)
     <Label>Default label</Label>
   ))
 <<<<<<< HEAD
+<<<<<<< HEAD
   .add('With requirement text', () => (
     <Label requirementText='(Optional)'>Default label</Label>
   ))
@@ -26,14 +27,31 @@ storiesOf('Label', module)
 =======
   .add('With a subtext', () => (
     <Label subtext='(Optional)'>Default label</Label>
+=======
+  .add('With requirement text', () => (
+    <Label requirementText='(Optional)'>Default label</Label>
+>>>>>>> adds help text and format text to label component
   ))
-  .add('With a long label', () => (
-    <Label subtext='(Optional)'>With a really really really really really really really really long label</Label>
+  .add('With help text', () => (
+    <Label helpText = 'Enter a date below'>Default label</Label>
   ))
+<<<<<<< HEAD
   .add('TESTING REMOVE THIS', () => (
     <React.Fragment>
         <Label>Default label</Label>
         <Label subtext='(Optional)'>Default label</Label>
     </React.Fragment>
 >>>>>>> adds Label component and changes proptype for display on Text component
+=======
+  .add('With format text', () => (
+    <Label formatText='(DD-MM-YYYY)'>Default label</Label>
+  ))
+  .add('With all additional text', () => (
+    <Label requirementText='(Optional)' helpText = 'Enter a date below' formatText='(DD-MM-YYYY)'>
+    Default label
+    </Label>
+  ))
+  .add('With a long text', () => (
+    <Label helpText = 'Really long help text Really long help text Really long help text Really long help text Really long help text Really long help text.' requirementText='(Optional)'>With a really really really really really really really really long label</Label>
+>>>>>>> adds help text and format text to label component
   ));
