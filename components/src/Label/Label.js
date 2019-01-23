c948cb3031079acaa64735f471a4d18fef91f481
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-23 10:53:21 -0500

    adds Label component and changes proptype for display on Text component

--- components/src/Label/Label.js
+++ components/src/Label/Label.js
@@ -9,18 +9,27 @@ import Box from '../Box/Box.js'
 const Label = (props) =>{
     return(
         <Box {...props}>
+<<<<<<< HEAD
             <Text display='inline' mb={0} fontSize={theme.fontSizes[1]}>{props.children}</Text>
             <Text display='inline' mb={0} ml={2} fontSize='12px' color='darkGrey'>{props.requirementText}</Text>
             <Text mb={0} fontSize={theme.fontSizes[0]} lineHeight={theme.lineHeights.smallTextBase}>{props.helpText}</Text>
             <Text mb={0} fontSize='12px' lineHeight='16px' color='darkGrey'>{props.formatText}</Text>
+=======
+            <Text display='inline' mb={0} fontSize={theme.fontSizes[1]} color='black'>{props.children}</Text>
+            <Text display='inline' mb={0} ml={2} fontSize={theme.fontSizes[0]} color='darkGrey'>{props.subtext}</Text>
+>>>>>>> adds Label component and changes proptype for display on Text component
         </Box>
     )
 }
 
 Label.propTypes = {
+<<<<<<< HEAD
     requirementText: PropTypes.oneOf(['(Optional)','(Required)']),
     helpText: PropTypes.string,
     formatText: PropTypes.string   
+=======
+    subtext: PropTypes.oneOf(['(Optional)','(Required)'])
+>>>>>>> adds Label component and changes proptype for display on Text component
 }
 
 export default Label
