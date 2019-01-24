d03bc79badb0a5cd7f82f93df8c974e26e307667
nikola-nulogy <nikolap@nulogy.com>
2019-01-24 11:48:43 -0500

    Adds styles to List component

--- components/src/List/List.js
+++ components/src/List/List.js
@@ -1,19 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import { space } from "styled-system";
+import theme from "../theme";
+import Text from "../Type/Text";
 
 const ListWrapper = styled.ul`
-  border: solid 1px red;
-`
+  list-style: none;
+  -webkit-font-smoothing: antialiased;
+  margin: 0;
+
+  padding-left: ${theme.space[4]};
+
+  ${space}
+
+`;
 
 const ListItem = styled.li`
-  border: solid 1px lime;
+  display: flex;
+  margin-bottom: ${theme.space[2]};
+  &:before{
+    content: "•";
+    margin-right: ${theme.space[3]};
+  }
+  &:last-child{
+    margin-bottom: 0;
+  }
 `
 
 const List = props => (
     <ListWrapper>
-      <ListItem>Hey</ListItem>
+      <ListItem><Text mb={0}>Yo</Text></ListItem>
+      <ListItem>List item 2 that is really relly really really really relly really really really relly really really long</ListItem>
+      <ListItem>List item 3</ListItem>
+      <ListItem>List item 4</ListItem>
     </ListWrapper>
 );
+
+ListWrapper.propTypes = {
+  ...space.propTypes
+};
+
+ListWrapper.defaultProps = {
+  mb: 4
+};
 
 export default List;
