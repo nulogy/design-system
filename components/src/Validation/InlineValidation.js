a4db27e3da755461fb54654461b54a2d019b6413
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-24 11:54:21 -0500

    changes API to accept list as a child component

--- components/src/Validation/InlineValidation.js
+++ components/src/Validation/InlineValidation.js
@@ -1,51 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import theme from "../theme";
-import Text from "../Type/Text";
-import Icon from "../Icon/Icon";
-import Flex from "../Flex/Flex";
-import Box from "../Box/Box";
-import icons from "../../icons/icons.json";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
+import theme from '../theme'
+import Text from '../Type/Text'
+import Icon from '../Icon/Icon'
+import Flex from '../Flex/Flex'
+import Box from '../Box/Box'
+import icons from '../../icons/icons.json'
 
-export const iconNames = Object.keys(icons);
+export const iconNames = Object.keys(icons)
 
-const List = styled.ul`
-  -webkit-font-smoothing: antialiased;
-  margin: 0;
-  padding-left: ${theme.space[4]};
-  list-style: none;
-  li:before{
-    content: "•";
-    font-size: 12px;
-    line-height: 16px;
-    padding-right: ${theme.space[2]};
-    position: relative;
-    top: -1px;
-  }
-`;
-
-const InlineValidation = props => (
-  <Box color={ props.color }>
-    <Flex mb={ 2 } alignItems="center">
-      <Icon size="20px" name={ props.icon } mr={ 1 } />
-      <Text mb={ 0 }>{props.message}</Text>
-    </Flex>
-    <List>
+const InlineValidation = (props) => {
+  return (
+    <Box color ={props.color}>
+      <Flex my={2}>
+        <Icon size={theme.space[4]} name={props.icon} mr={1} />
+        <Text mb={0}>{props.message}</Text>
+      </Flex>
       {props.children}
-    </List>
-  </Box>
-);
+    </Box>
+  )
+}
 
 InlineValidation.defaultProps = {
-  color: "red",
-  icon: "error",
-};
+  color: 'red',
+  icon: 'error'
+}
 
 InlineValidation.propTypes = {
   icon: PropTypes.oneOf(iconNames).isRequired,
-  message: PropTypes.string,
-  color: PropTypes.string,
-};
+  message: PropTypes.string.isRequired,
+  color: PropTypes.string
+} 
 
-export default InlineValidation;
+export default InlineValidation
--- components/src/Validation/InlineValidation.story.js
+++ components/src/Validation/InlineValidation.story.js
@@ -5,15 +5,19 @@ import InlineValidation from "./InlineValidation";
 storiesOf("Inline Validation", module)
   .add("Inline Validation", () => (
     <InlineValidation message="Something has gone wrong">
-      <li>Something has gone wrong.</li>
-      <li>Entry must be atleast 3 characters long</li>
-      <li><a href="">See here</a></li>
+      <ul style ={{margin: 0, "-webkit-font-smoothing": "antialiased"}}>
+        <li>Something has gone wrong.</li>
+        <li>Entry must be atleast 3 characters long</li>
+        <li><a href="">See here</a></li>
+      </ul>
     </InlineValidation>
   ))
   .add("With custom style", () => (
     <InlineValidation color="green" icon="check" message="Good job.">
-      <li>Everything is as expected</li>
-      <li><a href="#">Learn more</a></li>
+      <ul style ={{margin: 0, "-webkit-font-smoothing": "antialiased"}}>
+        <li>Everything is as expected</li>
+        <li><a href="#">Learn more</a></li>
+      </ul>
     </InlineValidation>
   ))
   .add("Without list items", () => (
