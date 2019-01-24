f7e6c263f11bdd11190a78a70f87b8e152081541
nikola-nulogy <nikolap@nulogy.com>
2019-01-24 12:52:41 -0500

    Add custom List stories

--- components/src/List/List.story.js
+++ components/src/List/List.story.js
@@ -6,6 +6,21 @@ import theme from "../theme.js";
 storiesOf("List", module)
   .add("List", () => (
     <React.Fragment>
-      <List/>
+      <List />
+    </React.Fragment>
+  ))
+  .add("With custom colour", () => (
+    <React.Fragment>
+      <List color='red' />
+    </React.Fragment>
+  ))
+  .add("With custom font size and line height", () => (
+    <React.Fragment>
+      <List fontSize={ 0 } lineHeight='smallTextBase' />
+    </React.Fragment>
+  ))
+  .add("With compact spacing", () => (
+    <React.Fragment>
+      <List spacing='compact' />
     </React.Fragment>
   ));
