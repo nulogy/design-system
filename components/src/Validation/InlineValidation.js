70725fd3c5928741103f1068f97d16f7f05f725c
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-24 11:56:07 -0500

    auto lint

--- components/src/Validation/InlineValidation.js
+++ components/src/Validation/InlineValidation.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components'
-import theme from '../theme'
-import Text from '../Type/Text'
-import Icon from '../Icon/Icon'
-import Flex from '../Flex/Flex'
-import Box from '../Box/Box'
-import icons from '../../icons/icons.json'
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import theme from "../theme";
+import Text from "../Type/Text";
+import Icon from "../Icon/Icon";
+import Flex from "../Flex/Flex";
+import Box from "../Box/Box";
+import icons from "../../icons/icons.json";
 
-export const iconNames = Object.keys(icons)
+export const iconNames = Object.keys(icons);
 
-const InlineValidation = (props) => {
-  return (
-    <Box color ={props.color}>
-      <Flex my={2}>
-        <Icon size={theme.space[4]} name={props.icon} mr={1} />
-        <Text mb={0}>{props.message}</Text>
-      </Flex>
-      {props.children}
-    </Box>
-  )
-}
+const InlineValidation = props => (
+  <Box color={ props.color }>
+    <Flex my={ 2 }>
+      <Icon size={ theme.space[4] } name={ props.icon } mr={ 1 } />
+      <Text mb={ 0 }>{props.message}</Text>
+    </Flex>
+    {props.children}
+  </Box>
+);
 
 InlineValidation.defaultProps = {
-  color: 'red',
-  icon: 'error'
-}
+  color: "red",
+  icon: "error",
+};
 
 InlineValidation.propTypes = {
   icon: PropTypes.oneOf(iconNames).isRequired,
   message: PropTypes.string.isRequired,
-  color: PropTypes.string
-} 
+  color: PropTypes.string,
+};
 
-export default InlineValidation
+export default InlineValidation;
--- components/src/Validation/InlineValidation.story.js
+++ components/src/Validation/InlineValidation.story.js
@@ -5,7 +5,7 @@ import InlineValidation from "./InlineValidation";
 storiesOf("Inline Validation", module)
   .add("Inline Validation", () => (
     <InlineValidation message="Something has gone wrong">
-      <ul style ={{margin: 0, "-webkit-font-smoothing": "antialiased"}}>
+      <ul style={ { margin: 0, "-webkit-font-smoothing": "antialiased" } }>
         <li>Something has gone wrong.</li>
         <li>Entry must be atleast 3 characters long</li>
         <li><a href="">See here</a></li>
@@ -14,7 +14,7 @@ storiesOf("Inline Validation", module)
   ))
   .add("With custom style", () => (
     <InlineValidation color="green" icon="check" message="Good job.">
-      <ul style ={{margin: 0, "-webkit-font-smoothing": "antialiased"}}>
+      <ul style={ { margin: 0, "-webkit-font-smoothing": "antialiased" } }>
         <li>Everything is as expected</li>
         <li><a href="#">Learn more</a></li>
       </ul>
