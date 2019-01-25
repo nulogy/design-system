092bafa00be37cb736b1f28aab9fec0c1473e2a3
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-25 12:19:16 -0500

    changes custom prop style to pass in all probs to children via an object prop

--- components/src/Label/Label.story.js
+++ components/src/Label/Label.story.js
@@ -4,22 +4,41 @@ import Label from "./Label";
 
 storiesOf("Label", module)
   .add("Label", () => (
-    <Label>Default label</Label>
+    <Label 
+      labelText =  {{ children: "Default label" }}
+    />
   ))
   .add("With requirement text", () => (
-    <Label requirementText="(Optional)">Default label</Label>
+    <Label 
+      labelText =  {{ children: "Default label" }}
+      requirementText= {{ children: "(Optional)" }}
+    />
   ))
   .add("With help text", () => (
-    <Label helpText="Enter a date below">Default label</Label>
+    <Label 
+      labelText =  {{ children: "Default label" }}
+      helpText= {{ children: "Enter a date below" }}
+    />
   ))
   .add("With format text", () => (
-    <Label formatText="(DD-MM-YYYY)">Default label</Label>
+    <Label 
+      labelText =  {{ children: "Default label" }}
+      formatText= {{ children: "(DD-MM-YYYY)" }}
+    />
   ))
   .add("With all additional text", () => (
-    <Label requirementText="(Optional)" helpText="Enter a date below" formatText="(DD-MM-YYYY)">
-    Default label
-    </Label>
+    <Label 
+      labelText =  {{ children: "Default label" }}
+      requirementText= {{ children: "(Optional)" }}
+      helpText= {{ children: "Enter a date below" }}
+      formatText= {{ children: "(DD-MM-YYYY)" }}
+    />
   ))
   .add("With a long text", () => (
-    <Label helpText="Really long help text Really long help text Really long help text Really long help text Really long help text Really long help text." requirementText="(Optional)">With a really really really really really really really really long label</Label>
+    <Label 
+      labelText =  {{ children: "Long long long long long long long long long long long long long long label" }}
+      requirementText= {{ children: "(Optional)" }}
+      helpText= {{ children: "Help text help text help text help text help text help text help text help text" }}
+      formatText= {{ children: "(DD-MM-YYYY)" }}
+    />
   ));
--- components/src/TextInput/TextInput.js
+++ components/src/TextInput/TextInput.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from "react";
-import styled from "styled-components";
-import { color, space, themeGet } from "styled-system";
-import theme from "../theme";
-import { subPx } from "../utils";
-
-const InputField = styled.input`
-  width: 100%;
-  border: 1px solid;
-  border-color:
-    ${props => (props.borderColor ? themeGet(`colors.${props.borderColor}`, props.borderColor) : props.theme.colors.grey)}; 
-  border-radius: ${theme.radii[1]};
-  padding: ${subPx(theme.space[2])};
-  font-size: ${theme.fontSizes[1]};
-  font-family: ${theme.fonts.base};
-  line-height: ${props => props.theme.lineHeights.base};
-  ${space}
-  ${color}
-  &:focus {
-      outline: none;
-      border-color: ${props => props.theme.colors.blue}; 
-      box-shadow: 0 0 3px ${props => props.theme.colors.blue};
-  }
-`;
-
-InputField.defaultProps = {
-  theme,
-};
-
-// TEMPORARY TextInput
-const TextInput = () => (
-  <div>
-    <InputField />
-    <InputField disabled color="grey" borderColor="grey" />
-    <InputField color="red" borderColor="red" />
-  </div>
-);
-=======
 import React from 'react'
 import PropTypes from 'prop-types';
 import theme from '../theme.js'
@@ -57,40 +15,7 @@ const TextInput = (props) => {
         </Box>
     )
 }
->>>>>>> adds help text and format text to label component
 
-=======
-=======
->>>>>>> fix merge conflict
-import React from 'react';
-import PropTypes from 'prop-types';
-import theme from '../theme.js';
-import InputField from './InputField.js';
-import Text from '../Type/Text';
-import Box from '../Box/Box';
-=======
-import React from "react";
-import PropTypes from "prop-types";
-import theme from "../theme.js";
-import InputField from "./InputField.js";
-import Text from "../Type/Text";
-import Box from "../Box/Box";
->>>>>>> eslint autofix
-
-// TEMPORARY TextInput
-const TextInput = props => (
-  <Box>
-    <Text mb={ 0 } fontSize={ theme.fontSizes[1] } lineHeight={ theme.lineHeights.smallTextBase }>I am helping</Text>
-    <Text
-      mb={ 0 } fontSize="12px" lineHeight="16px"
-      color="darkGrey"
-    >(DD-MM-YYY)
-    </Text>
-    <InputField />
-  </Box>
-);
-
->>>>>>> adds help text and format text to label component
 TextInput.defaultProps = {
   theme,
   p: 3,
