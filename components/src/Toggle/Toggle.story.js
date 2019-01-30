f6a2d010ba538c7fcad3385ec1eec1634ea9d574
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-30 14:51:58 -0500

    eslint

--- components/src/Toggle/Toggle.story.js
+++ components/src/Toggle/Toggle.story.js
@@ -4,61 +4,61 @@ import Toggle from "./Toggle";
 import Field from "../Field/Field";
 
 const onToggleDemo = (toggled, id) => {
-  const message = "Toggle id: (" + id + ") has been turned " + (toggled ? "on!" : "off!");
+  const message = `Toggle id: (${id}) has been turned ${toggled ? "on!" : "off!"}`;
   console.log(message);
 };
 
 storiesOf("Toggle", module)
   .add("Toggle", () => (
-    <Toggle/>
+    <Toggle />
   ))
   .add("Toggle set to disabled", () => (
-    <Toggle disabled/>
+    <Toggle disabled />
   ))
   .add("With text", () => (
-    <Toggle 
-      onText = "on"
-      offText = "off"
+    <Toggle
+      onText="on"
+      offText="off"
     />
   ))
   .add("Toggle Field", () => (
     <Field
-      labelText = "Setting"
-      helpText = "Turns setting on/off"
+      labelText="Setting"
+      helpText="Turns setting on/off"
     >
       <Toggle
-        onText = "on"
-        offText = "off"
+        onText="on"
+        offText="off"
       />
     </Field>
   ))
   .add("<TEMP> Toggle demo", () => (
-    <React.Fragment> 
-      <Toggle 
-        id = "toggle1" 
-        onToggle = {onToggleDemo}
-        onText = "on"
-        offText = "off"
+    <React.Fragment>
+      <Toggle
+        id="toggle1"
+        onToggle={ onToggleDemo }
+        onText="on"
+        offText="off"
       />
-      <Toggle 
-        id = "toggle2" 
-        onToggle = {onToggleDemo}
-        onText = "on"
-        offText = "off"
-        toggled = {true}
-      />      
-      <Toggle 
-        id = "toggle3" 
-        onToggle = {onToggleDemo}
-        onText = "on"
-        offText = "off"
+      <Toggle
+        id="toggle2"
+        onToggle={ onToggleDemo }
+        onText="on"
+        offText="off"
+        toggled
       />
-      <Toggle 
+      <Toggle
+        id="toggle3"
+        onToggle={ onToggleDemo }
+        onText="on"
+        offText="off"
+      />
+      <Toggle
         disabled
-        id = "toggle3" 
-        onToggle = {onToggleDemo}
-        onText = "on"
-        offText = "off"
+        id="toggle3"
+        onToggle={ onToggleDemo }
+        onText="on"
+        offText="off"
       />
     </React.Fragment>
   ));
