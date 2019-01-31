99e99cc1a620ab3b6c3f5661404b691c23810e82
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-01-31 14:32:47 -0500

    refactor of Toggle/input interaction

--- components/src/Toggle/Toggle.js
+++ components/src/Toggle/Toggle.js
@@ -78,6 +78,8 @@ class Toggle extends React.Component {
     this.setState({
       toggled: toggled,
     })
+    console.log("input checked : " + e.target.checked)
+    console.log("React toggled : " + toggled)
   }
 
   handleFocus() {
@@ -102,6 +104,7 @@ class Toggle extends React.Component {
       toggled,
       focused,
     } = this.state;
+    console.log(id + " : " + toggled)
     const fill = disabled ? theme.colors.grey : theme.colors.darkBlue;
     return (
       <Flex
@@ -133,7 +136,7 @@ class Toggle extends React.Component {
           value={ value }
           checked={ toggled }
           ref={ ref => { this.input = ref; } }
-          onChange={ this.handleClick }
+          onChange={ e => { this.handleClick(e); } }
           onFocus={ this.handleFocus }
           onBlur={ this.handleBlur }
           { ...props }
