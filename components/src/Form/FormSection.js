2d9506fd1959790e16a6789e0e32ecdefa5431c5
nikola-nulogy <nikolap@nulogy.com>
2019-01-31 11:24:36 -0500

    Adds form section component

---
+++ components/src/Form/FormSection.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import theme from "../theme";
+import { SubsectionTitle } from "../Type/Headings";
+import Box from "../Box/Box";
+import Field from "../Field/Field";
+
+const FormSectionTitle = SubsectionTitle.withComponent("legend");
+
+const BaseFormSection = ({
+  title,
+  children,
+  ...props,
+}) => (
+  <fieldset { ...props }>
+    <FormSectionTitle>{ title }</FormSectionTitle>
+    { children }
+  </fieldset>
+);
+
+const FormSection = styled(BaseFormSection)`
+  padding: 0;
+  margin: 0;
+  border: none;
+  ${FormSectionTitle} {
+    padding:0;
+    margin-bottom: ${props => (props.title ? theme.space[4] : '0')};
+  }
+  ${Field} {
+    margin-bottom: ${theme.space[4]};
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+FormSection.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+FormSection.defaultProps = {
+  children: [],
+};
+
+export default FormSection;
