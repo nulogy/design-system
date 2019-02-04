5eaa3005fd1523be81d17ff5398ca54781e23aaf
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-02-04 14:54:39 -0500

    removes optional elements from DOM when not included

--- components/src/Field/Field.js
+++ components/src/Field/Field.js
@@ -42,9 +42,9 @@ const BaseField = ({
   >
     <Box mb={ 2 }>
       <Label { ...labelProps }>{labelText}</Label>
-      <RequirementText>{requirementText}</RequirementText>
-      <HelpText>{helpText}</HelpText>
-      <FormatText>{formatText}</FormatText>
+      {requirementText && (<RequirementText>{requirementText}</RequirementText>)}
+      {helpText && (<HelpText>{helpText}</HelpText>)}
+      {formatText && (<FormatText>{formatText}</FormatText>)}
     </Box>
     {children}
   </FieldWrapper>
