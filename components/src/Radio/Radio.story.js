54e822219a3b728723e75aaba2a6034d570f6898
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-02-05 13:42:56 -0500

    refactor radio input

--- components/src/Radio/Radio.story.js
+++ components/src/Radio/Radio.story.js
@@ -3,12 +3,33 @@ import { storiesOf } from "@storybook/react";
 import Radio from "./Radio";
 
 storiesOf("Radio", module)
-.add("Radio", () => (
-  <Radio label="Boo" />
-))
-.add("Radio checked", () => (
-    <Radio checked label="Boo" />
+  .add("Radio", () => (
+    <>
+      <Radio labelText="I am a radio button" />
+      <input type="radio"/>
+    </>
+  ))
+  .add("Set to defaultChecked", () => (
+    <>
+      <Radio defaultChecked labelText="I am checked by default" />
+      <input defaultChecked type="radio"/>
+    </>
   ))
   .add("Set to disabled", () => (
-    <Radio label="Hoo" disabled />
+    <>
+      <Radio disabled labelText="I am disabled" />
+      <input disabled type="radio"/>
+      <br />
+      <Radio checked disabled labelText="I am disabled" />
+      <input checked disabled type="radio"/>
+    </>
+  ))
+  .add("Controlled", () => (
+    <>
+      <Radio checked labelText="I am controlled and checked" />
+      <input checked type="radio"/>
+      <br />
+      <Radio checked={ false } labelText="I am controlled and unchecked" />
+      <input checked={ false } type="radio"/>
+    </>
   ));
