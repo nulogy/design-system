65a64800778ae88dc600477920a9bcc6b3263d92
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-02-06 11:49:00 -0500

    initial commit for RadioGroup

---
+++ components/src/Radio/RadioGroup.story.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Radio from "./Radio";
+import RadioGroup from "./RadioGroup";
+import Field from "../Field/Field";
+
+storiesOf("RadioGroup", module)
+  .add("Radio Group", () => (
+    <RadioGroup name="settingSelection" defaultValue="a">
+      <Radio value="a" labelText="Option A"/>
+      <Radio value="b" labelText="Option B"/>
+      <Radio value="c" labelText="Option C"/>
+    </RadioGroup>
+  ))
+  .add("Radio Group Field", () => (
+    <Field labelText="Setting Selection" helpText="Select a setting from the menu below:">
+      <RadioGroup name="settingSelection" defaultValue="a">
+        <Radio value="a" labelText="Option A"/>
+        <Radio value="b" labelText="Option B"/>
+        <Radio value="c" labelText="Option C"/>
+      </RadioGroup>
+    </Field>
+  ))
+  .add("Set to disabled", () => (
+    <RadioGroup disabled name="settingSelection" defaultValue="a">
+      <Radio value="a" labelText="Option A"/>
+      <Radio value="b" labelText="Option B"/>
+      <Radio value="c" labelText="Option C"/>
+    </RadioGroup>
+  ))
+  .add("Controlled", () => (
+    <RadioGroup name="settingSelection" checkedValue="a" onChange={(e)=>{console.log(e.target.value)}}>
+      <Radio value="a" labelText="Option A"/>
+      <Radio value="b" labelText="Option B"/>
+      <Radio value="c" labelText="Option C"/>
+    </RadioGroup>
+  ));
