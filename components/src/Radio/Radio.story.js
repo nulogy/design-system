c637d42f901535ced45c7016d65560ed4a1ed106
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-02-06 09:54:12 -0500

    cleans up stories for merge

--- components/src/Radio/Radio.story.js
+++ components/src/Radio/Radio.story.js
@@ -6,35 +6,38 @@ storiesOf("Radio", module)
   .add("Radio", () => (
     <>
       <Radio labelText="I am a radio button" />
-      <input type="radio" value="abc" name="abc" id="abc"/>  
     </>
   ))
   .add("Set to defaultChecked", () => (
     <>
       <Radio defaultChecked labelText="I am checked by default" />
-      <input defaultChecked type="radio"/>
     </>
   ))
   .add("Set to disabled", () => (
     <>
       <Radio disabled labelText="I am disabled" />
-      <input disabled type="radio"/>
       <Radio checked disabled labelText="I am disabled" />
-      <input checked disabled type="radio"/>
     </>
   ))
   .add("Controlled", () => (
     <>
-      <Radio checked labelText="I am controlled and checked" />
-      <input checked type="radio"/>
-      <Radio checked={ false } labelText="I am controlled and unchecked" />
-      <input checked={ false } type="radio"/>
+      <Radio checked onChange={ () => {} } labelText="I am controlled and checked" />
+      <Radio checked={ false } onChange={ () => {} } labelText="I am controlled and unchecked" />
     </>
   ))
   .add("In Radio Group (TEMP EXAMPLE)", () => (
-    <div role ="radiogroup">
-      <Radio defaultChecked type="radio" id="Option 1" name="example" labelText="Option 1"/>
-      <Radio type="radio" id="Option 2" name="example" labelText="Option 2" />
-      <Radio type="radio" id="Option 3" name="example" labelText="Option 3" />
+    <div role="radiogroup">
+      <Radio
+        defaultChecked type="radio" id="Option 1"
+        name="example" labelText="Option 1"
+      />
+      <Radio
+        type="radio" id="Option 2" name="example"
+        labelText="Option 2"
+      />
+      <Radio
+        type="radio" id="Option 3" name="example"
+        labelText="Option 3"
+      />
     </div>
   ));
