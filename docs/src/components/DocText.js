ee1c8fbe0739f6178393737f4de0a11817596d97
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-02-12 11:39:33 -0500

    DocText component fix for Docs

---
+++ docs/src/components/DocText.js
@@ -0,0 +1,8 @@
+import { Text } from '@nulogy/components'
+import React from 'react'
+
+const DocText = ({children, ...props}) => (
+  <Text mb={4} {...props}>{children}</Text>
+)
+
+export default DocText;
