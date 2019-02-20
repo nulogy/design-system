c19999e494da87c2463fe01c8582ecead8980c92
Matt Dunn <mattd@nulogy.com>
2019-02-20 14:14:30 -0500

    adds toggleable grid

--- components/.storybook/config.js
+++ components/.storybook/config.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import NDSProvider from '../src/NDSProvider/NDSProvider';
 import { configure, addDecorator } from '@storybook/react';
-import theme from '../src/theme';
-import '../../css/src/nds-dev.css';
+import { withBackgrounds } from '@storybook/addon-backgrounds';
 import withStyles from "@sambego/storybook-styles";
+import NDSProvider from '../src/NDSProvider/NDSProvider';
+import theme from '../src/theme';
+
+const grid = {
+  base: { colour: 'hsla(120, 100%, 100%, 0.4)', size: `${theme.space[2]}` },
+  major: { colour: 'hsla(120, 100%, 100%, 0.5)', size: `${theme.space[4]}`},
+}
+
+addDecorator(withStyles({
+  backgroundImage: `
+    linear-gradient(${grid.major.colour} 1px, transparent 1px),
+    linear-gradient(90deg, ${grid.major.colour} 1px, transparent 1px),
+    linear-gradient(${grid.base.colour} 1px, transparent 1px),
+    linear-gradient(90deg, ${grid.base.colour} 1px, transparent 1px)`,
+  backgroundSize: `
+    ${grid.major.size} ${grid.major.size},
+    ${grid.major.size} ${grid.major.size},
+    ${grid.base.size} ${grid.base.size},
+    ${grid.base.size} ${grid.base.size}
+  `,
+  backgroundPosition: '-1px -1px',
+  borderTop: '1px solid red',
+  marginTop: '-1px',
+  padding: `${grid.major.size}`,
+  minHeight: `calc(100vh - ${grid.major.size * 2})`,
+}));
+
+addDecorator(withBackgrounds([
+  { name: 'none', value: '', default: true },
+  { name: 'grid', value: `${theme.colors.lightGrey}` },
+]));
 
 const req = require.context('../src', true, /\.story\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
-
-addDecorator(withStyles({
-  backgroundSize: '24px 24px',
-  backgroundImage: 'linear-gradient(to right, transparent 7px, #F0F2F5 8px, transparent 9px, transparent 14px, #F0F2F5 15px, transparent 16px, transparent 23px, #C0C8D1 24px), linear-gradient(to bottom, transparent 7px, #F0F2F5 8px, transparent 9px, transparent 15px, #F0F2F5 16px, transparent 16px, transparent 23px, #C0C8D1 24px)',
-  minHeight: 'calc(100vh - 32px)',
-}));
 
 addDecorator((story) => (
   <NDSProvider theme={theme}>
