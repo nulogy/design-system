0b67467c54cc21d3cbb9e95142b8ef41c27b25ba
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-02-22 10:14:59 -0500

    updates dependencies for Checkbox

--- components/src/Checkbox/Checkbox.story.js
+++ components/src/Checkbox/Checkbox.story.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Checkbox from "./Checkbox";
+import { Checkbox } from "../index";
 
 storiesOf("Checkbox", module)
   .add("Checkbox", () => (
