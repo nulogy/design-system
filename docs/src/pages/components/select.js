c198125c32e17f370e942fe5f2802854393d305a
Matt Dunn <mattd@nulogy.com>
2019-02-28 14:51:14 -0500

    updates select to include code samples

--- docs/src/pages/components/select.js
+++ docs/src/pages/components/select.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import {
-  Box, SectionTitle, SubsectionTitle, Title,
+  Box, SectionTitle, SubsectionTitle, Title, Select, Link,
 } from "@nulogy/components";
+import Highlight from "react-highlight";
 import {
-  Layout, Intro, DocSection, CheckList, Image,
+  Layout, Intro, DocSection, CheckList,
 } from "../../components";
-import anatomy from "../../images/select/select-anatomy.png";
-import basic from "../../images/select/select-basic.png";
-import defaultState from "../../images/select/select-state-default.png";
-import selected from "../../images/select/select-state-selected.png";
-import disabled from "../../images/select/select-disabled.png";
+
+const options = [
+  { value: "accepted", label: "Accepted" },
+  { value: "assigned", label: "Assigned to a line" },
+];
 
 export default () => (
   <Layout>
@@ -18,30 +19,94 @@ export default () => (
       <title>Select</title>
     </Helmet>
     <Box
-      bg="whiteGrey" p="x4" borderRadius={ 1 }
+      bg="" mt="x2"
       mb="x6"
     >
       <Title mb="none">Select</Title>
       <Intro>For making one selection from a large list of options.</Intro>
     </Box>
     <DocSection>
-      <Image src={ basic } width="75%" alt="Text input screenshot" />
-      <SubsectionTitle>Use when</SubsectionTitle>
+
+      <Select placeholder="Please select inventory status" options={ options } />
+      <Highlight className="js">
+        {`import {Select} from @nulogy-components;
+
+const options = [
+  { value: "accepted", label: "Accepted" },
+  { value: "assigned", label: "Assigned to a line" },
+];
+
+<Select placeholder="Please select inventory status" options={ options } />`
+}
+      </Highlight>
+    </DocSection>
+    <DocSection>
+      <SectionTitle>Use when</SectionTitle>
       <CheckList>Users need to choose a single option from a list of mutually exclusive options.</CheckList>
       <CheckList>There is a large data set that would be impractical for radio buttons or a toggle.</CheckList>
     </DocSection>
     <DocSection>
-      <SectionTitle>Anatomy</SectionTitle>
-      <Image src={ anatomy } width="50%" alt="Text input screenshot" />
+      <SectionTitle>Variations</SectionTitle>
+      <SubsectionTitle>Disabled</SubsectionTitle>
+
+      <Box mb="x3">
+        <Select placeholder="Please select inventory status" options={ options } disabled />
+        <Highlight className="jsx">
+          {"<Select placeholder=\"Please select inventory status\" options={ options } disabled />"}
+        </Highlight>
+      </Box>
+
+      <SubsectionTitle>Error</SubsectionTitle>
+
+      <Select placeholder="Please select inventory status" options={ options } error />
+      <Highlight className="jsx">
+        {"<Select placeholder=\"Please select inventory status\" options={ options } error />"}
+      </Highlight>
+
+    </DocSection>
+
+    <DocSection>
+      <SectionTitle>Props</SectionTitle>
+      <table>
+        <thead>
+          <tr>
+            <td>Prop</td>
+            <td>Type</td>
+            <td>Default value</td>
+            <td>Description</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Placeholder</td>
+            <td>String</td>
+            <td>""</td>
+            <td>A description of what the Select box contains</td>
+          </tr>
+          <tr>
+            <td>Disabled</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Marks the field as disabled and disallows user input</td>
+          </tr>
+          <tr>
+            <td>Error</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Marks the field as invalid and adds a red border</td>
+          </tr>
+          <tr>
+            <td>Required</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Makes the field require input before the form will submit</td>
+          </tr>
+        </tbody>
+      </table>
     </DocSection>
     <DocSection>
-      <SectionTitle>States</SectionTitle>
-      <SubsectionTitle>Default</SubsectionTitle>
-      <Image src={ defaultState } width="50%" alt="Text input screenshot" />
-      <SubsectionTitle>With input</SubsectionTitle>
-      <Image src={ selected } width="50%" alt="Text input screenshot" />
-      <SubsectionTitle>Disabled</SubsectionTitle>
-      <Image src={ disabled } width="50%" alt="Text input screenshot" />
+      <SectionTitle>Resources</SectionTitle>
+      <Link href="https://storybook.nulogy.design/">Storybook</Link> <br />
     </DocSection>
   </Layout>
 );
