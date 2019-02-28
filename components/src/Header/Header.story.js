1cba2d9abf3a82a7ea7d9200edadbf3ef0b6f9bb
nikola-nulogy <nikolap@nulogy.com>
2019-02-28 10:02:00 -0500

    Header component basic setup

---
+++ components/src/Header/Header.story.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Header } from "ComponentsRoot";
+
+storiesOf("Header", module)
+  .add("Header", () => (
+    <Header />
+  ));
