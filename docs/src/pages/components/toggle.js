cd7d9a3b4dfc5633a3d3df4336417407a37c5f15
Matt Dunn <mattd@nulogy.com>
2019-03-05 07:48:02 -0500

    adds new content to toggle docs

--- docs/src/pages/components/toggle.js
+++ docs/src/pages/components/toggle.js
@@ -1,52 +1,89 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import Highlight from "react-highlight";
 import {
-  Box, Flex, SectionTitle, SubsectionTitle, Title,
+  Box, SectionTitle, SubsectionTitle, Title, Toggle, Link,
 } from "@nulogy/components";
 import {
-  DocText as Text, Layout, Intro, DocSection, Image,
+  DocText as Text, CheckList, Layout, Intro, DocSection,
 } from "../../components";
-import anatomy from "../../images/toggle/anatomy.png";
-import on from "../../images/toggle/on.png";
-import off from "../../images/toggle/off.png";
-import onDisabled from "../../images/toggle/on-disabled.png";
-import offDisabled from "../../images/toggle/off-disabled.png";
 
 export default () => (
   <Layout>
     <Helmet>
       <title>Toggle</title>
     </Helmet>
-    <Box
-      bg="whiteGrey" p="x4" borderRadius={ 1 }
-      mb="x6"
-    >
+    <Box mt="x2" mb="x6">
       <Title mb="none">Toggle</Title>
       <Intro>For quickly switching between two possible states.</Intro>
     </Box>
     <DocSection>
-      <Image src={ on } width="75%" alt="Toggle screenshot" />
+      <Toggle onText="On" offText="Off" />
+      <Highlight className="jsx">
+          {`import { Toggle } from @nulogy/components;
+
+<Toggle onText="On" offText="Off" />`}
+      </Highlight>
     </DocSection>
     <DocSection>
-      <SectionTitle>Anatomy</SectionTitle>
-      <Image src={ anatomy } alt="Toggle screenshot" />
+      <SectionTitle>Variations</SectionTitle>
+      <Box mb="x6">
+        <SubsectionTitle>Disabled</SubsectionTitle>
+        <Toggle onText="On" offText="Off" disabled />
+        <Highlight className="jsx">
+          {`<Toggle onText="On" offText="Off" disabled />`}
+        </Highlight>
+      </Box>
+      <Box mb="x6">
+        <SubsectionTitle>Toggled by default</SubsectionTitle>
+        <Toggle onText="On" offText="Off" defaultToggled="true" />
+        <Highlight className="jsx">
+          {`<Toggle onText="On" offText="Off" defaultToggled="true" />`}
+        </Highlight>
+      </Box>
     </DocSection>
     <DocSection>
-      <SectionTitle>Guidelines</SectionTitle>
-      <Text>Always use the toggle value label to clearly communicate the selected state.</Text>
+      <SectionTitle>Props</SectionTitle>
+      <table>
+        <thead>
+          <tr>
+            <td>Prop</td>
+            <td>Type</td>
+            <td>Default</td>
+            <td>Description</td>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>defaultToggled</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Display the toggle as checked by default.</td>
+          </tr>
+          <tr>
+            <td>disabled</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Marks the toggle as disabled and disallows user input.</td>
+          </tr>
+          <tr>
+            <td>offText</td>
+            <td>String</td>
+            <td>null</td>
+            <td>A label for the toggle's off state.</td>
+          </tr>
+          <tr>
+            <td>onText</td>
+            <td>String</td>
+            <td>null</td>
+            <td>A label for the toggle's on state.</td>
+          </tr>
+        </tbody>
+      </table>
     </DocSection>
     <DocSection>
-      <SectionTitle>States</SectionTitle>
-      <SubsectionTitle>Default</SubsectionTitle>
-      <Flex mb="x1">
-        <Box><Image src={ on } alt="Toggle screenshot" /></Box>
-        <Box><Image src={ off } alt="Toggle screenshot" /></Box>
-      </Flex>
-      <SubsectionTitle>Disabled</SubsectionTitle>
-      <Flex>
-        <Box><Image src={ onDisabled } alt="Toggle screenshot" /></Box>
-        <Box><Image src={ offDisabled } alt="Toggle screenshot" /></Box>
-      </Flex>
+      <SectionTitle>Resources</SectionTitle>
+      <Link href="https://storybook.nulogy.design/?selectedKind=Toggle">View in Storybook</Link>
     </DocSection>
   </Layout>
 );
