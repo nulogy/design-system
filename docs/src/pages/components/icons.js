ad32f1508a7abe9bf204f46c2692b3592f2d20a4
Matt Dunn <mattd@nulogy.com>
2019-03-05 07:49:19 -0500

    adds new content to icons docs

--- docs/src/pages/components/icons.js
+++ docs/src/pages/components/icons.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import Highlight from "react-highlight";
 import {
   Icon, Box, Flex, SectionTitle, Title, Link,
 } from "@nulogy/components";
@@ -26,13 +27,19 @@ export default () => (
     <Helmet>
       <title>Icons</title>
     </Helmet>
-    <Box
-      bg="whiteGrey" p="x4" borderRadius={ 1 }
-      mb="x6"
-    >
-      <Title m="none">Icons</Title>
+    <Box mt="x2" mb="x6">
+      <Title mb="none">Icons</Title>
       <Intro>Icons can be used alongside text to help assist users in finding certain actions on a page. </Intro>
     </Box>
+
+    <DocSection>
+    <Icon icon="user" />
+    <Highlight className="jsx">
+          {`import { Icon } from @nulogy-components;
+
+<Icon icon="user" />`}
+      </Highlight>
+    </DocSection>
 
     <DocSection>
       <SectionTitle>Avaliable Icons</SectionTitle>
@@ -45,8 +52,50 @@ export default () => (
       <SectionTitle>Guidelines</SectionTitle>
       <CheckList>Avoid using icons without labels unless the meaning is ubiquitous, like print</CheckList>
     </DocSection>
+
     <DocSection>
-      <SectionTitle>Related</SectionTitle>
+      <SectionTitle>Props</SectionTitle>
+      <table>
+        <thead>
+          <tr>
+            <td>Prop</td>
+            <td>Type</td>
+            <td>Default</td>
+            <td>Description</td>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>color</td>
+            <td>String</td>
+            <td>currentcolor</td>
+            <td>The icon's colour, if different than parent's text colour.</td>
+          </tr>
+          <tr>
+            <td>icon</td>
+            <td>String</td>
+            <td><em>Required</em></td>
+            <td>The icon you'd like to display. Accepts icons listed in <em>Available Icons</em> above.</td>
+          </tr>
+          <tr>
+            <td>size</td>
+            <td>Number</td>
+            <td>24px</td>
+            <td>Size of the icon.</td>
+          </tr>
+          <tr>
+            <td>title</td>
+            <td>String</td>
+            <td>null</td>
+            <td>Alternative text to be read by assistive devices. Leave blank if icon is purely decorative.</td>
+          </tr>
+        </tbody>
+      </table>
+    </DocSection>
+
+    <DocSection>
+      <SectionTitle>Resources</SectionTitle>
+      <Link href="https://storybook.nulogy.design/?selectedKind=Icon">View in Storybook</Link>
       <Text>For more information on icon usability and recognition, see this study from Nielsen Norman Group: <Link href="https://www.nngroup.com/articles/icon-usability/">Icon Usability.</Link></Text>
     </DocSection>
   </Layout>
