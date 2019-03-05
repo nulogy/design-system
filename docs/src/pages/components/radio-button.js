970f11b0c51347717283cc5d3b95f98f07cd11f5
Matt Dunn <mattd@nulogy.com>
2019-03-05 07:46:23 -0500

    adds new content to radio button docs

--- docs/src/pages/components/radio-button.js
+++ docs/src/pages/components/radio-button.js
@@ -1,39 +1,58 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import {
-  Box, Link, SectionTitle, SubsectionTitle, Title,
+  Box, Link, SectionTitle, SubsectionTitle, Title, Radio
 } from "@nulogy/components";
+import Highlight from "react-highlight";
 import {
-  Layout, Intro, DocSection, CheckList, Image,
+  Layout, Intro, DocSection, CheckList,
 } from "../../components";
-import anatomy from "../../images/radio-button/radio-button-anatomy.png";
-import basic from "../../images/radio-button/radio-button-basic.png";
-import selected from "../../images/radio-button/radio-button-selected.png";
-import unselected from "../../images/radio-button/radio-button-unselected.png";
-import disabled from "../../images/radio-button/radio-button-disabled.png";
-
 
 export default () => (
   <Layout>
     <Helmet>
       <title>Radio button</title>
     </Helmet>
-    <Box
-      bg="whiteGrey" p="x4" borderRadius={ 1 }
-      mb="x6"
-    >
+    <Box mt="x2" mb="x6">
       <Title mb="none">Radio button</Title>
       <Intro>Radio buttons allow one selection from a group of options</Intro>
     </Box>
     <DocSection>
-      <Image src={ basic } width="75%" alt="Text input screenshot" />
-      <SubsectionTitle>Use when</SubsectionTitle>
+      <Radio labelText="Radio button" />
+      <Highlight className="jsx">
+          {`import { Radio } from @nulogy-components;
+
+<Radio labelText="Radio button" />`}
+      </Highlight>
+    </DocSection>
+      <DocSection>
+      <SectionTitle>Use when</SectionTitle>
       <CheckList>Users need to make a single choice from a set of mutually exclusive options</CheckList>
       <CheckList>In place of a dropdown when it would be beneficial to see all items up front</CheckList>
     </DocSection>
     <DocSection>
-      <SectionTitle>Anatomy</SectionTitle>
-      <Image src={ anatomy } alt="Radio button anatomy" />
+      <SectionTitle>Variations</SectionTitle>
+      <Box mb="x6">
+        <SubsectionTitle>Disabled</SubsectionTitle>
+        <Radio labelText="Radio button" disabled />
+        <Highlight className="jsx">
+          {`<Radio labelText="Radio button" disabled />`}
+        </Highlight>
+      </Box>
+      <Box mb="x6">
+        <SubsectionTitle>Error</SubsectionTitle>
+        <Radio labelText="Radio button" error />
+        <Highlight className="jsx">
+          {`<Radio labelText="Radio button" error />`}
+        </Highlight>
+      </Box>
+      <Box>
+        <SubsectionTitle>Checked</SubsectionTitle>
+        <Radio labelText="Radio button" defaultChecked="true"/>
+        <Highlight className="jsx">
+          {`<Radio labelText="Radio button" defaultChecked="true" />`}
+        </Highlight>
+      </Box>
     </DocSection>
     <DocSection>
       <SectionTitle>Guidelines</SectionTitle>
@@ -42,13 +61,53 @@ export default () => (
       <CheckList>Users should be able to select the radio button by clicking on the box directly or by clicking on its label.</CheckList>
     </DocSection>
     <DocSection>
-      <SectionTitle>States</SectionTitle>
-      <SubsectionTitle>Default</SubsectionTitle>
-      <Image src={ unselected } width="50%" alt="Text input screenshot" />
-      <SubsectionTitle>Selected</SubsectionTitle>
-      <Image src={ selected } width="50%" alt="Text input screenshot" />
-      <SubsectionTitle>Disabled</SubsectionTitle>
-      <Image src={ disabled } width="50%" alt="Text input screenshot" />
+      <SectionTitle>Props</SectionTitle>
+      <table>
+        <thead>
+          <tr>
+            <td>Prop</td>
+            <td>Type</td>
+            <td>Default</td>
+            <td>Description</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>defaultChecked</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Makes the field checked by default</td>
+          </tr>
+          <tr>
+            <td>disabled</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Marks the field as disabled and disallows user input</td>
+          </tr>
+          <tr>
+            <td>error</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Marks the field as invalid and turns red</td>
+          </tr>
+          <tr>
+            <td>labelText</td>
+            <td>String</td>
+            <td><em>Required</em></td>
+            <td>A label for your option</td>
+          </tr>
+          <tr>
+            <td>required</td>
+            <td>Boolean</td>
+            <td>false</td>
+            <td>Makes the field require selection before the form will submit</td>
+          </tr>
+        </tbody>
+      </table>
+    </DocSection>
+    <DocSection>
+      <SectionTitle>Resources</SectionTitle>
+      <Link href="https://storybook.nulogy.design/?selectedKind=Radio">View in storybook</Link>
     </DocSection>
   </Layout>
 );
