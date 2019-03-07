010880b0a442b874525fe580710590fffd2a4ee6
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-03-07 15:09:30 -0500

    cleanup changes

--- components/src/NavBar/MenuItem/MenuItem.js
+++ components/src/NavBar/MenuItem/MenuItem.js
@@ -2,93 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { Manager, Reference, Popper } from "react-popper";
-import theme from "../theme";
+import theme from "ComponentsRoot/theme";
 import SubMenuItemList from "./SubMenuItemList";
-
-const subMenuStyles = {
-  backgroundColor: theme.colors.whiteGrey,
-  borderColor: theme.colors.whiteGrey,
-  textColor: theme.colors.black,
-};
-
-const BaseSubMenu = ({
-  popperProps: {
-    ref,
-    style,
-    placement,
-    arrowProps,
-  },
-  children,
-  ...props
-}) => (
-  <div ref={ ref } style={ style } placement={ placement } { ...props }>
-    <Arrow { ...arrowProps } />
-    {children}
-  </div>
-);
-
-BaseSubMenu.propTypes = {
-  children: PropTypes.node,
-  popperProps: PropTypes.shape({}),
-};
-
-BaseSubMenu.defaultProps = {
-  children: null,
-  popperProps: null,
-};
-
-const SubMenu = styled(BaseSubMenu)({
-  color: subMenuStyles.textColor,
-  display: "flex",
-  flexDirection: "column",
-  fontSize: theme.fontSizes.small,
-  backgroundColor: subMenuStyles.backgroundColor,
-  borderRadius: theme.radii.medium,
-  borderTop: `1px solid ${subMenuStyles.borderColor}`,
-  boxShadow: theme.boxShadows,
-  padding: `${theme.space.x1} 0`,
-  transition: "opacity 0.3s",
-  zIndex: "999999",
-  marginTop: theme.space.half,
-},
-({ style }) => ({
-  ...style,
-}));
-
-const Arrow = styled.div({
-  height: theme.space.x1,
-  position: "absolute",
-  width: theme.space.x1,
-  margin: "12px",
-  left: 0,
-  top: 0,
-  marginTop: "-7px",
-  "&:before": {
-    borderStyle: "solid",
-    borderColor: `transparent transparent ${subMenuStyles.borderColor} transparent`,
-    borderWidth: `0 ${theme.space.x1} ${theme.space.x1} ${theme.space.x1}`,
-    content: "''",
-    display: "block",
-    height: 0,
-    margin: "auto",
-    position: "absolute",
-    width: 0,
-    top: "-2px",
-    left: `-${theme.space.half}`,
-  },
-  "&:after": {
-    borderStyle: "solid",
-    borderColor: `transparent transparent ${subMenuStyles.backgroundColor} transparent`,
-    borderWidth: `0 ${theme.space.x1} ${theme.space.x1} ${theme.space.x1}`,
-    content: "''",
-    display: "block",
-    height: 0,
-    margin: "auto",
-    position: "absolute",
-    width: 0,
-    left: `-${theme.space.half}`,
-  },
-});
+import SubMenu from "./SubMenu";
 
 const MenuItemButton = styled.button({
   display: "inline-flex",
@@ -263,10 +179,8 @@ class MenuItem extends React.Component {
         </Reference>
         {this.state.subMenuOpen && (
         <Popper placement="bottom-start">
-          {subMenuProps => (
-            <SubMenu
-              popperProps={ subMenuProps } { ...this.subMenuEventHandlers() }
-            >
+          {popperProps => (
+            <SubMenu popperProps={ popperProps } { ...this.subMenuEventHandlers() }>
               <SubMenuItemList focusIndex={ this.state.focusIndex }>
                 {this.props.children}
               </SubMenuItemList>
