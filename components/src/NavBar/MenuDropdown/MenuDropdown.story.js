5248ff4ca0104ba300a1ef61a448c8fbf1ca2400
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-03-08 11:05:46 -0500

    fixes errors when renaming

--- components/src/NavBar/MenuDropdown/MenuDropdown.story.js
+++ components/src/NavBar/MenuDropdown/MenuDropdown.story.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Flex } from "ComponentsRoot";
-import { MenuDropdown } from "./MenuDropdown";
+import MenuDropdown from "./MenuDropdown";
 import SubMenuItem from "../SubMenuItem";
 
 storiesOf("MenuDropdown", module)
