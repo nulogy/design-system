9d0efb5796a0d9895c6aa3ac0c358204df233a42
nikola-nulogy <nikolap@nulogy.com>
2019-03-11 12:55:50 -0400

    Adds Subsection component to docs

---
+++ docs/src/components/DocSubsection.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import { Flex } from "@nulogy/components";
+import styled from "styled-components";
+import { color, space } from "styled-system";
+
+const DocSubsection = styled(Flex)({
+  flexDirection: "column",
+  ":last-child": {
+    marginBottom: 0,
+  },
+},
+color,
+space);
+
+DocSubsection.PropTypes = {
+  color: PropTypes.string,
+  space: PropTypes.number,
+};
+
+DocSubsection.defaultProps = {
+  mt: "x3",
+};
+
+export default DocSubsection;
