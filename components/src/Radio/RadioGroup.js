0c2b81575554a8c4fbb1008c72408817fb8333b6
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-03-12 12:06:33 -0400

    changes Radio Group style to use legend and accept helpText and requirementText

--- components/src/Radio/RadioGroup.js
+++ components/src/Radio/RadioGroup.js
@@ -2,11 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Radio } from "ComponentsRoot";
+import theme from "../theme";
+import HelpText from "../Field/HelpText";
+import RequirementText from "../Field/RequirementText";
+
 
 const getRadioButtons = props => {
   const radioButtons = React.Children.map(props.children, radio => {
     const {
-
       value,
       disabled,
       ...radioProps
@@ -26,23 +29,53 @@ const getRadioButtons = props => {
   return (radioButtons);
 };
 
+const internalSpacingStyles = (hasHelpText) => {
+  if (hasHelpText){
+    return ({
+      "p":{
+        marginBottom: theme.space.x1,
+      },
+    })
+  } else {
+    return ({
+      "legend": {
+        marginBottom: theme.space.x1,
+      },
+    })
+  } 
+}
+
 const Fieldset = styled.fieldset({
   padding: 0,
   border: 0,
   margin: 0,
-});
+  "legend": {
+    padding: 0,
+  },
+},
+({hasHelpText}) => (
+  internalSpacingStyles(hasHelpText)
+),
+
+);
 
 const BaseRadioGroup = ({
   className,
+  labelText,
+  helpText,
+  requirementText,
   ...props
 }) => (
-  <Fieldset role="radiogroup" className={ className }>
-    <legend>Title</legend>
+  <Fieldset role="radiogroup" className={ className } hasHelpText={!!helpText}>
+    <legend>{ labelText }    { requirementText && (<RequirementText>{requirementText}</RequirementText>) }</legend>
+ 
+    { helpText && (<HelpText>{helpText}</HelpText>) }
     { getRadioButtons(props) }
   </Fieldset>
 );
 
 BaseRadioGroup.propTypes = {
+  labelText: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([
     PropTypes.shape({
@@ -58,6 +91,8 @@ BaseRadioGroup.propTypes = {
   checkedValue: PropTypes.string,
   onChange: PropTypes.func,
   className: PropTypes.string,
+  helpText: PropTypes.string,
+  requirementText: PropTypes.string,
 };
 
 BaseRadioGroup.defaultProps = {
@@ -65,6 +100,8 @@ BaseRadioGroup.defaultProps = {
   checkedValue: undefined,
   onChange: undefined,
   className: null,
+  helpText: null,
+  requirementText: null,
 };
 
 const RadioGroup = styled(BaseRadioGroup)({});
--- components/src/Radio/RadioGroup.story.js
+++ components/src/Radio/RadioGroup.story.js
@@ -3,25 +3,36 @@ import { storiesOf } from "@storybook/react";
 import {
   Radio,
   RadioGroup,
-  Field,
 } from "ComponentsRoot";
 
 storiesOf("RadioGroup", module)
   .add("Radio Group", () => (
-    <RadioGroup name="settingSelection" defaultValue="a">
+    <RadioGroup labelText="Setting Selection" name="settingSelection" defaultValue="a">
       <Radio value="a" labelText="Option A" />
       <Radio value="b" labelText="Option B" />
       <Radio value="c" labelText="Option C" />
     </RadioGroup>
   ))
-  .add("Radio Group Field", () => (
-    <Field labelText="Setting Selection" helpText="Select a setting from the menu below:"> {/* eslint-disable-line */}
-      <RadioGroup name="settingSelection" defaultValue="a">
+  .add("with helpText", () => (
+      <RadioGroup labelText="Setting Selection" helpText="Select a setting from the menu below:" name="settingSelection" defaultValue="a">
         <Radio value="a" labelText="Option A" />
         <Radio value="b" labelText="Option B" />
         <Radio value="c" labelText="Option C" />
       </RadioGroup>
-    </Field>
+  ))
+  .add("with requirementText", () => (
+    <RadioGroup labelText="Setting Selection" requirementText="(Required)" name="settingSelection" defaultValue="a">
+      <Radio value="a" labelText="Option A" />
+      <Radio value="b" labelText="Option B" />
+      <Radio value="c" labelText="Option C" />
+    </RadioGroup>
+  ))
+  .add("with additional text", () => (
+    <RadioGroup labelText="Setting Selection" helpText="Select a setting from the menu below:" requirementText="(Required)" name="settingSelection" defaultValue="a">
+      <Radio value="a" labelText="Option A" />
+      <Radio value="b" labelText="Option B" />
+      <Radio value="c" labelText="Option C" />
+    </RadioGroup>
   ))
   .add("Set to disabled", () => (
     <RadioGroup disabled name="settingSelection" defaultValue="a">
