e816a2be620f89448428afc4075004415ffa499e
Matt Dunn <mattd@nulogy.com>
2019-03-18 10:56:23 -0400

    adds default values to input props

--- docs/src/shared/inputProps.js
+++ docs/src/shared/inputProps.js
@@ -3,13 +3,13 @@ const inputProps = [
     name: "disabled", type: "Boolean", defaultValue: "false", description: "Marks the field as disabled and disallows user input",
   },
   {
-    name: "error", type: "String", defaultValue: "", description: "Sets the error state and displays message",
+    name: "error", type: "String", defaultValue: "null", description: "Sets the error state and displays message",
   },
   {
-    name: "helpText", type: "String", defaultValue: "", description: "Additional instruction to show up below label",
+    name: "helpText", type: "String", defaultValue: "null", description: "Additional instruction to show up below label",
   },
   {
-    name: "labelText", type: "String", defaultValue: "", description: "Label for the input",
+    name: "labelText", type: "String", defaultValue: "null", description: "Label for the input",
   },
   {
     name: "placeholder", type: "String", defaultValue: "null", description: "A hint to the expected format for the field. Not a replacement for a label.",
