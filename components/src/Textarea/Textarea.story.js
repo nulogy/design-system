9cc4cdacf5ebb12031c734e27beca6a03ee32166
Matt Dunn <mattd@nulogy.com>
2019-03-18 10:56:44 -0400

    updates textarea documentation

--- components/src/Textarea/Textarea.story.js
+++ components/src/Textarea/Textarea.story.js
@@ -4,20 +4,17 @@ import { Textarea } from "ComponentsRoot";
 
 storiesOf("Textarea", module)
   .add("Textarea", () => (
-    <Textarea id="textarea" labelText="Label" helpText="here's some help..." />
+    <Textarea id="textarea" labelText="Label" />
+  ))
+  .add("Textarea with all props", () => (
+    <Textarea id="textarea" labelText="Label" placeholder="Placeholder" helpText="here's some help..." requirementText="(Required)" required />
   ))
   .add("Set to disabled", () => (
     <Textarea id="textarea" labelText="Label" disabled />
   ))
   .add("Set to error", () => (
-    <Textarea id="textarea" labelText="Label" error />
-  ))
-  .add("Set to required", () => (
-    <Textarea id="textarea" labelText="Label" requirementText="(required)" required />
+    <Textarea id="textarea" labelText="Label" error="Please fill this out" />
   ))
   .add("With custom number of rows", () => (
     <Textarea id="textarea" labelText="Label" rows={ 7 } />
-  ))
-  .add("With placeholder text", () => (
-    <Textarea id="textarea" labelText="Label" placeholder="Month/Day/Year" />
   ));
