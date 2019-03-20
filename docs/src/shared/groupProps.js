1a4d51acc096bd215f1eb0bfed98d40c49bfa165
Matt Dunn <mattd@nulogy.com>
2019-03-20 09:58:24 -0400

    adds props object for checkbox and radio groups

---
+++ docs/src/shared/groupProps.js
@@ -0,0 +1,23 @@
+const groupProps = [
+  {
+    name: "defaultValue", type: "String", defaultValue: "null", description: "A Checkbox value to autoselect on pageload"
+  },
+  {
+    name: "error", type: "String", defaultValue: "null", description: "Sets the error state and displays message",
+  },
+  {
+    name: "helpText", type: "String", defaultValue: "null", description: "Additional instruction to show up below label",
+  },
+  {
+    name: "labelText", type: "String", defaultValue: "Required", description: "Label for the input",
+  },
+  {
+    name: "name", type: "String", defaultValue: "Required", description: "A unique name for this input",
+  },
+  {
+    name: "required", type: "Boolean", defaultValue: "false", description: "Makes the field require selection before the form will submit",
+  },
+  {
+    name: "requirementText", type: "String", defaultValue: "null", description: "(Optional) or (Required)",
+  },
+]
