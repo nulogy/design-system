a7b9fbc2d4d6276dcb89547794bdbe4885dc3908
nikola-nulogy <nikolap@nulogy.com>
2019-03-21 13:36:07 -0400

    Revert "Renames incorectly named const"
    This reverts commit 33b552e32f9754df418630acacc1a56fe7c0ce5e.

--- components/src/NavBar/SubMenuItem.js
+++ components/src/NavBar/SubMenuItem.js
@@ -5,19 +5,6 @@ import Link from "../Link/Link";
 import Text from "../Type/Text";
 import theme from "../theme";
 
-const screen = props => {
-  switch (props.screen) {
-    case "small":
-      return {
-        color: theme.colors.green,
-      };
-    case "medium":
-      return {
-        color: theme.colors.red,
-      };
-  }
-};
-
 const SubMenuItemLink = styled(Link)({
   display: "block",
   padding: `${theme.space.x1} ${theme.space.x2}`,
@@ -33,22 +20,19 @@ const SubMenuItemLink = styled(Link)({
   },
 });
 
-const Description = styled(Text)``
-
 const BaseSubMenuItem = React.forwardRef(({
   href,
   children,
   subText,
-  screen,
   ...props
 }, ref) => (
   <li { ...props }>
-    <SubMenuItemLink ref={ ref } tabIndex="-1" underline={ false } href={ href }>
+    <SubMenuItemLink ref={ ref } tabIndex="-1" color="darkBlue" underline={ false } href={ href }>
       <Text>{children}</Text>
       {subText && (
-      <Description>
+      <Text color="darkGrey" fontSize={ theme.fontSizes.small } lineHeight={ theme.lineHeights.smallTextBase }>
         {subText}
-      </Description>
+      </Text>
       )}
     </SubMenuItemLink>
   </li>
@@ -58,31 +42,15 @@ BaseSubMenuItem.propTypes = {
   children: PropTypes.node.isRequired,
   subText: PropTypes.string,
   href: PropTypes.string,
-  screen: PropTypes.oneOf(["small", "medium"]),
 };
 
 BaseSubMenuItem.defaultProps = {
   subText: undefined,
   href: "/",
-  screen: "medium",
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-const SubMenuItem = styled(BaseSubMenuItem)(
-  screen,
-  ({ small, medium }) => ({
-  //color: theme.colors.black,
+const SubMenuItem = styled(BaseSubMenuItem)({
+  color: theme.colors.black,
   borderColor: "transparent",
   backgroundColor: "transparent",
   justifyContent: "center",
@@ -93,15 +61,6 @@ const SubMenuItem = styled(BaseSubMenuItem)(
   transition: ".2s",
   fontSize: `${theme.fontSizes.medium}`,
   maxWidth: "20em",
-  [`${SubMenuItemLink}`]: {
-    color: theme.colors.darkBlue,
-  },
-  [`${Description}`]: {
-    fontSize: theme.fontSizes.small,
-    lineHeight: theme.lineHeights.smallTextBase,
-    //color: theme.colors.darkGrey,
-  },
-})
-);
+});
 
 export default SubMenuItem;
