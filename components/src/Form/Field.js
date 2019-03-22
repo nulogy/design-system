226305b0a5997871b1e32e0dc7f7f283c83bad92
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-03-22 16:01:33 -0400

    makes field 100% width

--- components/src/Form/Field.js
+++ components/src/Form/Field.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { Box } from "ComponentsRoot";
 
-const Field = styled(Box)({});
+const Field = styled(Box)({
+  width: "100%",
+});
 
 export default Field;
