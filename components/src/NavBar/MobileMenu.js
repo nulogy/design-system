2fcd0fbba99b807c34048664ad350b9f1b7ad250
Matt Dunn <mattd@nulogy.com>
2019-03-25 12:37:32 -0400

    adds marginBottom to mobileMenu menuLink

--- components/src/NavBar/MobileMenu.js
+++ components/src/NavBar/MobileMenu.js
@@ -116,7 +116,7 @@ const Menu = styled(Box)(() => (
       width: "100%",
       justifyContent: "flex-start",
       padding: `0${theme.space.x2} ${theme.space.x4}`,
-      marginBottom: theme.space.x1,
+      marginBottom: theme.space.x4,
       borderRadius: "0",
     },
   }));
