d48c444f7ddbb79bed52c78e4745476e3766f09f
Matt Dunn <mattd@nulogy.com>
2019-03-29 10:19:36 -0400

    removes unused components

--- docs/src/components/Layout.js
+++ docs/src/components/Layout.js
@@ -1,8 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import {
-  Box, Flex, NDSProvider,
+  Box, NDSProvider,
 } from "@nulogy/components";
 import { Helmet } from "react-helmet";
 import { Navigation } from "./Nav";
