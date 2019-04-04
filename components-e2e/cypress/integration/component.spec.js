e6e330ece933178408df6f1ff6ee51f9a9b01945
Clemens Park <clemensp@nulogy.com>
2019-04-04 18:02:22 -0400

    Adds the ability to render react components within cypress tests
    https://github.com/D1no/coinglaze/commit/677bcda02074e63d57155578e5ac466820e6d742
    
    Styled-components also work with this.

--- components-e2e/cypress/integration/component.spec.js
+++ components-e2e/cypress/integration/component.spec.js
@@ -3,28 +3,28 @@ import ReactDOM from "react-dom";
 
 import { NDSProvider, Select } from "@nulogy/components";
 
-it("renders a react component", () => {
-  cy.visit("cypress/index.html");
-  cy.contains("#test", "test goes here");
+// it("renders a react component", () => {
+//   cy.visit("cypress/index.html");
+//   cy.contains("#test", "test goes here");
 
-  // React component, you can use JSX
-  const Welcome = props => <p onClick={ props.onClick }>Hello, {props.name}</p>;
+//   // React component, you can use JSX
+//   const Welcome = props => <p onClick={ props.onClick }>Hello, {props.name}</p>;
 
-  const onClick = cy.stub().as("click");
-  cy.get("#test").then(el$ => {
-    const welcomeGleb = <Welcome name="Gleb" onClick={ onClick } />;
-    ReactDOM.render(welcomeGleb, el$[0]);
-  });
+//   const onClick = cy.stub().as("click");
+//   cy.get("#test").then(el$ => {
+//     const welcomeGleb = <Welcome name="Gleb" onClick={ onClick } />;
+//     ReactDOM.render(welcomeGleb, el$[0]);
+//   });
 
-  cy.log("Testing injected element by clicking on it");
-  // notice that we are using text that is created by `welcomeGleb` element
-  cy.contains("Hello, Gleb").click().click();
-  cy.get("@click").should("have.been.calledTwice");
-});
+//   cy.log("Testing injected element by clicking on it");
+//   // notice that we are using text that is created by `welcomeGleb` element
+//   cy.contains("Hello, Gleb").click().click();
+//   cy.get("@click").should("have.been.calledTwice");
+// });
 
 it("renders an NDS component", () => {
-  cy.visit("cypress/index.html");
-  cy.contains("#test", "test goes here");
+  // cy.visit("cypress/index.html");
+  // cy.contains("#test", "test goes here");
 
   const options = [
     { value: "v1", label: "V One" },
@@ -32,15 +32,21 @@ it("renders an NDS component", () => {
     { value: "v3", label: "V Three" },
   ];
 
-  cy.get("#test").then(el$ => {
-    const someJSX = (
-      <NDSProvider>
-        <Select options={ options } />
-      </NDSProvider>
-    );
+  cy.mount(
+    <NDSProvider>
+      <Select options={ options } />
+    </NDSProvider>
+  );
 
-    ReactDOM.render(someJSX, el$[0]);
-  });
+  // cy.get("#test").then(el$ => {
+  //   const someJSX = (
+  //     <NDSProvider>
+  //       <Select options={ options } />
+  //     </NDSProvider>
+  //   );
+
+  //   ReactDOM.render(someJSX, el$[0]);
+  // });
 
   cy.contains("#test", "V One");
 });
