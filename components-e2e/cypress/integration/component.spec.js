a83d07c09e5c34cfc4f4e601222faf2e66bd3a69
Clemens Park <clemensp@nulogy.com>
2019-04-05 10:22:07 -0400

    Adds the first useful test

--- components-e2e/cypress/integration/component.spec.js
+++ components-e2e/cypress/integration/component.spec.js
@@ -1,52 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import { NDSProvider, Select } from "@nulogy/components";
 
-// it("renders a react component", () => {
-//   cy.visit("cypress/index.html");
-//   cy.contains("#test", "test goes here");
+describe("Select", () => {
+  it("selects the first item when opened", () => {
+    const options = [
+      { value: "v1", label: "V One" },
+      { value: "v2", label: "V Two" },
+      { value: "v3", label: "V Three" },
+    ];
 
-//   // React component, you can use JSX
-//   const Welcome = props => <p onClick={ props.onClick }>Hello, {props.name}</p>;
+    cy.mount(
+      <NDSProvider>
+        <Select options={ options } />
+      </NDSProvider>
+    );
 
-//   const onClick = cy.stub().as("click");
-//   cy.get("#test").then(el$ => {
-//     const welcomeGleb = <Welcome name="Gleb" onClick={ onClick } />;
-//     ReactDOM.render(welcomeGleb, el$[0]);
-//   });
+    cy.get("[aria-label='open menu']").click();
 
-//   cy.log("Testing injected element by clicking on it");
-//   // notice that we are using text that is created by `welcomeGleb` element
-//   cy.contains("Hello, Gleb").click().click();
-//   cy.get("@click").should("have.been.calledTwice");
-// });
-
-it("renders an NDS component", () => {
-  // cy.visit("cypress/index.html");
-  // cy.contains("#test", "test goes here");
-
-  const options = [
-    { value: "v1", label: "V One" },
-    { value: "v2", label: "V Two" },
-    { value: "v3", label: "V Three" },
-  ];
-
-  cy.mount(
-    <NDSProvider>
-      <Select options={ options } />
-    </NDSProvider>
-  );
-
-  // cy.get("#test").then(el$ => {
-  //   const someJSX = (
-  //     <NDSProvider>
-  //       <Select options={ options } />
-  //     </NDSProvider>
-  //   );
-
-  //   ReactDOM.render(someJSX, el$[0]);
-  // });
-
-  cy.contains("#test", "V One");
+    cy.get("[aria-selected='true']").should("have.text", "V One");
+  });
 });
