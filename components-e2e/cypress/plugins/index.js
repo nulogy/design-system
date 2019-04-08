afa01e91eee501cf8b913beb06328d6994eb8172
Clemens Park <clemensp@nulogy.com>
2019-04-08 16:25:05 -0400

    Remove unnecessary code

--- components-e2e/cypress/plugins/index.js
+++ components-e2e/cypress/plugins/index.js
@@ -11,41 +11,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const webpack = require("@cypress/webpack-preprocessor");
-
-/**
- * We need to use the same preprocessor capabilities as create-react-app.
- * Note: ES7, partial decorator support.
- */
-const webpackOptions = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|mjs)$/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-react"],
-          // Macros for styled components
-          // See: https://www.styled-components.com/docs/tooling#babel-macro
-          plugins: ["macros"],
-        },
-      },
-    ],
-  },
-};
-
-/**
- * Place to inject the apps webpack.config.js.
- * Note: Not really necessary for this code challange.
- */
-const options = {
-  webpackOptions,
-  // We use cra's live reload
-  watchOptions: {},
-};
-
-module.exports = on => {
+/* eslint-disable no-unused-vars */
+module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on("file:preprocessor", webpack(options));
 };
+/* eslint-enable no-unused-vars */
