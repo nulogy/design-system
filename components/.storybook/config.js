262a92867aae94487a57990208b0080da685d482
Clemens Park <clemensp@nulogy.com>
2019-04-16 18:46:25 -0400

    Fix

--- components/.storybook/config.js
+++ components/.storybook/config.js
@@ -3,6 +3,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { withBackgrounds } from '@storybook/addon-backgrounds';
 import withStyles from "@sambego/storybook-styles";
 import NDSProvider from '../src/NDSProvider/NDSProvider';
+import theme from "../src/theme";
 
 const grid = {
   base: { colour: 'hsla(120, 100%, 100%, 0.4)', size: `${theme.space.x1}` },
