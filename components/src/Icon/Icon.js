71c1321d3fa47caaeb00e0ee35e78b502888fdf2
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-04-16 15:11:05 -0400

    fixes icon color on SubMenuTrigger

--- components/src/Icon/Icon.js
+++ components/src/Icon/Icon.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { space, color } from "styled-system";
+import { space, color, themeGet } from "styled-system";
 import icons from "../../icons/icons.json";
+import theme from "../theme.js";
 
 const iconNames = Object.keys(icons);
 
@@ -20,7 +21,7 @@ const Svg = props => {
       aria-hidden={ title == null }
       width={ size }
       height={ size }
-      fill={ fillColor }
+      fill={ theme.colors[fillColor] ? theme.colors[fillColor] : fillColor}
       viewBox={ icons[icon].viewBox }
       { ...props }
     >
