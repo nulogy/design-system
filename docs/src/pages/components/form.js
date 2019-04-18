dc0c597d46f5f102e68dd489788c2bbfdfc66ed9
Matt Dunn <mattd@nulogy.com>
2019-04-18 09:51:32 -0400

    capitalizes component names

--- docs/src/pages/components/form.js
+++ docs/src/pages/components/form.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Highlight from "react-highlight";
 import {
-  Box, SectionTitle, SubsectionTitle, Link, Title, List, ListItem, Form, Input, Select, Textarea, PrimaryButton, Button
+  Box, SectionTitle, SubsectionTitle, Link, Title, List, ListItem, Form, Input, Select, Textarea, PrimaryButton, Button,
 } from "@nulogy/components";
 import {
-  DocText as Text, Layout, Intro, IntroText, DocSection, DocSubsection, Image, PropsTable
+  DocText as Text, Layout, Intro, IntroText, DocSection, DocSubsection, Image, PropsTable,
 } from "../../components";
 import anatomy from "../../images/form/anatomy.png";
 import basic from "../../images/form/basic.png";
@@ -14,11 +14,11 @@ import helpTextAnatomy from "../../images/form/help-text-anatomy.png";
 import optionalRequiredAnatomy from "../../images/form/optional-required-anatomy.png";
 
 const options = [
-  { value: 'accepted', label: 'Open' },
-  { value: 'assigned', label: 'Closed' },
-  { value: 'hold', label: 'On hold' },
-  { value: 'rejected', label: 'Rejected' },
-  { value: 'open', label: 'In Progress' },
+  { value: "accepted", label: "Open" },
+  { value: "assigned", label: "Closed" },
+  { value: "hold", label: "On hold" },
+  { value: "rejected", label: "Rejected" },
+  { value: "open", label: "In Progress" },
 ];
 
 const optionToString = option => option && option.label;
@@ -39,11 +39,11 @@ class SelectWithState extends React.Component {
     const { selectedOption } = this.state;
     return (
       <Select
-        onChange={this.handleChange}
-        value={selectedOption}
-        options={options}
-        optionToString={optionToString}
-        {...this.props}
+        onChange={ this.handleChange }
+        value={ selectedOption }
+        options={ options }
+        optionToString={ optionToString }
+        { ...this.props }
       />
     );
   }
@@ -70,11 +70,11 @@ export default () => (
     <DocSection>
       <Box maxWidth="440px">
         <Form>
-          <Box width={1/2} pb="x3" >
+          <Box width={ 1 / 2 } pb="x3">
             <Input id="project" labelText="Project code" />
           </Box>
           <Input id="customer" labelText="Customer's name" />
-          <Select placeholder="Please select project status" options={options} labelText="Project status" />
+          <Select placeholder="Please select project status" options={ options } labelText="Project status" />
           <Textarea rows="4" mb="x6" labelText="Project description" />
           <Box mb="x6">
             <PrimaryButton mr="x1">Create project</PrimaryButton>
@@ -83,7 +83,7 @@ export default () => (
         </Form>
       </Box>
       <Highlight className="js">
-          {`<Form>
+        {`<Form>
   <Input id="project" labelText="Project code" />
   <Input id="customer" labelText="Customer's name" />
   <Select placeholder="Please select project status" options={options} labelText="Project status" />
@@ -93,25 +93,25 @@ export default () => (
     <Button>Cancel</Button>
   </Box>
 </Form>`}
-        </Highlight>
+      </Highlight>
     </DocSection>
 
     <DocSection>
       <SectionTitle>Variations</SectionTitle>
       <SubsectionTitle>With title</SubsectionTitle>
       <Box maxWidth="440px" mt="x6">
-      <Form title="New Project">
-        <Input id="project" labelText="Project code" />
-        <Input id="customer" labelText="Customer's name" />
-        <Select mb="x6" placeholder="Please select project status" options={options} labelText="Project status" />
-        <Box mb="x6" mt="x6">
-          <PrimaryButton mr="x1">Create project</PrimaryButton>
-          <Button>Cancel</Button>
-        </Box>
-      </Form>
-    </Box>
+        <Form title="New Project">
+          <Input id="project" labelText="Project code" />
+          <Input id="customer" labelText="Customer's name" />
+          <Select mb="x6" placeholder="Please select project status" options={ options } labelText="Project status" />
+          <Box mb="x6" mt="x6">
+            <PrimaryButton mr="x1">Create project</PrimaryButton>
+            <Button>Cancel</Button>
+          </Box>
+        </Form>
+      </Box>
       <Highlight className="js">
-            {`<Form title="New Project">
+        {`<Form title="New Project">
   <Input id="project" labelText="Project code" />
   <Input id="customer" labelText="Customer's name" />
   <Select mb="x6" placeholder="Please select project status" options={options} labelText="Project status" />
@@ -124,7 +124,7 @@ export default () => (
     </DocSection>
 
     <DocSection>
-    <SectionTitle>Guidelines</SectionTitle>
+      <SectionTitle>Guidelines</SectionTitle>
       <List>
         <ListItem>Always order items in a logical order that doesn’t require scrolling around</ListItem>
         <ListItem>Whenever possible group items together into form sections</ListItem>
@@ -145,7 +145,7 @@ export default () => (
         <ListItem>How to recover from an error</ListItem>
       </List>
       <Text>There are two types of validation: <Link href="/components/header-validation">Header validation</Link> and Inline validation. Examples of inline validation are available on individual form input components.</Text>
-      
+
       <SubsectionTitle mb="x3">Error message content guidelines</SubsectionTitle>
       <List>
         <ListItem>The best error is one that doesn’t exist so try to prevent errors when possible by grouping related fields, writing effective help text and clearly communicating input expectations.</ListItem>
@@ -162,21 +162,17 @@ export default () => (
     <DocSection>
       <SectionTitle>Related components</SectionTitle>
       <List>
-          <ListItem><Link href="/components/form-section">Form section</Link></ListItem>
-          <ListItem><Link href="/components/input">Input</Link></ListItem>
-          <ListItem><Link href="/components/checkbox">Checkbox</Link></ListItem>
-          <ListItem><Link href="/components/checkbox-group">Checkbox group</Link></ListItem>
-          <ListItem><Link href="/components/radio">Radio</Link></ListItem>
-          <ListItem><Link href="/components/radio-group">Radio group</Link></ListItem>
-          <ListItem><Link href="/components/select">Select</Link></ListItem>
-          <ListItem><Link href="/components/textarea">Textarea</Link></ListItem>
-          <ListItem><Link href="/components/toggle">Toggle</Link></ListItem>
+        <ListItem><Link href="/components/form-section">Form Section</Link></ListItem>
+        <ListItem><Link href="/components/input">Input</Link></ListItem>
+        <ListItem><Link href="/components/checkbox">Checkbox</Link></ListItem>
+        <ListItem><Link href="/components/checkbox-group">Checkbox Group</Link></ListItem>
+        <ListItem><Link href="/components/radio">Radio</Link></ListItem>
+        <ListItem><Link href="/components/radio-group">Radio Group</Link></ListItem>
+        <ListItem><Link href="/components/select">Select</Link></ListItem>
+        <ListItem><Link href="/components/textarea">Textarea</Link></ListItem>
+        <ListItem><Link href="/components/toggle">Toggle</Link></ListItem>
       </List>
     </DocSection>
 
   </Layout>
 );
-
-
-
-
