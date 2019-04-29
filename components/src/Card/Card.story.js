799ac8d660b119b6a6814ec12589dc0897bbd81a
nikola-nulogy <nikolap@nulogy.com>
2019-04-29 09:32:45 -0400

    Adds custom card story

--- components/src/Card/Card.story.js
+++ components/src/Card/Card.story.js
@@ -15,6 +15,9 @@ storiesOf("Card", module)
   .add("Card", () => (
     <Card>I am  a card.</Card>
   ))
+  .add("Custom card", () => (
+    <Card bg="black" color="white" borderRadius="small" p="x1">I am a custom card.</Card>
+  ))
   .add("Cardset", () => (
     <CardSet>
       <Card>I am a 1st card in a cardset.</Card>
