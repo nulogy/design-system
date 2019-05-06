d62b9046523022503d3d195066bd66a3233d536b
nikola-nulogy <nikolap@nulogy.com>
2019-05-06 12:14:33 -0400

    Adds IE11 minHeight stories

--- components/src/Flex/Flex.story.js
+++ components/src/Flex/Flex.story.js
@@ -2,13 +2,23 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Box } from "../Box";
 import { Flex } from ".";
+import { Text } from "../Type";
 import theme from "../theme";
 
-const flexWrapperStyles = {
+const baseFlexWrapperStyles = {
   boxSizing: "content-box",
-  minHeight: "400px",
   padding: theme.space.x3,
   background: theme.colors.whiteGrey
+};
+
+const modernFlexWrapperStyles = {
+  ...baseFlexWrapperStyles,
+  minHeight: "400px"
+};
+
+const ie11FlexWrapperStyles = {
+  ...baseFlexWrapperStyles,
+  height: "400px"
 };
 
 const boxStyles = {
@@ -19,7 +29,7 @@ const boxStyles = {
 
 storiesOf("Flex", module)
   .add("Flex", () => (
-    <Flex style={flexWrapperStyles}>
+    <Flex style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -28,7 +38,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("flexDirection set to row-reverse", () => (
-    <Flex flexDirection="row-reverse" style={flexWrapperStyles}>
+    <Flex flexDirection="row-reverse" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -37,7 +47,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("flexDirection set to column", () => (
-    <Flex flexDirection="column" style={flexWrapperStyles}>
+    <Flex flexDirection="column" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -46,7 +56,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("flexDirection set to column-reverse", () => (
-    <Flex flexDirection="column-reverse" style={flexWrapperStyles}>
+    <Flex flexDirection="column-reverse" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -56,7 +66,7 @@ storiesOf("Flex", module)
   ))
   .add("flexWrap set to no-wrap (default)", () => (
     <Box width={500}>
-      <Flex flexWrap="nowrap" style={flexWrapperStyles}>
+      <Flex flexWrap="nowrap" style={modernFlexWrapperStyles}>
         <Box style={boxStyles}>1</Box>
         <Box style={boxStyles}>2</Box>
         <Box style={boxStyles}>3</Box>
@@ -69,7 +79,7 @@ storiesOf("Flex", module)
   ))
   .add("flexWrap set to wrap", () => (
     <Box width={500}>
-      <Flex flexWrap="wrap" style={flexWrapperStyles}>
+      <Flex flexWrap="wrap" style={modernFlexWrapperStyles}>
         <Box style={boxStyles}>1</Box>
         <Box style={boxStyles}>2</Box>
         <Box style={boxStyles}>3</Box>
@@ -82,7 +92,7 @@ storiesOf("Flex", module)
   ))
   .add("flexWrap set to wrap-reverse", () => (
     <Box width={500}>
-      <Flex flexWrap="wrap-reverse" style={flexWrapperStyles}>
+      <Flex flexWrap="wrap-reverse" style={modernFlexWrapperStyles}>
         <Box style={boxStyles}>1</Box>
         <Box style={boxStyles}>2</Box>
         <Box style={boxStyles}>3</Box>
@@ -94,7 +104,7 @@ storiesOf("Flex", module)
     </Box>
   ))
   .add("justifyContent set to flex-start (default)", () => (
-    <Flex justifyContent="flex-start" style={flexWrapperStyles}>
+    <Flex justifyContent="flex-start" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -103,7 +113,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("justifyContent set to flex-end", () => (
-    <Flex justifyContent="flex-end" style={flexWrapperStyles}>
+    <Flex justifyContent="flex-end" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -112,7 +122,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("justifyContent set to center", () => (
-    <Flex justifyContent="center" style={flexWrapperStyles}>
+    <Flex justifyContent="center" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -121,7 +131,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("justifyContent set to space-between", () => (
-    <Flex justifyContent="space-between" style={flexWrapperStyles}>
+    <Flex justifyContent="space-between" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -130,7 +140,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("justifyContent set to space-around", () => (
-    <Flex justifyContent="space-around" style={flexWrapperStyles}>
+    <Flex justifyContent="space-around" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -139,7 +149,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("justifyContent set to space-evenly", () => (
-    <Flex justifyContent="space-evenly" style={flexWrapperStyles}>
+    <Flex justifyContent="space-evenly" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -148,7 +158,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("alignItems set to stretch (default)", () => (
-    <Flex alignItems="stretch" style={flexWrapperStyles}>
+    <Flex alignItems="stretch" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -157,7 +167,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("alignItems set to flex-start", () => (
-    <Flex alignItems="flex-start" style={flexWrapperStyles}>
+    <Flex alignItems="flex-start" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -166,7 +176,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("alignItems set to center", () => (
-    <Flex alignItems="center" style={flexWrapperStyles}>
+    <Flex alignItems="center" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -175,7 +185,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("alignItems set to flex-end", () => (
-    <Flex alignItems="flex-end" style={flexWrapperStyles}>
+    <Flex alignItems="flex-end" style={modernFlexWrapperStyles}>
       <Box style={boxStyles}>1</Box>
       <Box style={boxStyles}>2</Box>
       <Box style={boxStyles}>3</Box>
@@ -184,7 +194,7 @@ storiesOf("Flex", module)
     </Flex>
   ))
   .add("With custom order", () => (
-    <Flex style={flexWrapperStyles}>
+    <Flex style={modernFlexWrapperStyles}>
       <Box style={boxStyles} order="1">
         1
       </Box>
@@ -201,4 +211,32 @@ storiesOf("Flex", module)
         5
       </Box>
     </Flex>
+  ))
+  .add("IE11 minHeight solution 1", () => (
+    <>
+      <Flex style={ie11FlexWrapperStyles}>
+        <Box style={boxStyles}>1</Box>
+        <Box style={boxStyles}>2</Box>
+        <Box style={boxStyles}>3</Box>
+        <Box style={boxStyles}>4</Box>
+        <Box style={boxStyles}>5</Box>
+      </Flex>
+      <Text my="x2">This solution involves using height instead min-height.</Text>
+    </>
+  ))
+  .add("IE11 minHeight solution 2", () => (
+    <>
+      <Flex flexDirection="column">
+        <Flex style={modernFlexWrapperStyles}>
+          <Box style={boxStyles}>1</Box>
+          <Box style={boxStyles}>2</Box>
+          <Box style={boxStyles}>3</Box>
+          <Box style={boxStyles}>4</Box>
+          <Box style={boxStyles}>5</Box>
+        </Flex>
+      </Flex>
+      <Text my="x2">
+        This solution involves wrapping Flex element with another Flex element with column direction..
+      </Text>
+    </>
   ));
