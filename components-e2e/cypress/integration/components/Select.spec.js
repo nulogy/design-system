da336812a138736d932699b8e1973ce78d1e32b4
Clemens Park <clemensp@nulogy.com>
2019-05-06 11:17:42 -0400

    Add a spec for selecting on click

--- components-e2e/cypress/integration/components/Select.spec.js
+++ components-e2e/cypress/integration/components/Select.spec.js
@@ -20,4 +20,32 @@ describe("Select", () => {
 
     cy.get("[aria-selected='true']").should("have.text", "V One");
   });
+
+  it("selects an option on click", () => {
+    const options = [
+      { value: "v1", label: "V One" },
+      { value: "v2", label: "V Two" },
+      { value: "v3", label: "V Three" }
+    ];
+
+    cy.mount(
+      <NDSProvider>
+        <Select options={options} />
+      </NDSProvider>
+    );
+
+    cy.get("div#app").should("not.contain", "V Two");
+
+    cy.get("[aria-label='open menu']").click();
+    cy.get("div#app").should("contain", "V Two");
+
+    cy.get('[role="option"]')
+      .contains("V Two")
+      .click();
+
+    cy.get("input").should("have.value", "V Two");
+
+    // ensure the dropdown is closed
+    cy.get("div#app").should("not.contain", "V One");
+  });
 });
