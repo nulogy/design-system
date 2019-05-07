234b646005d9d831a5a61d0ad8a8f5b18a5afb86
Clemens Park <clemensp@nulogy.com>
2019-05-07 14:06:06 -0400

    Setup the skeleton for desktop and mobile menu specs

--- components-e2e/cypress/integration/components/NavBar.spec.js
+++ components-e2e/cypress/integration/components/NavBar.spec.js
@@ -3,20 +3,45 @@ import React from "react";
 import { NDSProvider, NavBar } from "@nulogy/components";
 
 describe("NavBar", () => {
-  it("renders", () => {
-    const data = {
-      primaryMenu: [
-        {
-          name: "Menu 1",
-          items: [{ name: "Item 1", href: "/" }]
-        }
-      ]
-    };
+  describe("the desktop menu", () => {
+    it("renders", () => {
+      cy.viewport("macbook-13");
+      const data = {
+        primaryMenu: [
+          {
+            name: "Menu 1",
+            items: [{ name: "Item 1", href: "/" }]
+          }
+        ]
+      };
 
-    cy.mount(
-      <NDSProvider>
-        <NavBar menuData={data} />
-      </NDSProvider>
-    );
+      cy.mount(
+        <NDSProvider>
+          <>
+            <NavBar menuData={data} />
+          </>
+        </NDSProvider>
+      );
+    });
+  });
+
+  describe("the mobile menu", () => {
+    it("renders", () => {
+      cy.viewport("ipad-mini");
+      const data = {
+        primaryMenu: [
+          {
+            name: "Menu 1",
+            items: [{ name: "Item 1", href: "/" }]
+          }
+        ]
+      };
+
+      cy.mount(
+        <NDSProvider>
+          <NavBar menuData={data} />
+        </NDSProvider>
+      );
+    });
   });
 });
