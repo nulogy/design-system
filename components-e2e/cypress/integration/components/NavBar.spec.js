2c96777c1979a6a683a6b8fbe3a7b746afc6a3f5
Clemens Park <clemensp@nulogy.com>
2019-05-16 17:20:23 -0400

    Use the storybook for testing

--- components-e2e/cypress/integration/components/NavBar.spec.js
+++ components-e2e/cypress/integration/components/NavBar.spec.js
@@ -1,57 +1,24 @@
-import React from "react";
-
-import { NDSProvider, NavBar } from "@nulogy/components";
-
 describe("NavBar", () => {
   context("when in desktop mode", () => {
     beforeEach(() => {
       cy.viewport("macbook-13");
-      window.innerWidth = 1280;
-      window.innerHeight = 800;
     });
 
-    it("can be navigated using the keyboard", () => {
-      const data = {
-        primaryMenu: [
-          {
-            name: "Menu 1",
-            items: [{ name: "Item 1", href: "/" }]
-          }
-        ]
-      };
+    it.only("can be navigated using the keyboard", () => {
+      cy.visit("http://localhost:8080/iframe.html?id=navbar--navbar");
 
-      cy.mount(
-        <NDSProvider>
-          <>
-            <NavBar menuData={data} />
-          </>
-        </NDSProvider>
-      );
+      cy.wait(500);
     });
   });
 
   context("when in mobile mode", () => {
     before(() => {
       cy.viewport("ipad-mini");
-      window.innerWidth = 768;
-      window.innerHeight = 1024;
     });
 
     it("renders", () => {
-      const data = {
-        primaryMenu: [
-          {
-            name: "Menu 1",
-            items: [{ name: "Item 1", href: "/" }]
-          }
-        ]
-      };
-
-      cy.mount(
-        <NDSProvider>
-          <NavBar menuData={data} />
-        </NDSProvider>
-      );
+      cy.visit("http://localhost:8080/iframe.html?id=navbar--navbar");
+      cy.wait(500);
     });
   });
 });
