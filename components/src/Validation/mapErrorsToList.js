c6bf7a117eb888797cec07d5f3cf80035ef3a6fd
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-05-21 13:38:20 -0400

    allows mapErrorsToList to handle string input

--- components/src/Validation/mapErrorsToList.js
+++ components/src/Validation/mapErrorsToList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { List, ListItem } from "../List";
 
-const mapErrorsToList = errorArray => {
-  if (!Array.isArray(errorArray) || !errorArray.length) {
+const mapErrorsToList = errors => {
+  if (!Array.isArray(errors) || !errors.length) {
     return null;
   }
-
+  const errorArray = Array.isArray(errors) ? errors : [errors];
   return (
     <List compact leftAlign>
       {errorArray.map(error => (
