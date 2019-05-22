4bf78197af69ad4c1914338b7fde08a594429f9b
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-05-22 10:13:29 -0400

    adds theming

--- components/src/NavBar/Branding.story.js
+++ components/src/NavBar/Branding.story.js
@@ -1,24 +1,46 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { Box } from "../Box";
+import { Flex } from "../Flex";
 import Branding from "./Branding";
 
 storiesOf("Branding", module).add("Branding", () => (
-  <>
-    <Branding />
-    <br />
-    <br />
-    <Branding letterMark />
-    <br />
-    <br />
-    <Branding solutionName="Operational Solution Long Name" />
-    <br />
-    <br />
-    <Branding large />
-    <br />
-    <br />
-    <Branding large solutionName="Operational Solution Long Name" />
-    <br />
-    <br />
-    <Branding large letterMark />
-  </>
+  <Flex>
+    <Box width={1 / 2}>
+      <Branding />
+      <br />
+      <br />
+      <Branding letterMark />
+      <br />
+      <br />
+      <Branding solutionName="Operational Solution" />
+      <br />
+      <br />
+      <Branding large />
+      <br />
+      <br />
+      <Branding large solutionName="Operational Solution" />
+      <br />
+      <br />
+      <Branding large letterMark />
+    </Box>
+    <Box bg="black" width={1 / 2}>
+      <Branding light />
+      <br />
+      <br />
+      <Branding light letterMark />
+      <br />
+      <br />
+      <Branding light solutionName="Operational Solution" />
+      <br />
+      <br />
+      <Branding light large />
+      <br />
+      <br />
+      <Branding light large solutionName="Operational Solution" />
+      <br />
+      <br />
+      <Branding light large letterMark />
+    </Box>
+  </Flex>
 ));
