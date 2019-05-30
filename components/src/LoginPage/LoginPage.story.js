58792c6448594f9e3d454ec764e8f76577a0a11e
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-05-30 14:27:38 -0400

    removes references to Nulogy products in Login Page stories

--- components/src/LoginPage/LoginPage.story.js
+++ components/src/LoginPage/LoginPage.story.js
@@ -13,7 +13,7 @@ import { Checkbox } from "../Checkbox";
 import theme from "../theme";
 
 storiesOf("LoginPages", module)
-  .add("PackManager", () => (
+  .add("Base", () => (
     <Flex
       minHeight="100vh"
       justifyContent="center"
@@ -34,14 +34,9 @@ storiesOf("LoginPages", module)
           flexDirection="column"
           alignItems="center"
         >
-          <Branding
-            size="large"
-            subtext={"Operational Solution"}
-            alignment="middle"
-            style={{ marginBottom: theme.space.x2 }}
-          />
+          <Branding size="large" subtext={"Logo Subtext"} alignment="middle" style={{ marginBottom: theme.space.x2 }} />
           <Text fontSize="small" lineHeight="smallTextBase" mb="x4">
-            Formerly known as <span style={{ color: theme.colors.yellow }}>PackManager</span>
+            Additional Text
           </Text>
           <Form style={{ width: "100%" }}>
             <Input labelText="Email" />
@@ -64,44 +59,7 @@ storiesOf("LoginPages", module)
       </Box>
     </Flex>
   ))
-  .add("GO", () => (
-    <Flex
-      minHeight="100vh"
-      justifyContent="center"
-      alignItems="center"
-      bg={{ extraSmall: "white", small: "whiteGrey" }}
-    >
-      <Flex
-        height={{ extraSmall: "100%", small: "auto" }}
-        width="384px"
-        borderRadius={{ extraSmall: null, small: "medium" }}
-        boxShadow={{ extraSmall: null, small: "small" }}
-        px={{ extraSmall: "x2", small: "x4" }}
-        py={{ extraSmall: "x3", small: "x5" }}
-        bg="white"
-        flexDirection="column"
-        alignItems="center"
-        style={{ margin: "auto" }}
-      >
-        <Branding
-          size="large"
-          subtext={"Ecosystem Solution"}
-          alignment="middle"
-          style={{ marginBottom: theme.space.x2 }}
-        />
-        <Text fontSize="small" lineHeight="smallTextBase" mb="x4">
-          Formerly known as <span style={{ color: theme.colors.green }}>GO</span>
-        </Text>
-        <form style={{ width: "100%" }}>
-          <Input mb="x3" labelText="Email" />
-          <Input type="password" labelText="Password" />
-          <Checkbox mb="x3" labelText="Remember me" />
-          <PrimaryButton fullWidth>Sign In</PrimaryButton>
-        </form>
-      </Flex>
-    </Flex>
-  ))
-  .add("QCloud", () => (
+  .add("with remember me", () => (
     <Flex
       minHeight="100vh"
       justifyContent="center"
@@ -122,14 +80,56 @@ storiesOf("LoginPages", module)
           flexDirection="column"
           alignItems="center"
         >
-          <Branding
-            size="large"
-            subtext={"Operational Solution"}
-            alignment="middle"
-            style={{ marginBottom: theme.space.x2 }}
-          />
+          <Branding size="large" subtext={"Logo Subtext"} alignment="middle" style={{ marginBottom: theme.space.x2 }} />
           <Text fontSize="small" lineHeight="smallTextBase" mb="x4">
-            Formerly known as <span style={{ color: theme.colors.blue }}>QCLOUD</span>
+            Additional Text
+          </Text>
+          <form style={{ width: "100%" }}>
+            <Input mb="x3" labelText="Email" />
+            <Input type="password" labelText="Password" />
+            <Checkbox mb="x3" labelText="Remember me" />
+            <PrimaryButton fullWidth>Sign In</PrimaryButton>
+          </form>
+        </Flex>
+      </Box>
+      <Box
+        width={{
+          extraSmall: `calc(100% - ${theme.space.x4})`,
+          small: `calc(100% - ${theme.space.x8})`
+        }}
+        pt="x2"
+        mb="x4"
+        textAlign="center"
+        style={{ borderTop: `solid 1px ${theme.colors.lightGrey}` }}
+      >
+        <Text color="darkGrey">© 2007-2019 Nulogy Corporation</Text>
+      </Box>
+    </Flex>
+  ))
+  .add("with forgot password link", () => (
+    <Flex
+      minHeight="100vh"
+      justifyContent="center"
+      flexDirection="column"
+      alignItems="center"
+      bg={{ extraSmall: "white", small: "whiteGrey" }}
+    >
+      <Box flexGrow="1">
+        <Flex
+          height={{ extraSmall: "100%", small: "auto" }}
+          width="384px"
+          borderRadius={{ extraSmall: null, small: "medium" }}
+          boxShadow={{ extraSmall: null, small: "small" }}
+          px={{ extraSmall: "x2", small: "x4" }}
+          py={{ extraSmall: "x3", small: "x5" }}
+          bg="white"
+          my="x2"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Branding size="large" subtext={"Logo Subtext"} alignment="middle" style={{ marginBottom: theme.space.x2 }} />
+          <Text fontSize="small" lineHeight="smallTextBase" mb="x4">
+            Additional Text
           </Text>
           <Form style={{ width: "100%" }}>
             <Input labelText="Email" />
@@ -153,36 +153,5 @@ storiesOf("LoginPages", module)
       >
         <Text color="darkGrey">© 2007-2019 Nulogy Corporation</Text>
       </Box>
-    </Flex>
-  ))
-  .add("CPI", () => (
-    <Flex
-      minHeight="100vh"
-      justifyContent="center"
-      alignItems="center"
-      bg={{ extraSmall: "white", small: "whiteGrey" }}
-    >
-      <Flex
-        height={{ extraSmall: "100%", small: "auto" }}
-        width="384px"
-        borderRadius={{ extraSmall: null, small: "medium" }}
-        boxShadow={{ extraSmall: null, small: "small" }}
-        px={{ extraSmall: "x2", small: "x4" }}
-        py={{ extraSmall: "x3", small: "x5" }}
-        bg="white"
-        flexDirection="column"
-        alignItems="center"
-        style={{ margin: "auto" }}
-      >
-        <Branding size="large" subtext={"Connections"} alignment="middle" style={{ marginBottom: theme.space.x2 }} />
-        <Text fontSize="small" lineHeight="smallTextBase" mb="x4">
-          Formerly known as <span style={{ fontWeight: theme.fontWeights.medium }}>Common Platform Interface</span>
-        </Text>
-        <Form>
-          <Input labelText="Email" />
-          <Input type="password" labelText="Password" />
-          <PrimaryButton fullWidth>Sign In</PrimaryButton>
-        </Form>
-      </Flex>
     </Flex>
   ));
