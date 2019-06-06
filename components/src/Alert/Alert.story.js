3d52c69cb3ea1bc28dd762bf672ad1e72a16c5d7
Matt Dunn <mattd@nulogy.com>
2019-06-06 11:16:44 -0400

    alphabetizes stories

--- components/src/Alert/Alert.story.js
+++ components/src/Alert/Alert.story.js
@@ -3,12 +3,12 @@ import { storiesOf } from "@storybook/react";
 import { Alert } from ".";
 
 storiesOf("Alert", module)
-  .add("Informational", () => <Alert>Informational alert</Alert>)
   .add("Danger", () => <Alert type="danger">Danger alert</Alert>)
-  .add("Warning", () => <Alert type="warning">Warning alert</Alert>)
+  .add("Informational", () => <Alert>Informational alert</Alert>)
   .add("Success", () => <Alert type="success">Success alert</Alert>)
+  .add("Warning", () => <Alert type="warning">Warning alert</Alert>)
   .add("With a title", () => (
-    <Alert title="Danger!" type="danger">
-      Hello Button
+    <Alert title="Danger title!" type="danger">
+      Danger alert
     </Alert>
   ));
