9187a0afd993be360117c6879a3f71ef643f0f84
Matt Dunn <mattd@nulogy.com>
2019-06-06 11:30:45 -0400

    simplifies alert colours

--- components/src/Alert/Alert.js
+++ components/src/Alert/Alert.js
@@ -25,32 +25,28 @@ const alertColours = {
   }
 };
 
-const Alert = props => {
-  const alertColour = alertColours[props.type];
-
-  return (
-    <Box
-      bg={alertColour.backgroundColor}
-      p="x2"
-      minWidth="304px"
-      borderWidth="4"
-      borderStyle="solid"
-      borderColor="red"
-      borderRadius={theme.radii.medium}
-      borderLeft={`${theme.space.half} solid ${alertColour.borderColor}`}
-    >
-      <Flex>
-        {props.type == "danger" && <Icon icon="error" mr="x1" color={alertColour.borderColor} />}
-        {props.type == "success" && <Icon icon="check" mr="x1" color={alertColour.borderColor} />}
-        <Box mr="auto">
-          {props.title && <Text fontWeight="bold">{props.title}</Text>}
-          {props.children}
-        </Box>
-        {props.isCloseable && <Icon icon="close" color="darkGrey" size="16" />}
-      </Flex>
-    </Box>
-  );
-};
+const Alert = props => (
+  <Box
+    bg={alertColours[props.type].backgroundColor}
+    p="x2"
+    minWidth="304px"
+    borderWidth="4"
+    borderStyle="solid"
+    borderColor="red"
+    borderRadius={theme.radii.medium}
+    borderLeft={`${theme.space.half} solid ${alertColours[props.type].borderColor}`}
+  >
+    <Flex>
+      {props.type == "danger" && <Icon icon="error" mr="x1" color={alertColours[props.type].borderColor} />}
+      {props.type == "success" && <Icon icon="check" mr="x1" color={alertColours[props.type].borderColor} />}
+      <Box mr="auto">
+        {props.title && <Text fontWeight="bold">{props.title}</Text>}
+        {props.children}
+      </Box>
+      {props.isCloseable && <Icon icon="close" color="darkGrey" size="16" />}
+    </Flex>
+  </Box>
+);
 
 Alert.propTypes = {
   children: PropTypes.node.isRequired,
