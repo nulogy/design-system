3a38e7ccdb9df1e88c79e660cd2b229c2b30f6ca
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-06-14 11:01:31 -0400

    adds error story for login page

--- components/src/LoginPage/LoginPage.story.js
+++ components/src/LoginPage/LoginPage.story.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import theme from "../theme";
-import { Box, Branding, Checkbox, Flex, Form, Input, Link, PrimaryButton, Text } from "../index";
+import { Alert, Box, Branding, Checkbox, Flex, Form, Input, Link, PrimaryButton, Text } from "../index";
 
 storiesOf("LoginPage", module)
   .add("Base", () => (
@@ -148,6 +148,61 @@ storiesOf("LoginPage", module)
           <Box py="x1" mt="x1">
             <Link href="/">Forgot your password?</Link>
           </Box>
+        </Flex>
+      </Flex>
+      <Box
+        width={{
+          extraSmall: `calc(100% - ${theme.space.x4})`,
+          small: `calc(100% - ${theme.space.x8})`
+        }}
+        pt="x2"
+        mb="x4"
+        textAlign="center"
+        style={{ borderTop: `solid 1px ${theme.colors.lightGrey}` }}
+      >
+        <Text color="darkGrey">© 2007-2019 Nulogy Corporation</Text>
+      </Box>
+    </Flex>
+  ))
+  .add("with error", () => (
+    <Flex
+      minHeight="100vh"
+      justifyContent="center"
+      flexDirection="column"
+      alignItems="center"
+      bg={{ extraSmall: "white", small: "whiteGrey" }}
+    >
+      <Flex alignItems="center" flexGrow="1">
+        <Flex
+          height={{ extraSmall: "100%", small: "auto" }}
+          width="384px"
+          borderRadius={{ extraSmall: null, small: "medium" }}
+          boxShadow={{ extraSmall: null, small: "small" }}
+          px={{ extraSmall: "x2", small: "x4" }}
+          py={{ extraSmall: "x3", small: "x5" }}
+          bg="white"
+          my="x2"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Branding
+            withLine
+            size="large"
+            subtext="Logo Subtext"
+            alignment="center"
+            style={{ marginBottom: theme.space.x2 }}
+          />
+          <Text fontSize="small" lineHeight="smallTextBase" mb="x4">
+            Additional Text
+          </Text>
+          <Alert mb="x5" style={{ width: "100%" }} type="danger">
+            text
+          </Alert>
+          <Form style={{ width: "100%" }}>
+            <Input labelText="Email" />
+            <Input type="password" labelText="Password" />
+            <PrimaryButton fullWidth>Sign In</PrimaryButton>
+          </Form>
         </Flex>
       </Flex>
       <Box
