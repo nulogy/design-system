f34baf96fe1f8c8f90e81199769073cc80f14e8d
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-06-25 15:34:34 -0400

    adds and configures viewport addon to css storybook

--- css/.storybook/config.js
+++ css/.storybook/config.js
@@ -1,10 +1,52 @@
-import { configure } from '@storybook/html';
-import '../src/nds-dev.css';
+import { configure, addParameters } from "@storybook/html";
+import "../src/nds-dev.css";
 
-const req = require.context('../src/scss', true, /\.story\.js$/);
+const req = require.context("../src/scss", true, /\.story\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-  configure(loadStories, module);
+const newViewports = {
+  extraSmall: {
+    name: "Extra small",
+    styles: {
+      width: "320px",
+      height: "100%"
+    }
+  },
+  small: {
+    name: "Small",
+    styles: {
+      width: "768px",
+      height: "100%"
+    }
+  },
+  medium: {
+    name: "Medium",
+    styles: {
+      width: "1024px",
+      height: "100%"
+    }
+  },
+  large: {
+    name: "Large",
+    styles: {
+      width: "1360px",
+      height: "100%"
+    }
+  },
+  extraLarge: {
+    name: "Extra large",
+    styles: {
+      width: "1920px",
+      height: "100%"
+    }
+  }
+};
+
+addParameters({
+  viewport: { viewports: newViewports }
+});
+
+configure(loadStories, module);
