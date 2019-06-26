5b21e4eeb51cf9be56525ee08f71c66914a45444
Matt Dunn <mattd@nulogy.com>
2019-06-26 11:35:47 -0400

    fixes alert icon responsive sizing

--- css/src/scss/components/_alert.story.js
+++ css/src/scss/components/_alert.story.js
@@ -6,8 +6,10 @@ storiesOf("Components|Alerts", module)
     "Alert",
     () => `
     <div class="Alert">
-      <p class="Alert__title">Important information</p>
-      <p class="Alert__message">Details about important information</p>
+      <div class="Alert__content">
+        <p class="Alert__title">Important information</p>
+        <p class="Alert__message">Details about important information</p>
+      </div>
     </div>
 `
   )
@@ -15,7 +17,9 @@ storiesOf("Components|Alerts", module)
     "Alert--danger",
     () => `
   <div class="Alert Alert--danger">
-    <svg class="Alert--danger__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>
+    <div class="Alert--danger__icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>
+    </div>
     <div class="Alert__content">
       <p class="Alert__title">Important information</p>
       <p class="Alert__message">Details about important information</p>
