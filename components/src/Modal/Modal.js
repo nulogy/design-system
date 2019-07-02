240c0d126e26154669232118c7164fbc17439796
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-07-02 15:39:59 -0400

    adds modal markup and scrolling

--- components/src/Modal/Modal.js
+++ components/src/Modal/Modal.js
@@ -1,6 +1,78 @@
 import React from "react";
 import styled from "styled-components";
+import { transparentize } from "polished";
+import { SectionTitle } from "../Type";
+import { Button, PrimaryButton, DangerButton } from "../Button";
+import theme from "../theme";
 
-const Modal = styled.div({});
+const DimPage = styled.div({
+  width: "100vw",
+  height: "100vh",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  backgroundColor: transparentize(0.1, theme.colors.whiteGrey),
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+});
+
+const ModalCard = styled.div({
+  position: "relative",
+  backgroundColor: theme.colors.white,
+  borderRadius: theme.radii.medium,
+  padding: theme.space.x2,
+  width: "300px",
+  height: "300px"
+});
+
+const ModalContent = styled.div({
+  //margin: `${theme.space.x8} 0`,
+  padding: `${theme.space.x8} 0px`,
+  overflow: "scroll",
+  height: "100%"
+});
+
+const ModalHeader = styled.div({
+  position: "absolute",
+  top: theme.space.x2,
+  left: theme.space.x1,
+  right: theme.space.x1,
+  backgroundColor: transparentize(0.1, theme.colors.white),
+  borderBottom: `solid 1px ${theme.colors.lightGrey}`
+});
+
+const ModalFooter = styled.div({
+  position: "absolute",
+  bottom: theme.space.x2,
+  left: theme.space.x1,
+  right: theme.space.x1,
+  backgroundColor: transparentize(0.1, theme.colors.white),
+  borderTop: `solid 1px ${theme.colors.lightGrey}`
+});
+
+const ButtonSet = styled.div({
+  marginTop: theme.space.x2,
+  button: {
+    marginLeft: theme.space.x1
+  }
+});
+
+const Modal = ({ children, ...props }) => (
+  <DimPage>
+    <ModalCard>
+      <ModalHeader>
+        <SectionTitle ml="x1">Modal Title</SectionTitle>
+      </ModalHeader>
+      <ModalContent>{children}</ModalContent>
+      <ModalFooter>
+        <ButtonSet>
+          <PrimaryButton>Primary Button</PrimaryButton>
+          <Button>Button</Button>
+        </ButtonSet>
+      </ModalFooter>
+    </ModalCard>
+  </DimPage>
+);
 
 export default Modal;
--- components/src/Modal/Modal.story.js
+++ components/src/Modal/Modal.story.js
@@ -2,4 +2,12 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Modal } from "../index";
 
-storiesOf("Modal", module).add("Modal", () => <Modal>Modal</Modal>);
+storiesOf("Modal", module)
+  .add("Modal", () => <Modal>Content Content Content</Modal>)
+  .add("with scrolling content", () => (
+    <Modal>
+      Content Content Content Content Content Content Content Content Content Content Content Content Content Content
+      Content Content Content Content Content Content Content Content Content Content Content Content Content Content
+      Content Content Content Content Content Content Content Content Content Content Content
+    </Modal>
+  ));
