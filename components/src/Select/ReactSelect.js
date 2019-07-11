781d5cb2b7d43c1838c50d0541594bf87628a584
Matt Dunn <mattd@nulogy.com>
2019-07-11 09:09:07 -0400

    adds reactselect alongside select for dev

---
+++ components/src/Select/ReactSelect.js
@@ -0,0 +1,128 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { transparentize } from "polished";
+import Select from "react-select";
+import { Field } from "../Form";
+import { MaybeFieldLabel } from "../FieldLabel";
+import { InlineValidation } from "../Validation";
+import theme from "../theme";
+import { subPx, ScrollIndicators } from "../utils";
+
+const ReactSelect = ({
+  options,
+  labelText,
+  required,
+  requirementText,
+  helpText,
+  isMulti,
+  isDisabled,
+  errorMessage,
+  errorList,
+  error = !!(errorMessage || errorList)
+}) => (
+  <Field>
+    <MaybeFieldLabel labelText={labelText} requirementText={requirementText} helpText={helpText}>
+      <Select
+        placeholder="Please select inventory status"
+        options={options}
+        isMulti={isMulti}
+        labelText={labelText}
+        styles={customStyles(error)}
+        isDisabled={isDisabled}
+        aria-required={required}
+        aria-invalid={error}
+      />
+      <InlineValidation mt="x1" errorMessage={errorMessage} errorList={errorList} />
+    </MaybeFieldLabel>
+  </Field>
+);
+
+const getBorderColor = ({ error, disabled, isOpen, isFocused }) => {
+  const { red, lightGrey, blue, grey } = theme.colors;
+
+  if (error) {
+    return red;
+  }
+
+  if (disabled) {
+    return lightGrey;
+  }
+  if (isOpen || isFocused) {
+    return blue;
+  }
+
+  return grey;
+};
+
+const customStyles = error => {
+  return {
+    option: (provided, state) => ({
+      padding: theme.space.x1,
+      fontWeight: state.isSelected ? theme.fontWeights.medium : theme.fontWeights.normal,
+      background: state.isFocused ? theme.colors.lightBlue : null
+    }),
+    control: (provided, state) => ({
+      display: "flex",
+      position: "relative",
+      fontFamily: theme.fonts.base,
+      width: "100%",
+      fontSize: theme.fontSizes.medium,
+      lineHeight: theme.lineHeights.base,
+      background: state.isDisabled ? theme.colors.whiteGrey : theme.colors.white,
+      border: `1px solid ${theme.colors.grey}`,
+      borderColor: state.isFocused ? theme.colors.blue : theme.colors.grey,
+      borderColor: getBorderColor({
+        error,
+        disabled: state.isDisabled,
+        isOpen: state.selectProps.menuIsOpen,
+        isFocused: state.isFocused
+      }),
+      boxShadow: state.selectProps.menuIsOpen ? theme.shadows.small : null,
+      borderRadius: theme.radii.medium,
+      borderBottomLeftRadius: state.selectProps.menuIsOpen ? 0 : theme.radii.medium,
+      "&:hover, &:focus": {
+        borderColor: getBorderColor({
+          error,
+          disabled: state.isDisabled,
+          isOpen: state.selectProps.menuIsOpen,
+          isFocused: true
+        })
+      }
+    }),
+    menu: (provided, state) => ({
+      marginTop: 0,
+      position: "absolute",
+      zIndex: "100",
+      width: "100%",
+      background: theme.colors.white,
+      borderWidth: "1px",
+      borderColor: getBorderColor({
+        error,
+        isOpen: true,
+        disabled: state.isDisabled,
+        isFocused: false
+      }),
+      borderBottomStyle: "solid",
+      borderLeftStyle: "solid",
+      borderRightStyle: "solid",
+      borderRadius: `0 0 ${theme.radii.medium} ${theme.radii.medium}`,
+      boxShadow: theme.shadows.small
+    }),
+    menuList: provided => ({
+      ...provided,
+      padding: 0
+    }),
+    placeholder: (provided, state) => ({
+      // ...provided,
+      color: state.isDisabled ? transparentize(0.6667, theme.colors.black) : "hsl(0,0%,50%)"
+    })
+  };
+};
+
+const extractLabelFromOption = option => option && option.label;
+
+ReactSelect.propTypes = {};
+ReactSelect.defaultProps = {};
+
+export default ReactSelect;
---
+++ components/src/Select/ReactSelect.story.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Box, ReactSelect } from "../index";
+import theme from "../theme";
+
+const errorMessage = "an error!";
+const errorList = ["Error message 1", "Error message 2"];
+
+const options = [
+  { value: "accepted", label: "Accepted" },
+  { value: "assigned", label: "Assigned to a line" },
+  { value: "hold", label: "On hold" },
+  { value: "rejected", label: "Rejected" },
+  { value: "open", label: "Open" },
+  { value: "progress", label: "In progress" },
+  { value: "quarantine", label: "In quarantine" }
+];
+
+storiesOf("Select", module).add("ReactSelect", () => (
+  <>
+    <Box mb="x4">
+      <ReactSelect
+        maxHeight="200px"
+        laceholder="Please select inventory status"
+        options={options}
+        labelText="Inventory status"
+      />
+    </Box>
+    <Box mb="x4">
+      <ReactSelect
+        placeholder="Please select inventory status"
+        options={options}
+        labelText="With multiselect"
+        isMulti
+      />
+    </Box>
+    <Box mb="x4">
+      <ReactSelect
+        placeholder="Please select inventory status"
+        options={options}
+        labelText="With errors"
+        errorMessage="Please select an inventory status"
+        errorList={errorList}
+      />
+    </Box>
+    <ReactSelect placeholder="Please select inventory status" options={options} labelText="Disabled" isDisabled />
+  </>
+));
