a965a8fae81770c523c25f418bdd4ece55f5d651
Matt Dunn <mattd@nulogy.com>
2019-07-16 10:24:07 -0400

    removes unused styled-components import

--- components/src/Select/ReactSelect.js
+++ components/src/Select/ReactSelect.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import { transparentize } from "polished";
 import Select from "react-select";
 import { Field } from "../Form";
