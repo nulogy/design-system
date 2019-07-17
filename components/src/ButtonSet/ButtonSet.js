541d04819dad5c36184c6df25a274c572669b9fd
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-07-17 13:46:01 -0400

    corrects button gutters on ButtonSet

--- components/src/ButtonSet/ButtonSet.js
+++ components/src/ButtonSet/ButtonSet.js
@@ -3,12 +3,47 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import theme from "../theme";
 
-const ButtonSet = styled.div({
-  display: "flex",
-  width: "100%",
-  "button:not(:last-child)": {
-    marginRight: theme.space.x1
+const alignments = {
+  left: "flex-start",
+  center: "space-between",
+  right: "flex-end"
+};
+
+const buttonSpacings = {
+  left: {
+    "button:not(:last-child)": {
+      marginRight: theme.space.x1
+    }
+  },
+  center: {},
+  right: {
+    "button:not(:first-child)": {
+      marginLeft: theme.space.x1
+    }
   }
-});
+};
+
+const getAlignment = alignment => alignments[alignment] || alignments.right;
+
+const getButtonSpacing = alignment => buttonSpacings[alignment] || buttonSpacings.right;
+
+const ButtonSet = styled.div(({ alignment }) => ({
+  display: "flex",
+  flexWrap: "wrap",
+  marginBottom: `-${theme.space.x1}`,
+  justifyContent: getAlignment(alignment),
+  button: {
+    marginBottom: theme.space.x1
+  },
+  ...getButtonSpacing(alignment)
+}));
+
+ButtonSet.propTypes = {
+  alignment: PropTypes.oneOf(["left", "center", "right"])
+};
+
+ButtonSet.defaultProps = {
+  alignment: "right"
+};
 
 export default ButtonSet;
--- components/src/ButtonSet/ButtonSet.story.js
+++ components/src/ButtonSet/ButtonSet.story.js
@@ -2,12 +2,74 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Box, ButtonSet, Button, PrimaryButton, DangerButton, QuietButton, IconicButton } from "../index";
 
-storiesOf("ButtonSet", module).add("ButtonSet", () => (
-  <Box bg="whiteGrey" p="x2" width="500px">
-    <ButtonSet>
-      <Button>Button</Button>
-      <Button>Button</Button>
-      <PrimaryButton>Button</PrimaryButton>
-    </ButtonSet>
-  </Box>
-));
+storiesOf("ButtonSet", module)
+  .add("ButtonSet", () => (
+    <Box bg="whiteGrey" p="x2" width="500px">
+      <ButtonSet>
+        <Button>Button</Button>
+        <Button>Button</Button>
+        <PrimaryButton>Button</PrimaryButton>
+      </ButtonSet>
+    </Box>
+  ))
+  .add("with alignment left", () => (
+    <Box bg="whiteGrey" p="x2" width="500px">
+      <ButtonSet alignment="left">
+        <PrimaryButton>Button</PrimaryButton>
+        <Button>Button</Button>
+        <Button>Button</Button>
+      </ButtonSet>
+    </Box>
+  ))
+  .add("with alignment center", () => (
+    <Box bg="whiteGrey" p="x2" width="500px">
+      <ButtonSet alignment="center">
+        <Button>Button</Button>
+        <PrimaryButton>Button</PrimaryButton>
+      </ButtonSet>
+    </Box>
+  ))
+  .add("more button types", () => (
+    <Box bg="whiteGrey" p="x2" width="600px">
+      <ButtonSet>
+        <Button>Button</Button>
+        <PrimaryButton>Button</PrimaryButton>
+        <DangerButton>Button</DangerButton>
+        <QuietButton>Button</QuietButton>
+        <IconicButton icon="menu" />
+        <IconicButton icon="menu">Button</IconicButton>
+      </ButtonSet>
+    </Box>
+  ))
+  .add("wrapping buttons", () => (
+    <>
+      <Box bg="whiteGrey" p="x2">
+        <ButtonSet>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+        </ButtonSet>
+      </Box>
+      <Box bg="whiteGrey" p="x2" mt="x2">
+        <ButtonSet alignment="left">
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+          <Button>Button</Button>
+        </ButtonSet>
+      </Box>
+    </>
+  ));
