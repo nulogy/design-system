f2d26eb19deae7dc11343049e7ffa7183f8e251c
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-07-24 15:20:25 -0400

    fixes bug in LockBodyScroll;

--- components/src/utils/LockBodyScroll.js
+++ components/src/utils/LockBodyScroll.js
@@ -1,11 +1,5 @@
 import React from "react";
 
-const preventIOSScroll = e => {
-  e.preventDefault();
-};
-
-const defaultFnc = e => {};
-
 class LockBodyScroll extends React.Component {
   constructor(props) {
     super();
@@ -15,14 +9,10 @@ class LockBodyScroll extends React.Component {
   componentDidMount() {
     this.initialBodyOverflow = document.body.style.overflow;
     document.body.style.overflow = "hidden";
-    document.body.addEventListener("touchmove", preventIOSScroll, { passive: false });
-    this.props.newScrollingContainer.addEventListener("touchmove", defaultFnc, { passive: false });
   }
 
   componentWillUnmount() {
     document.body.style.overflow = this.initialBodyOverflow;
-    document.body.removeEventListener("touchmove", preventIOSScroll, { passive: false });
-    this.props.newScrollingContainer.removeEventListener("touchmove", defaultFnc, { passive: false });
   }
 
   render() {
