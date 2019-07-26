6242325bee2342d7fa1bfc3ccebc29f52a43c06b
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-07-26 16:17:20 -0400

    adds scrollIndicators to the left and right

--- components/src/Tabs/Tabs.js
+++ components/src/Tabs/Tabs.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import theme from "../theme";
 import React from "react";
+import ReactResizeDetector from "react-resize-detector";
 
 const TabContainer = styled.div({
   display: "flex",
@@ -20,15 +21,82 @@ const TabContainer = styled.div({
   }
 });
 
+const ScrollIndicatorLeft = ({ left }) => (
+  <div
+    style={{
+      position: "absolute",
+      top: 0,
+      bottom: 0,
+      left: left,
+      height: theme.space.x6,
+      width: theme.space.x6,
+      background: theme.colors.lightGrey,
+      opacity: 0.8,
+      pointerEvents: "none"
+    }}
+  />
+);
+
+const ScrollIndicatorRight = ({ right }) => (
+  <div
+    style={{
+      position: "absolute",
+      top: 0,
+      bottom: 0,
+      right: right,
+      height: theme.space.x6,
+      width: theme.space.x6,
+      background: theme.colors.lightGrey,
+      opacity: 0.8,
+      pointerEvents: "none"
+    }}
+  />
+);
+
 class Tabs extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedIndex: this.props.selectedIndex || null
+      selectedIndex: this.props.selectedIndex || null,
+      scrollLeft: 0,
+      offsetWidth: 0,
+      scrollWidth: 0
     };
 
+    this.tabsRef = React.createRef();
+    this.handleScroll = this.handleScroll.bind(this);
+    this.handleResize = this.handleResize.bind(this);
     this.handleTabClick = this.handleTabClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleResize();
+  }
+
+  handleResize() {
+    this.setState({
+      offsetWidth: this.tabsRef.current.offsetWidth,
+      scrollWidth: this.tabsRef.current.scrollWidth
+    });
+  }
+
+  handleScroll() {
+    console.log(this.state);
+    if (this.tabsRef.current) {
+      this.setState({
+        scrollLeft: this.tabsRef.current.scrollLeft
+      });
+    }
+  }
+
+  contentHiddenRight() {
+    console.log(this.state.scrollLeft + this.state.offsetWidth < this.state.scrollWidth);
+    return this.state.scrollLeft + this.state.offsetWidth < this.state.scrollWidth;
+  }
+
+  contentHiddenLeft() {
+    return this.state.scrollLeft !== 0 && this.state.offsetWidth < this.state.scrollWidth;
   }
 
   handleTabClick(index) {
@@ -42,7 +110,9 @@ class Tabs extends React.Component {
     const { selectedIndex } = this.state;
 
     return (
-      <TabContainer>
+      <TabContainer onScroll={this.handleScroll} ref={this.tabsRef}>
+        <ReactResizeDetector handleWidth onResize={this.handleResize} />
+        {this.contentHiddenLeft() && <ScrollIndicatorLeft left={this.state.scrollLeft} />}
         {React.Children.map(children, (tab, index) =>
           React.cloneElement(tab, {
             onClick: this.props.selectedIndex
@@ -55,6 +125,7 @@ class Tabs extends React.Component {
             fullWidth: fitted
           })
         )}
+        {this.contentHiddenRight() && <ScrollIndicatorRight right={this.state.scrollLeft * -1} />}
       </TabContainer>
     );
   }
