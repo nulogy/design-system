93362bcebb704de3b133cd78cb7191f6adb7062f
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-07-29 12:56:04 -0400

    saves overflow x and y on body

--- components/src/utils/PreventBodyElementScrolling.js
+++ components/src/utils/PreventBodyElementScrolling.js
@@ -3,11 +3,15 @@ import PropTypes from "prop-types";
 
 class PreventBodyElementScrolling extends React.Component {
   componentDidMount() {
+    this.initialBodyOverflowX = document.body.style.overflowX;
+    this.initialBodyOverflowY = document.body.style.overflowY;
     this.initialBodyOverflow = document.body.style.overflow;
     document.body.style.overflow = "hidden";
   }
 
   componentWillUnmount() {
+    document.body.style.overflowX = this.initialBodyOverflowX;
+    document.body.style.overflowY = this.initialBodyOverflowY;
     document.body.style.overflow = this.initialBodyOverflow;
   }
 
