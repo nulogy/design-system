4d99f9b2828e43012d9274d94f5b83e0df6657b5
Matt Dunn <mattd@nulogy.com>
2019-08-06 12:23:47 -0400

    adds plop generator

---
+++ plopfile.js
@@ -0,0 +1,38 @@
+module.exports = function(plop) {
+  plop.setGenerator("controller", {
+    description: "new component controller logic",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Enter a CamelCase name for your component:"
+      }
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "components/src/{{name}}/{{name}}.js",
+        templateFile: "components/template/component.hbs"
+      },
+      {
+        type: "add",
+        path: "components/src/{{name}}/index.js",
+        templateFile: "components/template/index.hbs"
+      },
+      {
+        type: "add",
+        path: "components/src/{{name}}/{{name}}.story.js",
+        templateFile: "components/template/component.story.hbs"
+      },
+      {
+        type: "modify",
+        path: "components/src/index.js",
+        pattern: /$/,
+        templateFile: "components/template/index-js-injection.txt"
+      },
+      function(data) {
+        return "Your component was created! 😊";
+      }
+    ]
+  });
+};
