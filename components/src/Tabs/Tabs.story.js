dc580f71c694b0799058c6c11243a288cdcdc549
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-08-14 09:29:44 -0400

    cleans up Tabs stories that will be sued for testing

--- components/src/Tabs/Tabs.story.js
+++ components/src/Tabs/Tabs.story.js
@@ -73,25 +73,9 @@ storiesOf("Tabs", module)
       <Tab label="Tab" disabled selected />
     </div>
   ))
-  .add("with many tabs", () => (
-    <Tabs>
-      <Tab label="Tab 1">Tab 1 Content</Tab>
-      <Tab label="Tab 2">Tab 2 Content</Tab>
-      <Tab label="Tab 3">Tab 3 Content</Tab>
-      <Tab label="Tab 4">Tab 4 Content</Tab>
-      <Tab label="Tab 5">Tab 5 Content</Tab>
-      <Tab label="Tab 6">Tab 6 Content</Tab>
-      <Tab label="Tab 7">Tab 7 Content</Tab>
-      <Tab label="Tab 8">Tab 8 Content</Tab>
-      <Tab label="Tab 9">Tab 9 Content</Tab>
-      <Tab label="Tab 10">Tab 10 Content</Tab>
-      <Tab label="Tab 11">Tab 11 Content</Tab>
-      <Tab label="Tab 12">Tab 12 Content</Tab>
-    </Tabs>
-  ))
-  .add("TESTING tabs", () => (
-    <>
-      <button>1</button>
+  .add(
+    "with scrolling tabs",
+    () => (
       <Tabs>
         <Tab label="Tab 1">Tab 1 Content</Tab>
         <Tab label="Tab 2">Tab 2 Content</Tab>
@@ -106,73 +90,7 @@ storiesOf("Tabs", module)
         <Tab label="Tab 11">Tab 11 Content</Tab>
         <Tab label="Tab 12">Tab 12 Content</Tab>
       </Tabs>
-      <button>2</button>
-    </>
-  ))
-  .add("TESTING container", () => (
-    <div style={{ maxWidth: "500px" }}>
-      <Tabs>
-        <Tab label="Tab 1">Tab 1 Content</Tab>
-        <Tab label="Tab 2">Tab 2 Content</Tab>
-        <Tab label="Tab 3">Tab 3 Content</Tab>
-        <Tab label="Tab 4">Tab 4 Content</Tab>
-        <Tab label="Tab 5">Tab 5 Content</Tab>
-        <Tab label="Tab 6">Tab 6 Content</Tab>
-        <Tab label="Tab 7">Tab 7 Content</Tab>
-        <Tab label="Tab 8">Tab 8 Content</Tab>
-        <Tab label="Tab 9">Tab 9 Content</Tab>
-        <Tab label="Tab 10">Tab 10 Content</Tab>
-        <Tab label="Tab 11">Tab 11 Content</Tab>
-        <Tab label="Tab 12">Tab 12 Content</Tab>
-      </Tabs>
-    </div>
-  ))
-  .add("TESTING persistant tab content", () => (
-    <Tabs>
-      <Tab label="Tab 1">
-        Tab 1 Content
-        <input />
-      </Tab>
-      <Tab label="Tab 2">
-        Tab 2 Content
-        <input />
-      </Tab>
-      <Tab label="Tab 3">
-        Tab 3 Content
-        <input />
-      </Tab>
-    </Tabs>
-  ))
-  .add("TESTING re-rending tab content", () => (
-    <Tabs renderTabContentOnlyWhenSelected>
-      <Tab label="Tab 1">
-        Tab 1 Content
-        <input />
-      </Tab>
-      <Tab label="Tab 2">
-        Tab 2 Content
-        <input />
-      </Tab>
-      <Tab label="Tab 3">
-        Tab 3 Content
-        <input />
-      </Tab>
-    </Tabs>
-  ))
-  .add("TESTING classNames", () => (
-    <Tabs className="tabs-className" tabContentClassName="tab-content-className">
-      <Tab className="tab-className" label="Tab 1">
-        Tab 1 Content
-        <input />
-      </Tab>
-      <Tab className="tab-className" label="Tab 2">
-        Tab 2 Content
-        <input />
-      </Tab>
-      <Tab className="tab-className" label="Tab 3">
-        Tab 3 Content
-        <input />
-      </Tab>
-    </Tabs>
-  ))
-  .add("TESTING controlled Tabs", () => <ControlledTabs />);
+    ),
+    { viewport: { defaultViewport: "extraSmall" } }
+  )
+  .add("controlled Tabs", () => <ControlledTabs />);
