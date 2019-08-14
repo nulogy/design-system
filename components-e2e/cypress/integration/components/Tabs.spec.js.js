4065ad8a4a98077fdb3a40e5cf8abb6ddaf6bb3c
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-08-14 15:36:08 -0400

    adds tests for arrow key focus navigation

--- components-e2e/cypress/integration/components/Tabs.spec.js.js
+++ components-e2e/cypress/integration/components/Tabs.spec.js.js
@@ -1,4 +1,5 @@
 describe("Tabs", () => {
+  //behavior tests
   it("renders tab components", () => {
     cy.renderFromStorybook("tabs--base");
 
@@ -31,5 +32,46 @@ describe("Tabs", () => {
       "have.text",
       "Tab 1 Content"
     );
+  });
+
+  //focus
+  it("moves to the next tab when right arrow key is pressed", () => {
+    cy.renderFromStorybook("tabs--base");
+
+    cy.get(".Tab1")
+      .click()
+      .type("{rightarrow}");
+
+    cy.focused().should("have.text", "Tab 2");
+  });
+
+  it("moves to the previous tab whenleft arrow key is pressed", () => {
+    cy.renderFromStorybook("tabs--base");
+
+    cy.get(".Tab2")
+      .click()
+      .type("{leftarrow}");
+
+    cy.focused().should("have.text", "Tab 1");
+  });
+
+  it("moves to the first tab when right arrow key is pressed on last tab", () => {
+    cy.renderFromStorybook("tabs--base");
+
+    cy.get(".Tab4")
+      .click()
+      .type("{rightarrow}");
+
+    cy.focused().should("have.text", "Tab 1");
+  });
+
+  it("moves to the last tab when left arrow key is pressed on first tab", () => {
+    cy.renderFromStorybook("tabs--base");
+
+    cy.get(".Tab1")
+      .click()
+      .type("{leftarrow}");
+
+    cy.focused().should("have.text", "Tab 4");
   });
 });
