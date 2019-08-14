386b444330fc03259bd62afacb2dbc97730b97aa
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-08-14 15:02:25 -0400

    adds two more tests when clicking a tab

--- components-e2e/cypress/integration/components/Tabs.spec.js.js
+++ components-e2e/cypress/integration/components/Tabs.spec.js.js
@@ -5,6 +5,23 @@ describe("Tabs", () => {
     cy.get(".Tab1").should("have.text", "Tab 1");
   });
 
+  it("selects the tab that is clicked", () => {
+    cy.renderFromStorybook("tabs--base");
+
+    cy.get(".Tab1")
+      .should("have.attr", "aria-selected", "false")
+      .click()
+      .should("have.attr", "aria-selected", "true");
+  });
+
+  it("moves focus to the tab that is clicked", () => {
+    cy.renderFromStorybook("tabs--base");
+
+    cy.get(".Tab1").click();
+
+    cy.focused().should("have.text", "Tab 1");
+  });
+
   it("opens tab content when the tab is clicked", () => {
     cy.renderFromStorybook("tabs--base");
 
