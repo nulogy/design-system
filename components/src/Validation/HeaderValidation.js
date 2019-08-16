6207d2cfa2dd13ac57c02eed28cf7995924c696b
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-08-16 11:01:39 -0400

    removes margin bottom from HeaderValidaiton

--- components/src/Validation/HeaderValidation.js
+++ components/src/Validation/HeaderValidation.js
@@ -1,18 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { Box } from "../Box";
 import { Alert } from "../Alert";
 import mapErrorsToList from "./mapErrorsToList";
 
 const BaseHeaderValidation = ({ className, title, errorMessage, errorList, children }) => (
-  <Box mb="x2" className={className}>
-    <Alert title={title} type="danger">
-      {errorMessage}
-      {mapErrorsToList(errorList)}
-      {children}
-    </Alert>
-  </Box>
+  <Alert className={className} title={title} type="danger">
+    {errorMessage}
+    {mapErrorsToList(errorList)}
+    {children}
+  </Alert>
 );
 
 const HeaderValidation = styled(BaseHeaderValidation)({});
