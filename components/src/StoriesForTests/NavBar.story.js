521f9273e420371c12864462718389993a53dcbc
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-08-20 13:44:32 -0400

    adds testing story for renders other than links and dropdowns

--- components/src/StoriesForTests/NavBar.story.js
+++ components/src/StoriesForTests/NavBar.story.js
@@ -54,10 +54,88 @@ const secondaryMenu = [
   }
 ];
 
+const customPrimaryMenu = [
+  {
+    name: "Text"
+  },
+  {
+    name: "Link",
+    href: "/"
+  },
+  {
+    name: "Custom",
+    render: () => <span>Custom</span>
+  },
+  {
+    name: "Button",
+    items: [
+      {
+        name: "Submenu Text"
+      },
+      {
+        name: "Submenu Link",
+        href: "/"
+      },
+      {
+        name: "Submenu Custom",
+        render: () => <span>Submenu Custom</span>
+      },
+      {
+        name: "Submenu Button",
+        items: [
+          {
+            name: "Nested Submenu Text"
+          }
+        ]
+      }
+    ]
+  }
+];
+
+const customSecondaryMenu = [
+  {
+    name: "Text 2"
+  },
+  {
+    name: "Link 2",
+    href: "/"
+  },
+  {
+    name: "Custom 2",
+    render: () => <span>Custom 2</span>
+  },
+  {
+    name: "Button 2",
+    items: [
+      {
+        name: "Submenu Text 2"
+      },
+      {
+        name: "Submenu Link 2",
+        href: "/"
+      },
+      {
+        name: "Submenu Custom 2",
+        render: () => <span>Submenu Custom 2</span>
+      },
+      {
+        name: "Submenu Button 2",
+        items: [
+          {
+            name: "Nested Submenu Text 2"
+          }
+        ]
+      }
+    ]
+  }
+];
+
 const search = {
   onSubmit: () => {}
 };
 
-storiesOf("StoriesForTests/NavBar", module).add("Base", () => (
-  <NavBar menuData={{ primaryMenu, secondaryMenu, search }} />
-));
+storiesOf("StoriesForTests/NavBar", module)
+  .add("Base", () => <NavBar menuData={{ primaryMenu, secondaryMenu, search }} />)
+  .add("Custom Render Components", () => (
+    <NavBar defaultOpen menuData={{ primaryMenu: customPrimaryMenu, secondaryMenu: customSecondaryMenu }} />
+  ));
