0a649a9857168d75f4d8271c51a88d16f9bd26cf
Matt Dunn <mattd@nulogy.com>
2019-08-20 15:00:31 -0400

    removes old globalObject hack

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,7 @@ const path = require("path");
 module.exports = {
   target: "node", // https://github.com/emotion-js/emotion/issues/1113
   output: {
-    libraryTarget: "umd",
-    globalObject: "(typeof self !== 'undefined' ? self : this)"
-    // https://github.com/markdalgleish/static-site-generator-webpack-plugin/issues/130
+    libraryTarget: "umd"
   },
   externals: ["react", "react-dom", "styled-components"],
   module: {
