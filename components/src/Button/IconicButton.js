3ad759ad47ffb9ab1fa8ce8aaccdc88c59446ef3
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-08-20 14:37:33 -0400

    adds viewport boundariesElement for IconicButton

--- components/src/Button/IconicButton.js
+++ components/src/Button/IconicButton.js
@@ -81,7 +81,12 @@ const BaseIconicButton = React.forwardRef(({ children, icon, labelHidden, ...pro
   <WrapperButton ref={forwardedRef} aria-label={children} {...props}>
     <Manager>
       <Reference>{({ ref }) => <Icon ref={ref} size={theme.space.x4} icon={icon} p="half" />}</Reference>
-      <Popper placement="bottom">
+      <Popper
+        placement="bottom"
+        modifiers={{
+          preventOverflow: { enabled: true, padding: 8, boundariesElement: "viewport" }
+        }}
+      >
         {({ ref, style, placement }) =>
           labelHidden ? (
             <HoverText ref={ref} style={style} placement={placement}>
