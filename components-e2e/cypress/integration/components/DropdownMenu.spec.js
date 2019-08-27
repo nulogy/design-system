c87c7e3ef6e3e993951c22fa10114234bc0cc048
Matt Dunn <mattd@nulogy.com>
2019-08-27 12:25:27 -0400

    adds behaviour tests for DropdownMenu

---
+++ components-e2e/cypress/integration/components/DropdownMenu.spec.js
@@ -0,0 +1,81 @@
+describe("DropdownMenu", () => {
+  const getOpenButton = () => cy.get("[aria-label='Open menu']");
+  const getCloseButton = () => cy.get("[aria-label='Close menu']");
+  const getDropdownLink = () => cy.contains("Dropdown Link");
+  const getCustomTrigger = () => cy.get("button.customtrigger");
+  const assertDropdownIsOpen = () => getDropdownLink().should("exist");
+  const assertDropdownIsClosed = () => getDropdownLink().should("not.exist");
+  const enterKeyCode = 13;
+
+  it("toggles the menu with click", () => {
+    cy.renderFromStorybook("dropdownmenu--base");
+
+    getOpenButton().click();
+
+    assertDropdownIsOpen();
+
+    getCloseButton().click();
+
+    assertDropdownIsClosed();
+  });
+
+  it("toggles the menu with enter", () => {
+    cy.renderFromStorybook("dropdownmenu--base");
+
+    getOpenButton()
+      .click()
+      .trigger("keypress", {
+        keyCode: enterKeyCode,
+        which: enterKeyCode
+      });
+
+    assertDropdownIsOpen();
+
+    getCloseButton()
+      .click()
+      .trigger("keypress", {
+        keyCode: enterKeyCode,
+        which: enterKeyCode
+      });
+
+    assertDropdownIsClosed();
+  });
+
+  it("closes the menu when hitting escape", () => {
+    cy.renderFromStorybook("dropdownmenu--base");
+
+    getOpenButton().click();
+
+    cy.get("body").type("{esc}");
+
+    assertDropdownIsClosed();
+  });
+
+  it("closes the menu when clicking outside of it", () => {
+    cy.renderFromStorybook("dropdownmenu--base");
+
+    getOpenButton().click();
+
+    cy.get("body").click();
+
+    assertDropdownIsClosed();
+  });
+
+  it("scrolls through the list with tab", () => {
+    cy.renderFromStorybook("dropdownmenu--base");
+
+    getOpenButton()
+      .click()
+      .tab();
+
+    getDropdownLink().should("be.focused");
+  });
+
+  it("can be opened with a different element", () => {
+    cy.renderFromStorybook("dropdownmenu--custom-trigger");
+
+    getCustomTrigger().click();
+
+    assertDropdownIsOpen();
+  });
+});
