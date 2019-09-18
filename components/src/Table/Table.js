1eb17c03b86e587a79a045464be3c3e0b9871c28
Clemens Park <clemensp@nulogy.com>
2019-09-18 11:56:38 -0400

    NDS-1047: Add some NDS styling!

--- components/src/Table/Table.js
+++ components/src/Table/Table.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Table as RVTable, Column as RVColumn } from "react-virtualized";
 
+import { theme } from "..";
+
 const StyledTable = styled(RVTable)({
   ".ReactVirtualized__Table": {},
   ".ReactVirtualized__Table__Grid": {},
   ".ReactVirtualized__Table__headerRow": {
-    fontWeight: 700,
-    textTransform: "uppercase",
     display: "flex",
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    color: theme.colors.darkGrey,
+    borderBottom: `1px solid ${theme.colors.lightGrey}`
   },
   ".ReactVirtualized__Table__row": {
     display: "flex",
@@ -26,23 +28,19 @@ const StyledTable = styled(RVTable)({
     overflow: "hidden"
   },
   ".ReactVirtualized__Table__headerColumn": {
-    marginRight: "10px",
-    minWidth: "50px",
+    marginRight: `${theme.space.x2}`,
+    minWidth: `${theme.space.x6}`,
     textOverflow: "ellipsis",
     whiteSpace: "nowrap"
   },
   ".ReactVirtualized__Table__rowColumn": {
-    marginRight: "10px",
-    minWidth: "50px",
+    marginRight: `${theme.space.x2}`,
+    minWidth: `${theme.space.x6}`,
     textOverflow: "ellipsis",
     whiteSpace: "nowrap"
   },
-  ".ReactVirtualized__Table__headerColumn:first-of-type": {
-    marginLeft: "10px"
-  },
-  ".ReactVirtualized__Table__rowColumn:first-of-type": {
-    marginLeft: "10px"
-  }
+  ".ReactVirtualized__Table__headerColumn:first-of-type": {},
+  ".ReactVirtualized__Table__rowColumn:first-of-type": {}
 });
 
 const generateColumns = columns =>
@@ -54,8 +52,8 @@ const Table = ({ columns, rows }) => (
   <StyledTable
     width={500}
     height={500}
-    rowHeight={30}
-    headerHeight={20}
+    rowHeight={56}
+    headerHeight={56}
     rowGetter={rowGetter(rows)}
     rowCount={rows.length}
   >
