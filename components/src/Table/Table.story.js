908d26c050bc73585cfab9e5e3da807e0a82705d
Clemens Park <clemensp@nulogy.com>
2019-09-23 12:52:26 -0400

    NDS-1047: Remove the story that kills visual storyshots

--- components/src/Table/Table.story.js
+++ components/src/Table/Table.story.js
@@ -20,19 +20,9 @@ const columnsWithAlignment = [
 
 const rowData = [{ c1: "row 1 cell 1", c2: "r1c2", c3: "r1c3" }, { c1: "r2c1", c2: "r2c2", c3: "r2c3" }];
 
-const generateLotsOfRows = () => {
-  const rows = [];
-  for (let i = 0; i < 10000; i += 1) {
-    rows.push({ c1: `r${i}c1`, c2: `r${i}c2`, c3: `r${i}c3`, c4: `r${i}c4`, c5: `r${i}c5`, c6: `r${i}c6` });
-  }
-
-  return rows;
-};
-
 storiesOf("Table", module)
   .add("Table with data", () => <Table columns={columns} rows={rowData} />)
   .add("Cell alignment", () => <Table columns={columnsWithAlignment} rows={rowData} />)
-  // .add("with a lot of data", () => <Table columns={columns} rows={generateLotsOfRows()} />)
   .add("with no data", () => <Table columns={columns} rows={[]} />)
   .add("truncated", () => (
     <Box width={400}>
