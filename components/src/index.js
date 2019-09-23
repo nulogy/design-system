55bd7a393754652b869e96812dd60bb137f0a8ec
Clemens Park <clemensp@nulogy.com>
2019-09-23 12:20:06 -0400

    NDS-1047: Fix dependency ordering

--- components/src/index.js
+++ components/src/index.js
@@ -29,9 +29,6 @@ export { Card, CardSet, CPCard } from "./Card";
 export { Modal } from "./Modal";
 export { Tab, Tabs } from "./Tabs";
 
-export { DemoPage } from "./DemoPage";
-
-// Components listed after this comment have been automatically
-// added to this page and may need to be moved up in the list
-// if they are dependencies for other components
 export { Table } from "./Table";
+
+export { DemoPage } from "./DemoPage";
