8ac033bbfb22f1f8e9c99c08b1c7e51d8edc5548
Clemens Park <clemensp@nulogy.com>
2019-09-24 15:36:47 -0400

    NDS-1099: Add a test for `onChange` for regular selects

--- components/src/Select/Select.spec.js
+++ components/src/Select/Select.spec.js
@@ -2,7 +2,30 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import { Select } from ".";
 
-describe("select", () => {});
+describe("select", () => {
+  it("returns the selected item on change", () => {
+    const options = [
+      { label: "One", value: "one" },
+      { label: "Two", value: "two" },
+      { label: "Three", value: "three" }
+    ];
+
+    const callback = jest.fn();
+
+    const { container, queryByText } = render(
+      <Select
+        noOptionsMessage={() => "No options"}
+        options={options}
+        labelText="Select something"
+        onChange={callback}
+      />
+    );
+
+    selectOption("Two", container, queryByText);
+
+    expect(callback).toHaveBeenCalledWith("two");
+  });
+});
 
 describe("multi select", () => {
   it("returns the selected items on change", () => {
@@ -16,7 +39,6 @@ describe("multi select", () => {
 
     const { container, queryByText } = render(
       <Select
-        className="find-me"
         noOptionsMessage={() => "No options"}
         options={options}
         labelText="Select some things"
@@ -25,14 +47,18 @@ describe("multi select", () => {
       />
     );
 
-    expect(queryByText("Three")).toBeNull();
-
-    fireEvent.focus(container.querySelector("input"));
-    fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
-
-    expect(queryByText("Three")).not.toBeNull();
-    fireEvent.click(queryByText("Three"));
+    selectOption("Three", container, queryByText);
 
     expect(callback).toHaveBeenCalledWith(["three"]);
   });
 });
+
+function selectOption(text, container, queryByText) {
+  expect(queryByText(text)).toBeNull();
+
+  fireEvent.focus(container.querySelector("input"));
+  fireEvent.keyDown(container.querySelector("input"), { key: "ArrowDown", code: 40 });
+
+  expect(queryByText(text)).not.toBeNull();
+  fireEvent.click(queryByText(text));
+}
