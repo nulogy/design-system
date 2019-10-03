09fd7ea9ed08308d50ea37bb0d5fe862919cf752
Marianne Borowiak <marianneb@MarianneBs-MBPro.local>
2019-10-03 17:07:03 -0400

    NDS-1138: create basic html table component

---
+++ components/src/RVTable/Table.story.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Table } from ".";
+import { Box, IconicButton } from "..";
+
+const dateToString = ({ rowData, dataKey }) => {
+  const cellContent = rowData[dataKey];
+  return new Date(cellContent).toDateString();
+};
+
+// eslint-disable-next-line react/prop-types
+const customCellRenderer = ({ cellData }) => (
+  <>
+    <IconicButton icon="delete">{cellData}</IconicButton>
+  </>
+);
+
+const columns = [
+  { label: "Column 1", dataKey: "c1" },
+  { label: "Column 2", dataKey: "c2" },
+  { label: "Column 3", dataKey: "c3" },
+  { label: "Column 4", dataKey: "c4" },
+  { label: "Column 5", dataKey: "c5" },
+  { label: "Column 6", dataKey: "c6" }
+];
+
+const columnsWithCellRenderer = [
+  { label: "Column 1", dataKey: "c1" },
+  { label: "Column 2", dataKey: "c2" },
+  { label: "Column 3", dataKey: "c3", cellRenderer: customCellRenderer }
+];
+
+const columnsWithAlignment = [
+  { label: "Column 1", dataKey: "c1" },
+  { label: "Column 2", dataKey: "c2" },
+  { label: "Column 3", dataKey: "c3", align: "right" }
+];
+
+const columnsWithFormatter = [
+  { label: "Column 1", dataKey: "c1" },
+  { label: "Column 2", dataKey: "c2" },
+  { label: "Column 3", dataKey: "c3", cellFormatter: dateToString }
+];
+
+const rowData = [{ c1: "row 1 cell 1", c2: "r1c2", c3: "2019-09-21" }, { c1: "r2c1", c2: "r2c2", c3: "2019-09-22" }];
+
+storiesOf("RVTable", module)
+  .add("Table with data", () => <Table columns={columns} rows={rowData} />)
+  .add("Cell alignment", () => <Table columns={columnsWithAlignment} rows={rowData} />)
+  .add("with no data", () => <Table columns={columns} rows={[]} />)
+  .add("with a cell formatter", () => <Table columns={columnsWithFormatter} rows={rowData} />)
+  .add("with a custom component", () => <Table columns={columnsWithCellRenderer} rows={rowData} />)
+  .add("truncated", () => (
+    <Box width={400}>
+      <Table columns={columns} rows={rowData} />
+    </Box>
+  ));
--- components/src/Table/Table.js
+++ components/src/Table/Table.js
@@ -1,111 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { Table as RVTable, Column as RVColumn, AutoSizer, WindowScroller } from "react-virtualized";
 
 import theme from "../theme";
-import { Box } from "../Box";
-
-const StyledTable = styled(RVTable)({
-  ".ReactVirtualized__Table": {},
-  ".ReactVirtualized__Table__Grid": {},
-  ".ReactVirtualized__Table__headerRow": {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    color: theme.colors.darkGrey,
-    borderBottom: `1px solid ${theme.colors.lightGrey}`
-  },
-  ".ReactVirtualized__Table__row": {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  ".ReactVirtualized__Table__row:hover": {
-    backgroundColor: theme.colors.whiteGrey
-  },
-  ".ReactVirtualized__Table__headerTruncatedText": {
-    display: "inline-block",
-    maxWidth: "100%",
-    whiteSpace: "nowrap",
-    textOverflow: "ellipsis",
-    overflow: "hidden"
-  },
-  ".ReactVirtualized__Table__headerColumn": {
-    marginRight: theme.space.x2,
-    minWidth: theme.space.x6,
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap"
-  },
-  ".ReactVirtualized__Table__rowColumn": {
-    marginRight: theme.space.x2,
-    minWidth: theme.space.x6,
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap"
-  },
-  ".ReactVirtualized__Table__rowColumn.rowColumn--alignRight": {
-    textAlign: "right"
-  },
-  ".ReactVirtualized__Table__headerColumn:first-of-type": {
-    marginLeft: theme.space.x2
-  },
-  ".ReactVirtualized__Table__rowColumn:first-of-type": {
-    marginLeft: theme.space.x2
-  },
-  ".ReactVirtualized__Grid:focus": {
-    outline: "none"
-  }
-});
-
-const NoRowsContainer = styled(Box)({
-  padding: `${theme.space.x3} 0`,
-  fontSize: theme.fontSizes.small,
-  color: theme.colors.darkGrey
-});
-
-const generateColumns = columns =>
-  columns.map(({ label, dataKey, align, cellFormatter, cellRenderer }) => (
-    <RVColumn
-      className={align === "right" ? "rowColumn--alignRight" : undefined}
-      key={dataKey}
-      label={label}
-      dataKey={dataKey}
-      width={100}
-      flexGrow={1}
-      flexShrink={1}
-      cellDataGetter={cellFormatter}
-      cellRenderer={cellRenderer}
-    />
-  ));
-
-const rowGetter = rows => ({ index }) => rows[index];
-
-const ROW_HEIGHT = 56;
 
 const Table = ({ columns, rows, noRowsContent }) => (
-  <WindowScroller>
-    {({ height, isScrolling, onChildScroll, scrollTop }) => (
-      <AutoSizer disableHeight>
-        {({ width }) => (
-          <StyledTable
-            autoHeight
-            width={width}
-            height={height}
-            isScrolling={isScrolling}
-            onScroll={onChildScroll}
-            scrollTop={scrollTop}
-            rowHeight={ROW_HEIGHT}
-            headerHeight={ROW_HEIGHT}
-            rowGetter={rowGetter(rows)}
-            rowCount={rows.length}
-            noRowsRenderer={() => <NoRowsContainer>{noRowsContent}</NoRowsContainer>}
-          >
-            {generateColumns(columns)}
-          </StyledTable>
-        )}
-      </AutoSizer>
-    )}
-  </WindowScroller>
+  <table>
+    <tr>
+      {columns.map(({ label }) => (
+        <th>{label}</th>
+      ))}
+    </tr>
+    {rows.map(row => (
+      <tr>
+        {columns.map(({ dataKey }) => (
+          <td>{row[dataKey]}</td>
+        ))}
+      </tr>
+    ))}
+  </table>
 );
 Table.propTypes = {
   columns: PropTypes.arrayOf(
