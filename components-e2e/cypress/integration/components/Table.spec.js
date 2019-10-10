b5061645d9768e804b894d8ace9ebff274a5ac2e
Marianne Borowiak <marianneb@nulogy.com>
2019-10-10 17:08:21 -0400

    NDS-1137 clean up table spec

--- components-e2e/cypress/integration/components/Table.spec.js
+++ components-e2e/cypress/integration/components/Table.spec.js
@@ -1,13 +1,15 @@
 describe("Table", () => {
-  const selectAllCheckbox = () => cy.get("th input[type='checkbox']");
+  const getSelectAllCheckbox = () => cy.get("th input[type='checkbox']");
   it("toggles the value of the header checkbox on click", () => {
     cy.renderFromStorybook("table--with-preselected-rows");
-    cy.get("th input[type='checkbox']").click();
-    selectAllCheckbox().should("be.checked");
+
+    getSelectAllCheckbox().click();
+    getSelectAllCheckbox().should("be.checked");
   });
 
   it("selects a row on click", () => {
     cy.renderFromStorybook("table--with-preselected-rows");
+
     cy.get("tbody input[type='checkbox']")
       .first()
       .click();
@@ -18,10 +20,12 @@ describe("Table", () => {
 
   it("selects a row on click", () => {
     cy.renderFromStorybook("table--with-preselected-rows");
-    cy.get("th input[type='checkbox']").click();
-    selectAllCheckbox().should("be.checked");
+
+    getSelectAllCheckbox().click();
+    getSelectAllCheckbox().should("be.checked");
     cy.get("tbody input[type='checkbox']").should("be.checked");
-    cy.get("th input[type='checkbox']").click();
-    selectAllCheckbox().should("not.be.checked");
+
+    getSelectAllCheckbox().click();
+    getSelectAllCheckbox().should("not.be.checked");
   });
 });
