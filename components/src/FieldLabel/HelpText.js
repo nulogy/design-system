6f4b16cd214d76193f435ede4c36d9075a3b2f25
Matt Dunn <mattd@nulogy.com>
2019-10-17 09:13:38 -0400

    styles Links inside helpText to be 14px

--- components/src/FieldLabel/HelpText.js
+++ components/src/FieldLabel/HelpText.js
@@ -1,11 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import { Text } from "../Type";
+import { Link } from "../Link";
 import theme from "../theme";
 
-const HelpText = props => (
-  <Text fontSize={theme.fontSizes.small} lineHeight={theme.lineHeights.smallTextBase} {...props} />
-);
+const HelpTextContent = styled(Text)({
+  fontSize: theme.fontSizes.small,
+  lineHeight: theme.lineHeights.smallTextBase,
+
+  [`${Link}`]: {
+    fontSize: theme.fontSizes.small
+  }
+});
+
+const HelpText = props => <HelpTextContent {...props} />;
 
 HelpText.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
