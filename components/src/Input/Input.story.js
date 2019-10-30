2239aa2fca3fece061dd34f1d7ff1d2150c399bf
Marianne Borowiak <marboro92@gmail.com>
2019-10-30 14:03:15 -0400

    improves onChange documentation for inputs (#424)
    * improves onChange documentation for inputs
    
    * update storyshots

--- components/src/Input/Input.story.js
+++ components/src/Input/Input.story.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { Input } from "../index";
 
 const errorList = ["Error message 1", "Error message 2"];
@@ -12,10 +13,15 @@ storiesOf("Input", module)
       labelText="Input"
       helpText="Additional help text"
       requirementText="Required"
+      onChange={action("value changed")}
       required
     />
   ))
   .add("set to disabled", () => <Input labelText="Set to disabled" disabled />)
-  .add("with error message", () => <Input labelText="Label" errorMessage="Error message" />)
-  .add("with error list ", () => <Input labelText="Label" errorMessage="Error message" errorList={errorList} />)
-  .add("with custom ID", () => <Input id="my-own-id" labelText="Label" />);
+  .add("with error message", () => (
+    <Input labelText="Label" errorMessage="Error message" onChange={action("value changed")} />
+  ))
+  .add("with error list ", () => (
+    <Input labelText="Label" errorMessage="Error message" errorList={errorList} onChange={action("value changed")} />
+  ))
+  .add("with custom ID", () => <Input id="my-own-id" labelText="Label" onChange={action("value changed")} />);
--- components/src/Textarea/Textarea.story.js
+++ components/src/Textarea/Textarea.story.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { Textarea } from "../index";
 
 const errorList = ["Error message 1", "Error message 2"];
@@ -13,12 +14,20 @@ storiesOf("Textarea", module)
       helpText="here's some help..."
       requirementText="(Required)"
       required
+      onChange={action("value changed")}
     />
   ))
   .add("Set to disabled", () => <Textarea labelText="Label" disabled />)
-  .add("with error message", () => <Textarea labelText="Label" errorMessage="Please fill this out" />)
-  .add("with error list", () => (
-    <Textarea labelText="Label" errorMessage="Please fill this out" errorList={errorList} />
+  .add("with error message", () => (
+    <Textarea labelText="Label" errorMessage="Please fill this out" onChange={action("value changed")} />
   ))
-  .add("With custom number of rows", () => <Textarea labelText="Label" rows={7} />)
-  .add("With custom id", () => <Textarea id="my-custom-id" labelText="Label" />);
+  .add("with error list", () => (
+    <Textarea
+      labelText="Label"
+      errorMessage="Please fill this out"
+      errorList={errorList}
+      onChange={action("value changed")}
+    />
+  ))
+  .add("With custom number of rows", () => <Textarea labelText="Label" rows={7} onChange={action("value changed")} />)
+  .add("With custom id", () => <Textarea id="my-custom-id" labelText="Label" onChange={action("value changed")} />);
