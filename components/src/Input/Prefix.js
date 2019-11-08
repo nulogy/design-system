193095a55eb2feca2c7debaa4ccfd87c6b261c15
nikola-nulogy <nikolap@nulogy.com>
2019-11-08 11:44:21 -0500

    Resolved dependency cycle issue and updates storyshots

--- components/src/Input/Prefix.js
+++ components/src/Input/Prefix.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Box } from "../Box";
 import { Text } from "../Type";
 
-const Prefix = ({ prefix, prefixWidth, children, ...props }) =>
+const Prefix = ({ prefix, prefixWidth, ...props }) =>
   prefix && (
     <Box width={prefixWidth} pt="x1" pr="x1" pb="x1">
       <Text {...props}>{prefix}</Text>
