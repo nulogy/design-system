88e398737954a48ee39d64cd6b259d0990ab82b0
nikola-nulogy <nikolap@nulogy.com>
2019-11-08 11:21:35 -0500

    Moves prefix and suffix to input folder

--- components/src/FieldLabel/index.js
+++ components/src/FieldLabel/index.js
@@ -2,5 +2,3 @@ export { default as HelpText } from "./HelpText";
 export { default as RequirementText } from "./RequirementText";
 export { default as FieldLabel } from "./FieldLabel";
 export { default as MaybeFieldLabel } from "./MaybeFieldLabel";
-export { default as Suffix } from "./Suffix";
-export { default as Prefix } from "./Prefix";
