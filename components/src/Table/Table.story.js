121aa27d56cb56591725a3e6557cf5b024a115e4
Matt Dunn <mattd@nulogy.com>
2019-11-25 11:38:10 -0500

    reproduces table footer bug by removing ids from data

--- components/src/Table/Table.story.js
+++ components/src/Table/Table.story.js
@@ -89,14 +89,14 @@ const columnsWithEverything = [
 ];
 
 const rowData = [
-  { date: "2019-10-01", expectedQuantity: "2,025 eaches", actualQuantity: "1,800 eaches", id: "r1" },
-  { date: "2019-10-02", expectedQuantity: "2,475 eaches", actualQuantity: "2,250 eaches", id: "r2" },
-  { date: "2019-10-03", expectedQuantity: "2,475 eaches", actualQuantity: "1,425 eaches", id: "r3" },
-  { date: "2019-10-04", expectedQuantity: "2,475 eaches", actualQuantity: "675 eaches", id: "r4" },
-  { date: "2019-10-07", expectedQuantity: "2,475 eaches", actualQuantity: "1,575 eaches", id: "r5" },
-  { date: "2019-10-22", expectedQuantity: "1,725 eaches", actualQuantity: "-", id: "r6" },
-  { date: "2019-10-23", expectedQuantity: "2,475 eaches", actualQuantity: "-", id: "r7" },
-  { date: "2019-10-24", expectedQuantity: "2,475 eaches", actualQuantity: "-", id: "r8" }
+  { date: "2019-10-01", expectedQuantity: "2,025 eaches", actualQuantity: "1,800 eaches" },
+  { date: "2019-10-02", expectedQuantity: "2,475 eaches", actualQuantity: "2,250 eaches" },
+  { date: "2019-10-03", expectedQuantity: "2,475 eaches", actualQuantity: "1,425 eaches" },
+  { date: "2019-10-04", expectedQuantity: "2,475 eaches", actualQuantity: "675 eaches" },
+  { date: "2019-10-07", expectedQuantity: "2,475 eaches", actualQuantity: "1,575 eaches" },
+  { date: "2019-10-22", expectedQuantity: "1,725 eaches", actualQuantity: "-" },
+  { date: "2019-10-23", expectedQuantity: "2,475 eaches", actualQuantity: "-" },
+  { date: "2019-10-24", expectedQuantity: "2,475 eaches", actualQuantity: "-" }
 ];
 
 const rowDataWithWidths = [
@@ -158,8 +158,8 @@ const rowDataWithEverything = [
 ];
 
 const footerRowData = [
-  { date: "Total", expectedQuantity: "18,000 eaches", actualQuantity: "7,725 eaches", id: "r1" },
-  { date: "Attainment", expectedQuantity: "", actualQuantity: "41.5%", id: "r2" }
+  { date: "Total", expectedQuantity: "18,000 eaches", actualQuantity: "7,725 eaches" },
+  { date: "Attainment", expectedQuantity: "", actualQuantity: "41.5%" }
 ];
 
 storiesOf("Table", module)
