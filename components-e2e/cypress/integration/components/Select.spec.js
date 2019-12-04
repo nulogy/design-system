f1515d5026c06ad38700d4f12b4cb53943b50235
Marianne Borowiak <marianneb@nulogy.com>
2019-12-04 15:55:11 -0500

    test: add e2e tests for multiselect functionality

--- components-e2e/cypress/integration/components/Select.spec.js
+++ components-e2e/cypress/integration/components/Select.spec.js
@@ -2,8 +2,98 @@ describe("Select", () => {
   const getSelectComponent = () => cy.get(".Select");
   const getDropdownMenu = () => cy.get(".SelectTest__menu");
   const getValue = () => cy.get(".SelectTest__control");
+  const getSelectedItems = () => cy.get("div[class*='multiValue']");
+  const getCloseButtons = () =>
+    cy.get("div[class*='multiValue'] div:last-child");
+  const getClearButton = () =>
+    cy.get("div[class*='indicatorContainer']").first();
   const assertDropDownIsClosed = () => getDropdownMenu().should("not.exist");
   const assertDropDownIsOpen = () => getDropdownMenu().should("exist");
+
+  describe("Multiselect", () => {
+    beforeEach(() => {
+      cy.renderFromStorybook("select--multiselect");
+    });
+
+    it("has the correct initial state", () => {
+      getSelectedItems()
+        .first()
+        .should("have.text", "PCN2");
+      getSelectedItems()
+        .eq(1)
+        .should("have.text", "PCN1");
+      getSelectedItems().should("have.length", 2);
+    });
+
+    it("can select all values", () => {
+      getSelectComponent().click();
+
+      cy.focused().type("{enter}");
+
+      getSelectComponent().click();
+
+      cy.focused().type("{enter}");
+
+      getSelectedItems()
+        .eq(2)
+        .should("have.text", "PCN4");
+      getSelectedItems()
+        .eq(3)
+        .should("have.text", "PCN9");
+      getSelectedItems().should("have.length", 4);
+    });
+
+    it("selects the first item when opened", () => {
+      getSelectComponent().click();
+
+      cy.focused().type("{enter}");
+
+      getSelectedItems()
+        .eq(2)
+        .should("have.text", "PCN4");
+      getSelectedItems().should("have.length", 3);
+    });
+
+    it("removes a selected item", () => {
+      getSelectedItems().should("have.length", 2);
+
+      getCloseButtons()
+        .first()
+        .click();
+
+      getSelectedItems().should("have.length", 1);
+    });
+
+    it("removes using the keyboard", () => {
+      getSelectComponent().click();
+
+      cy.focused().type("{leftarrow}");
+
+      cy.focused().type("{del}");
+
+      getSelectedItems().should("have.length", 1);
+    });
+
+    it("removes all selected items", () => {
+      getSelectedItems().should("have.length", 2);
+
+      getCloseButtons()
+        .first()
+        .click();
+
+      getCloseButtons()
+        .first()
+        .click();
+
+      getSelectedItems().should("have.length", 0);
+    });
+
+    it("clears all selected items", () => {
+      getClearButton().click();
+
+      getSelectedItems().should("have.length", 0);
+    });
+  });
 
   it("selects the first item when opened", () => {
     cy.renderFromStorybook("select--base");
