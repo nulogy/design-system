fd0c879b3fd6e7e03237fb2753c76fd0315f0de3
Marianne Borowiak <marboro92@gmail.com>
2019-12-04 10:00:37 -0500

    improve toggle storybook examples with onChange action (#479)
    * improve toggle storybook examples with onChange action
    
    * update storyshots

--- components/src/Toggle/Toggle.story.js
+++ components/src/Toggle/Toggle.story.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { Toggle } from "../index";
 
 storiesOf("Toggle", module)
-  .add("Toggle", () => <Toggle />)
+  .add("Toggle", () => <Toggle onChange={action("on change")} />)
   .add("Toggle with all props", () => (
     <Toggle
       labelText="Toggle"
@@ -13,28 +14,42 @@ storiesOf("Toggle", module)
       defaultToggled
       required
       requirementText="(Required)"
+      onChange={action("on change")}
     />
   ))
-  .add("Toggle set to defaultToggled", () => <Toggle labelText="Toggle" defaultToggled />)
+  .add("Toggle set to defaultToggled", () => (
+    <Toggle labelText="Toggle" defaultToggled onChange={action("on change")} />
+  ))
   .add("Toggle set to disabled", () => (
     <>
-      <Toggle labelText="Toggle" disabled onText="on" offText="off" />
-      <Toggle id="toggle-2" disabled onText="on" offText="off" defaultToggled labelText="Toggle" />
+      <Toggle labelText="Toggle" disabled onText="on" offText="off" onChange={action("on change")} />
+      <Toggle
+        id="toggle-2"
+        disabled
+        onText="on"
+        offText="off"
+        defaultToggled
+        labelText="Toggle"
+        onChange={action("on change")}
+      />
     </>
   ))
-  .add("With custom id", () => <Toggle id="my-custom-id" labelText="Toggle" onText="on" offText="off" />)
-  .add("With text", () => <Toggle labelText="Toggle" onText="on" offText="off" />)
+  .add("With custom id", () => (
+    <Toggle id="my-custom-id" labelText="Toggle" onText="on" offText="off" onChange={action("on change")} />
+  ))
+  .add("With text", () => <Toggle labelText="Toggle" onText="on" offText="off" onChange={action("on change")} />)
   .add("With long text", () => (
     <Toggle
       labelText="Toggle"
       defaultToggled
       onText="this state has a very long text label to explain it's state"
       offText="not this one"
+      onChange={action("on change")}
     />
   ))
   .add("Controlled Toggle", () => (
     <>
-      <Toggle labelText="Toggle" toggled onText="on" offText="off" />
-      <Toggle labelText="Toggle" toggled={false} onText="on" offText="off" />
+      <Toggle labelText="Toggle" toggled onText="on" offText="off" onChange={action("on change")} />
+      <Toggle labelText="Toggle" toggled={false} onText="on" offText="off" onChange={action("on change")} />
     </>
   ));
