2dbda5fe8631f98e4eec22f84f04b800f98a2c31
Marianne Borowiak <marianneb@nulogy.com>
2019-12-09 11:33:58 -0500

    Use Input component as datepicker input

--- components/src/DatePicker/DatePicker.js
+++ components/src/DatePicker/DatePicker.js
@@ -4,7 +4,9 @@ import styled from "styled-components";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import theme from "../theme";
+import { Input } from "../Input";
 
+const DatePickerInput = ({ value, onClick }) => <Input labelText="Date" value={value} onClick={onClick} />;
 class DatePicker extends React.Component {
   state = {
     startDate: new Date()
@@ -18,7 +20,10 @@ class DatePicker extends React.Component {
 
   render() {
     const { startDate } = this.state;
-    return <ReactDatePicker selected={startDate} onChange={this.handleStartDateChange} />;
+
+    return (
+      <ReactDatePicker selected={startDate} onChange={this.handleStartDateChange} customInput={<DatePickerInput />} />
+    );
   }
 }
 
