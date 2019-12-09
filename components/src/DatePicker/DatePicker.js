f9b1400ac19ce2e920380c97519e6e7b0ff834ae
Marianne Borowiak <marianneb@nulogy.com>
2019-12-09 13:23:54 -0500

    add calandar icon to input

--- components/src/DatePicker/DatePicker.js
+++ components/src/DatePicker/DatePicker.js
@@ -5,8 +5,23 @@ import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import theme from "../theme";
 import { Input } from "../Input";
+import { Icon } from "../Icon";
 
-const DatePickerInput = ({ value, onClick }) => <Input labelText="Date" value={value} onClick={onClick} />;
+const StyledDateInputIcon = styled(Icon)({
+  position: "absolute",
+  right: "8px",
+  color: theme.colors.darkGrey,
+  top: "50%",
+  transform: "translateY(-50%)",
+  pointerEvents: "none"
+});
+
+const DatePickerInput = ({ value, onClick }) => (
+  <>
+    <Input value={value} onClick={onClick} />
+    <StyledDateInputIcon icon="calendarToday" size={theme.space.x2} />
+  </>
+);
 class DatePicker extends React.Component {
   state = {
     startDate: new Date()
