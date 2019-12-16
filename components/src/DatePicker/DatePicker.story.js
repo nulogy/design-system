4e6c6ffe7b874dd10972b67ece6f142a9aa85f0c
Marianne Borowiak <marianneb@nulogy.com>
2019-12-16 13:14:31 -0500

    skip storyshots

--- components/src/DatePicker/DatePicker.story.js
+++ components/src/DatePicker/DatePicker.story.js
@@ -4,7 +4,7 @@ import { action } from "@storybook/addon-actions";
 import { DatePicker } from ".";
 
 storiesOf("DatePicker", module)
-  .add("default", () => (
+  .add("default (SkipStoryshot)", () => (
     <DatePicker
       selected={new Date("2019-01-01T05:00:00.000Z")}
       onChange={action("date changed")}
@@ -12,7 +12,7 @@ storiesOf("DatePicker", module)
       inputProps={{ labelText: "Expiry Date" }}
     />
   ))
-  .add("with custom date format", () => (
+  .add("with custom date format (SkipStoryshot)", () => (
     <DatePicker
       selected={new Date("2019-01-01T05:00:00.000Z")}
       dateFormat="MMMM d, yyyy"
@@ -21,7 +21,7 @@ storiesOf("DatePicker", module)
       inputProps={{ labelText: "Expiry Date" }}
     />
   ))
-  .add("with custom placeholder", () => (
+  .add("with custom placeholder (SkipStoryshot)", () => (
     <DatePicker
       dateFormat="MMMM d, yyyy"
       onChange={action("date changed")}
@@ -29,7 +29,7 @@ storiesOf("DatePicker", module)
       inputProps={{ labelText: "Expiry Date", placeholder: "Month day, year" }}
     />
   ))
-  .add("with error state", () => (
+  .add("with error state (SkipStoryshot)", () => (
     <DatePicker
       dateFormat="MMMM d, yyyy"
       onChange={action("date changed")}
