03bb327bf68513570a446bca8203bdf3e1dea83f
Marianne Borowiak <marianneb@nulogy.com>
2020-01-02 16:35:10 -0500

    update storyshots

--- components/src/MonthRange/MonthRange.js
+++ components/src/MonthRange/MonthRange.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { isBefore } from "date-fns/esm";
+import { isBefore } from "date-fns";
 import { MonthPicker } from "../MonthPicker";
 import { Text } from "../Type";
 import { Flex } from "../Flex";
