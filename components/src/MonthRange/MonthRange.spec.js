c1467812d4753d24ca930c54378a7b8d194a5ffb
Marianne Borowiak <marianneb@nulogy.com>
2020-01-03 11:15:24 -0500

    add month range spec

---
+++ components/src/MonthRange/MonthRange.spec.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { MonthRange } from ".";
+
+describe("MonthRange", () => {
+  describe("range selection", () => {
+    it("returns the selected range when the range changes", () => {
+      const onRangeChange = jest.fn();
+      const { container } = render(<MonthRange onRangeChange={onRangeChange} />);
+      const startDateInput = container.querySelectorAll("input")[0];
+      fireEvent.click(startDateInput);
+      fireEvent.click(container.querySelectorAll(".react-datepicker__month-3")[0]);
+      const endDateInput = container.querySelectorAll("input")[1];
+      fireEvent.click(endDateInput);
+      fireEvent.click(container.querySelectorAll(".react-datepicker__month-5")[0]);
+      expect(onRangeChange).lastCalledWith({
+        startDate: new Date("2020-04-01T04:00:00.000Z"),
+        endDate: new Date("2020-06-01T04:00:00.000Z"),
+        error: undefined
+      });
+    });
+    it("returns the selected range with an error if the range is invalid", () => {
+      const onRangeChange = jest.fn();
+      const { container } = render(<MonthRange onRangeChange={onRangeChange} />);
+      const startDateInput = container.querySelectorAll("input")[0];
+      fireEvent.click(startDateInput);
+      fireEvent.click(container.querySelectorAll(".react-datepicker__month-5")[0]);
+      const endDateInput = container.querySelectorAll("input")[1];
+      fireEvent.click(endDateInput);
+      fireEvent.click(container.querySelectorAll(".react-datepicker__month-3")[0]);
+      expect(onRangeChange).lastCalledWith({
+        startDate: new Date("2020-06-01T04:00:00.000Z"),
+        endDate: new Date("2020-04-01T04:00:00.000Z"),
+        error: "End date is before start Month"
+      });
+    });
+    it("returns the start date when the start date changes", () => {
+      const onStartDateChange = jest.fn();
+      const onEndDateChange = jest.fn();
+      const { container } = render(
+        <MonthRange onStartDateChange={onStartDateChange} onEndDateChange={onEndDateChange} />
+      );
+      const startDateInput = container.querySelectorAll("input")[0];
+      fireEvent.click(startDateInput);
+      fireEvent.click(container.querySelectorAll(".react-datepicker__month-3")[0]);
+      expect(onStartDateChange).toHaveBeenCalledWith(new Date("2020-04-01T04:00:00.000Z"));
+      expect(onEndDateChange).not.toHaveBeenCalled();
+    });
+    it("returns the end date when the end date changes", () => {
+      const onStartDateChange = jest.fn();
+      const onEndDateChange = jest.fn();
+      const { container } = render(
+        <MonthRange onStartDateChange={onStartDateChange} onEndDateChange={onEndDateChange} />
+      );
+      const endDateInput = container.querySelectorAll("input")[1];
+      fireEvent.click(endDateInput);
+      fireEvent.click(container.querySelectorAll(".react-datepicker__month-10")[0]);
+      expect(onEndDateChange).toHaveBeenCalledWith(new Date("2020-11-01T04:00:00.000Z"));
+      expect(onStartDateChange).not.toHaveBeenCalled();
+    });
+  });
+});
