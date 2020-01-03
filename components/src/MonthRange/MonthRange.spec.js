e33c8028e1a86f72ddea1005a48eaa98e380b575
Marianne Borowiak <marianneb@nulogy.com>
2020-01-03 14:46:09 -0500

    Match only month and year of dates for month picker tests

--- components/src/MonthRange/MonthRange.spec.js
+++ components/src/MonthRange/MonthRange.spec.js
@@ -6,12 +6,29 @@ import { MonthRange } from ".";
 describe("MonthRange", () => {
   describe("range selection", () => {
     beforeEach(() => {
-      MockDate.set("2020-04-01T04:00:00.000Z", 0);
+      const now = new Date("2020-02-01T00:00:00.000Z");
+      MockDate.set(now);
+      expect.extend({
+        toMatchMonthAndYear(received, date) {
+          const pass = date.getMonth() === received.getMonth() && date.getYear() === received.getYear();
+          if (pass) {
+            return {
+              message: () => `expected ${received} not to match month and year of ${date}`,
+              pass: true
+            };
+          } else {
+            return {
+              message: () => `expected ${received} to match month and year of ${date}`,
+              pass: false
+            };
+          }
+        }
+      });
     });
     afterEach(() => {
       MockDate.reset();
     });
-    it("returns the selected range when the range changes", () => {
+    test("returns the selected range when the range changes", () => {
       const onRangeChange = jest.fn();
       const { container } = render(<MonthRange onRangeChange={onRangeChange} />);
       const startDateInput = container.querySelectorAll("input")[0];
@@ -20,11 +37,11 @@ describe("MonthRange", () => {
       const endDateInput = container.querySelectorAll("input")[1];
       fireEvent.click(endDateInput);
       fireEvent.click(container.querySelectorAll(".react-datepicker__month-5")[0]);
-      expect(onRangeChange).lastCalledWith({
-        startDate: new Date("2020-04-01T04:00:00.000Z"),
-        endDate: new Date("2020-06-01T04:00:00.000Z"),
-        error: undefined
-      });
+      const onChangeCalls = onRangeChange.mock.calls;
+      const latestCall = onChangeCalls[onChangeCalls.length - 1][0];
+      expect(latestCall.startDate).toMatchMonthAndYear(new Date("2020-04-01T04:00:00.000Z"));
+      expect(latestCall.endDate).toMatchMonthAndYear(new Date("2020-06-01T04:00:00.000Z"));
+      expect(latestCall.error).toBeUndefined();
     });
     it("returns the selected range with an error if the range is invalid", () => {
       const onRangeChange = jest.fn();
@@ -35,11 +52,11 @@ describe("MonthRange", () => {
       const endDateInput = container.querySelectorAll("input")[1];
       fireEvent.click(endDateInput);
       fireEvent.click(container.querySelectorAll(".react-datepicker__month-3")[0]);
-      expect(onRangeChange).lastCalledWith({
-        startDate: new Date("2020-06-01T04:00:00.000Z"),
-        endDate: new Date("2020-04-01T04:00:00.000Z"),
-        error: "End date is before start Month"
-      });
+      const onChangeCalls = onRangeChange.mock.calls;
+      const latestCall = onChangeCalls[onChangeCalls.length - 1][0];
+      expect(latestCall.startDate).toMatchMonthAndYear(new Date("2020-06-01T04:00:00.000Z"));
+      expect(latestCall.endDate).toMatchMonthAndYear(new Date("2020-04-01T04:00:00.000Z"));
+      expect(latestCall.error).toEqual("End date is before start Month");
     });
     it("returns the start date when the start date changes", () => {
       const onStartDateChange = jest.fn();
@@ -50,7 +67,9 @@ describe("MonthRange", () => {
       const startDateInput = container.querySelectorAll("input")[0];
       fireEvent.click(startDateInput);
       fireEvent.click(container.querySelectorAll(".react-datepicker__month-3")[0]);
-      expect(onStartDateChange).toHaveBeenCalledWith(new Date("2020-04-01T04:00:00.000Z"));
+      const onChangeCalls = onStartDateChange.mock.calls;
+      const latestCall = onChangeCalls[onChangeCalls.length - 1][0];
+      expect(latestCall).toMatchMonthAndYear(new Date("2020-04-01T04:00:00.000Z"));
       expect(onEndDateChange).not.toHaveBeenCalled();
     });
     it("returns the end date when the end date changes", () => {
@@ -62,7 +81,8 @@ describe("MonthRange", () => {
       const endDateInput = container.querySelectorAll("input")[1];
       fireEvent.click(endDateInput);
       fireEvent.click(container.querySelectorAll(".react-datepicker__month-10")[0]);
-      expect(onEndDateChange).toHaveBeenCalledWith(new Date("2020-11-01T04:00:00.000Z"));
+      const latestCall = onEndDateChange.mock.calls[0][0];
+      expect(latestCall).toMatchMonthAndYear(new Date("2020-11-01T04:00:00.000Z"));
       expect(onStartDateChange).not.toHaveBeenCalled();
     });
   });
