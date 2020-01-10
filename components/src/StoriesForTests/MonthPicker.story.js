398e7bc95c0ec5e6fa2304484e78b21248e02afe
Marianne Borowiak <marianneb@nulogy.com>
2020-01-10 11:21:55 -0500

    skip storyshots in storiesForTests

--- components/src/StoriesForTests/MonthPicker.story.js
+++ components/src/StoriesForTests/MonthPicker.story.js
@@ -3,8 +3,8 @@ import { storiesOf } from "@storybook/react";
 import { MonthPicker } from "../MonthPicker";
 
 storiesOf("StoriesForTests/Monthpicker", module)
-  .add("default", () => <MonthPicker selected={new Date("ri, 01 Jan 2019")} />)
-  .add("with a min and max date", () => (
+  .add("default (SkipStoryshots)", () => <MonthPicker selected={new Date("ri, 01 Jan 2019")} />)
+  .add("with a min and max date (SkipStoryshots)", () => (
     <MonthPicker
       selected={new Date("2019-07-10T05:00:00.000Z")}
       minDate={new Date("2019-07-05T05:00:00.000Z")}
