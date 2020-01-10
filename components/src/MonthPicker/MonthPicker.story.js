920c74cde1e915675961210159e9586677aa015b
Marianne Borowiak <marianneb@nulogy.com>
2020-01-10 10:29:07 -0500

    add rest of the date component storyshots

--- components/src/MonthPicker/MonthPicker.story.js
+++ components/src/MonthPicker/MonthPicker.story.js
@@ -4,7 +4,7 @@ import { action } from "@storybook/addon-actions";
 import { MonthPicker } from ".";
 
 storiesOf("MonthPicker", module)
-  .add("default (SkipStoryshot)", () => (
+  .add("default", () => (
     <MonthPicker
       selected={new Date("2019-01-01T05:00:00.000Z")}
       onChange={action("date changed")}
@@ -12,14 +12,14 @@ storiesOf("MonthPicker", module)
       inputProps={{ labelText: "Month" }}
     />
   ))
-  .add("with custom placeholder (SkipStoryshot)", () => (
+  .add("with custom placeholder", () => (
     <MonthPicker
       onChange={action("date changed")}
       onInputChange={action("input changed")}
       inputProps={{ labelText: "Month and Year", placeholder: "Month, year" }}
     />
   ))
-  .add("with error state (SkipStoryshot)", () => (
+  .add("with error state", () => (
     <MonthPicker
       dateFormat="yyyy MMM"
       onChange={action("date changed")}
@@ -28,7 +28,7 @@ storiesOf("MonthPicker", module)
       errorMessage="The date is invalid"
     />
   ))
-  .add("with a min and max date(SkipStoryshot)", () => (
+  .add("with a min and max date", () => (
     <MonthPicker
       selected={new Date("2019-07-10T05:00:00.000Z")}
       minDate={new Date("2019-07-05T05:00:00.000Z")}
--- components/src/MonthRange/MonthRange.story.js
+++ components/src/MonthRange/MonthRange.story.js
@@ -5,14 +5,14 @@ import { action } from "@storybook/addon-actions";
 import MonthRange from "./MonthRange";
 
 storiesOf("MonthRange", module)
-  .add("default (SkipStoryshot)", () => (
+  .add("default", () => (
     <MonthRange
       onRangeChange={action("range changed")}
       onStartDateChange={action("start date changed")}
       onEndDateChange={action("end date changed")}
     />
   ))
-  .add("default start and end date (SkipStoryshot)", () => (
+  .add("default start and end date", () => (
     <MonthRange
       defaultStartDate={new Date("2019-07-05T05:00:00.000Z")}
       defaultEndDate={new Date("2019-09-10T05:00:00.000Z")}
@@ -21,7 +21,7 @@ storiesOf("MonthRange", module)
       onEndDateChange={action("end date changed")}
     />
   ))
-  .add("disabled range validation (SkipStoryshot)", () => (
+  .add("disabled range validation", () => (
     <MonthRange
       disableRangeValidation
       defaultEndDate={new Date("2019-07-05T05:00:00.000Z")}
@@ -31,7 +31,7 @@ storiesOf("MonthRange", module)
       onEndDateChange={action("end date changed")}
     />
   ))
-  .add("with custom error (SkipStoryshot)", () => (
+  .add("with custom error", () => (
     <MonthRange
       errorMessage="This range conflicts with another range"
       defaultStartDate={new Date("2019-07-05T05:00:00.000Z")}
@@ -41,7 +41,7 @@ storiesOf("MonthRange", module)
       onEndDateChange={action("end date changed")}
     />
   ))
-  .add("customizing input props (SkipStoryshot)", () => (
+  .add("customizing input props", () => (
     <MonthRange
       startDateInputProps={{ placeholder: "From (Mon YYYY)" }}
       endDateInputProps={{ placeholder: "To (Mon YYYY)" }}
@@ -50,7 +50,7 @@ storiesOf("MonthRange", module)
       onEndDateChange={action("end date changed")}
     />
   ))
-  .add("individual input error (SkipStoryshot)", () => (
+  .add("individual input error", () => (
     <MonthRange
       startDateErrorMessage="Start date is required"
       startDateInputProps={{ required: true }}
