ec1ef4fa98e2d1072c3aa50931c3aa23b87e7f48
Marianne Borowiak <marianneb@nulogy.com>
2020-01-21 14:53:13 -0500

    Add alert e2e test

---
+++ components-e2e/cypress/integration/components/Alert.spec.js
@@ -0,0 +1,14 @@
+describe("Alert", () => {
+  beforeEach(() => {
+    cy.renderFromStorybook("alert--with-a-close-button");
+  });
+  it("shows an alert", () => {
+    cy.get('[role="alert"]').should("be.visible");
+    cy.isInViewport('[role="alert"]');
+  });
+  it("hides the alert when closed", () => {
+    cy.get('[aria-label="Close"]').click();
+    cy.get('[role="alert"]').should("not.be.visible");
+    cy.get('[role="alert"]').should("not.exist");
+  });
+});
