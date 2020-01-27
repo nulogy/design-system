23f41ef8533d9253b79fd665057b4ffa687c3757
Marianne Borowiak <marianneb@nulogy.com>
2020-01-27 13:19:26 -0500

    change from console.warn to error

--- components/src/utils/deprecatedProp.js
+++ components/src/utils/deprecatedProp.js
@@ -3,7 +3,7 @@ export const deprecatedProp = (propType, newPropName) => {
   return (props, propName, componentName, ...rest) => {
     if (props[propName] != null) {
       const message = `NDS Warning: "${propName}" prop of "${componentName}" has been deprecated.\n Please use the "${newPropName}" prop instead. If you need assistance upgrading please message #design-system`;
-      console.warn(message);
+      console.error(message);
     }
 
     return propType(props, propName, componentName, ...rest);
