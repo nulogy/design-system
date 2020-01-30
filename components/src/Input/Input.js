8db5d300b9ec26dbe619af118749c7b7c80fb178
Matt Dunn <mattd@nulogy.com>
2020-01-30 11:14:00 -0500

    removes unnecessary propTypes from Input

--- components/src/Input/Input.js
+++ components/src/Input/Input.js
@@ -1,6 +1,5 @@
 import React, { forwardRef } from "react";
 import PropTypes from "prop-types";
-import propTypes from "@styled-system/prop-types";
 import { Field } from "../Form";
 import { InputField } from "./InputField";
 import { InlineValidation } from "../Validation";
@@ -17,8 +16,7 @@ Input.propTypes = {
   className: PropTypes.string,
   errorMessage: PropTypes.string,
   errorList: PropTypes.arrayOf(PropTypes.string),
-  ...InputFieldPropTypes,
-  ...propTypes.space
+  ...InputFieldPropTypes
 };
 
 Input.defaultProps = {
