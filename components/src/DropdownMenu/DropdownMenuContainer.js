813e52e59f23dde48e19a1b5518fa124b64c41fa
Marianne Borowiak <marianneb@nulogy.com>
2020-02-03 13:01:39 -0500

    fix eslint errors and update generateId spec

--- components/src/DropdownMenu/DropdownMenuContainer.js
+++ components/src/DropdownMenu/DropdownMenuContainer.js
@@ -1,8 +1,6 @@
-import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import theme from "../theme";
-import { PopperArrow } from "../utils";
 import { Box } from "../Box";
 
 const getThemeColor = color => (theme.colors[color] ? theme.colors[color] : color);
