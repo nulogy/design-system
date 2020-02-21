d7f9693f903ab2a485eba98bd357aef674ea2e97
Marianne Borowiak <marianneb@nulogy.com>
2020-02-21 12:20:18 -0500

    set up translations in json files

--- components/src/i18n.js
+++ components/src/i18n.js
@@ -1,32 +1,26 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import en from "../locales/en.json";
+import fr from "../locales/fr.json";
 
 // the translations
 // (tip move them in a JSON file and import them)
 const resources = {
   en: {
-    translation: {
-      "Welcome to React": "Welcome to React and react-i18next"
-    }
+    translation: en
   },
-  es: {
-    translation: {
-      "Welcome to React": "Hola"
-    }
+  fr: {
+    translation: fr
   }
 };
+i18n.init({
+  resources,
+  lng: "en",
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-    lng: "en",
+  keySeparator: false, // we do not use keys in form messages.welcome
 
-    keySeparator: false, // we do not use keys in form messages.welcome
-
-    interpolation: {
-      escapeValue: false // react already safes from xss
-    }
-  });
+  interpolation: {
+    escapeValue: false // react already safes from xss
+  }
+});
 
 export default i18n;
