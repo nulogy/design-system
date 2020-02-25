4113fa128df4ace97d5d4e59ad0a8d6356548ac1
Matt Dunn <mattd@nulogy.com>
2020-02-25 08:50:08 -0500

    adds Card component

---
+++ css/src/scss/components/_card.story.js
@@ -0,0 +1,9 @@
+/* global document */
+import { storiesOf } from "@storybook/html";
+
+storiesOf("Components|Card", module).add(
+  "Card",
+  () => `
+    <div class="Card">I am a card.</div>
+`
+);
