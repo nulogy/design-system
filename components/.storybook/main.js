4d3319986b27c9ed33650906d107f5f0cb4fd863
Matt Dunn <mattd@nulogy.com>
2020-02-25 10:32:10 -0500

    moves addons and story loading to new main.js file

---
+++ components/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ["../src/**/*.story.js"],
+  addons: [
+    "@storybook/addon-knobs",
+    "@storybook/addon-viewport",
+    "@storybook/addon-storysource",
+    "@storybook/addon-a11y",
+    "@storybook/addon-actions"
+  ]
+};
