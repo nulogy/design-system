c08dafdeca2bface411c09d62829d2c7c62d296c
Marianne Borowiak <marianneb@nulogy.com>
2020-02-26 14:23:11 -0500

    update storyshots

--- components/src/TimeRange/TimeRange.js
+++ components/src/TimeRange/TimeRange.js
@@ -41,13 +41,12 @@ const TimeRange = ({
   };
   const validateTimeRange = () => {
     let error;
-    const { t } = useTranslation();
     const end = endTime || defaultEndTime;
     const start = startTime || defaultStartTime;
     if (start && end) {
       const duration = getDuration(start, end);
       if (duration < 0) {
-        error = t("end time is before start time");
+        error = "end time is before start time";
       }
     }
     setRangeError(error);
