1c93f3469dc288f1ff72553e1d80507fee81ceaf
Marianne Borowiak <marianneb@nulogy.com>
2020-03-03 14:57:26 -0500

    fix type on file name

--- components/src/i18n.js
+++ components/src/i18n.js
@@ -5,7 +5,7 @@ import en_US from "../locales/en_US.json";
 import es_MX from "../locales/es_MX.json";
 import fr_FR from "../locales/fr_FR.json";
 import nl_NL from "../locales/nl_NL.json";
-import pl_PL from "../locales/pl_pL.json";
+import pl_PL from "../locales/pl_PL.json";
 import pt_BR from "../locales/pt_BR.json";
 import ro_RO from "../locales/ro_RO.json";
 /* eslint-enable */
