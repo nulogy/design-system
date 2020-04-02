7b594694bd89fd45a814cc3c4d38295412520987
Matt Dunn <mattd@nulogy.com>
2020-04-02 14:31:49 -0400

    feat: adds BEM notation to buttons

--- css/src/scss/components/_buttons.story.js
+++ css/src/scss/components/_buttons.story.js
@@ -23,9 +23,9 @@ storiesOf("Components|Buttons", module).add(
 
     <div style="padding: 24px">
     <h1 class="SubsectionTitle" style="margin-bottom: 1rem;">Sizes</h1>
-        <button class="Button small">Small</button>
+        <button class="Button Button--small">Small</button>
         <button class="Button">Default</button>
-        <button class="Button large">Large</button>
+        <button class="Button Button--large">Large</button>
     </div>
 `
 );
