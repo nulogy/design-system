112cb4705a522f9733e25f8bc0a44dc1755aa7a5
Marianne Borowiak <marianneb@nulogy.com>
2020-04-08 16:19:44 -0400

    add disabled styles to toggle

--- css/src/scss/components/_toggle.story.js
+++ css/src/scss/components/_toggle.story.js
@@ -8,5 +8,15 @@ storiesOf("Components|Toggle", module).add(
     <span class="Toggle__slider"></span>
     <span class="Toggle__text">I am a toggle</span>
   </label>
+  <label class="Toggle">
+    <input type="checkbox" disabled class="Toggle__input">
+    <span class="Toggle__slider"></span>
+    <span class="Toggle__text">I am a  disabled toggle</span>
+  </label>
+  <label class="Toggle">
+    <input type="checkbox" disabled checked class="Toggle__input">
+    <span class="Toggle__slider"></span>
+    <span class="Toggle__text">I am a  disabled, checked toggle</span>
+  </label>
   `
 );
