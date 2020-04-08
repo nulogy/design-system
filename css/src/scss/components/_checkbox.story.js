eb5ef775e6f804bf4784c3e23961e928f2dfdad1
Matt Dunn <mattd@nulogy.com>
2020-04-08 15:38:27 -0400

    adds legends for groups of options

--- css/src/scss/components/_checkbox.story.js
+++ css/src/scss/components/_checkbox.story.js
@@ -4,22 +4,27 @@ storiesOf("Components|Checkbox", module)
   .add(
     "Checkbox",
     () => `
-  <label class="Checkbox">
-    <input type="checkbox" class="Checkbox__input">
-    <p class="Checkbox__text">I am a checkbox</p>
-  </label>
-  <label class="Checkbox">
-    <input type="checkbox" checked class="Checkbox__input">
-    <p class="Checkbox__text">I am a checkbox</p>
-  </label>
-  <label class="Checkbox">
-    <input type="checkbox" disabled class="Checkbox__input">
-    <p class="Checkbox__text">I am disabled</p>
-  </label>
-  <label class="Checkbox">
-    <input type="checkbox" checked disabled class="Checkbox__input">
-    <p class="Checkbox__text">I am disabled</p>
-  </label>
+
+  <fieldset>
+    <legend class="Label"> I'm a label for the checkboxes</legend>
+    <label class="Checkbox">
+      <input type="checkbox" class="Checkbox__input">
+      <p class="Checkbox__text">I am a checkbox</p>
+    </label>
+    <label class="Checkbox">
+      <input type="checkbox" checked class="Checkbox__input">
+      <p class="Checkbox__text">I am a checkbox</p>
+    </label>
+    <label class="Checkbox">
+      <input type="checkbox" disabled class="Checkbox__input">
+      <p class="Checkbox__text">I am disabled</p>
+    </label>
+    <label class="Checkbox">
+      <input type="checkbox" checked disabled class="Checkbox__input">
+      <p class="Checkbox__text">I am disabled</p>
+    </label>
+  </fieldset>
+
 `
   )
   .add(
