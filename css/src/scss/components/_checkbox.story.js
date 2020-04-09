82bca5ba08fc140cd3e7470f838fdd1f8786cea4
Marianne Borowiak <marianneb@nulogy.com>
2020-04-09 14:19:22 -0400

    use Form class in example to reset fieldset

--- css/src/scss/components/_checkbox.story.js
+++ css/src/scss/components/_checkbox.story.js
@@ -4,27 +4,27 @@ storiesOf("Components|Checkbox", module)
   .add(
     "Checkbox",
     () => `
-
-  <fieldset>
-    <legend class="Label"> I'm a label for the checkboxes</legend>
-    <label class="Checkbox">
-      <input type="checkbox" class="Checkbox__input">
-      <span class="Checkbox__text">I am a checkbox</span>
-    </label>
-    <label class="Checkbox">
-      <input type="checkbox" checked class="Checkbox__input">
-      <span class="Checkbox__text">I am a checkbox</span>
-    </label>
-    <label class="Checkbox">
-      <input type="checkbox" disabled class="Checkbox__input">
-      <span class="Checkbox__text">I am disabled</span>
-    </label>
-    <label class="Checkbox">
-      <input type="checkbox" checked disabled class="Checkbox__input">
-      <span class="Checkbox__text">I am disabled</span>
-    </label>
-  </fieldset>
-
+      <form class="Form">
+        <fieldset>
+          <legend class="Label"> I'm a label for the checkboxes</legend>
+          <label class="Checkbox">
+            <input type="checkbox" class="Checkbox__input">
+            <span class="Checkbox__text">I am a checkbox</span>
+          </label>
+          <label class="Checkbox">
+            <input type="checkbox" checked class="Checkbox__input">
+            <span class="Checkbox__text">I am a checkbox</span>
+          </label>
+          <label class="Checkbox">
+            <input type="checkbox" disabled class="Checkbox__input">
+            <span class="Checkbox__text">I am disabled</span>
+          </label>
+          <label class="Checkbox">
+            <input type="checkbox" checked disabled class="Checkbox__input">
+            <span class="Checkbox__text">I am disabled</span>
+          </label>
+        </fieldset>
+      </form>
 `
   )
   .add(
