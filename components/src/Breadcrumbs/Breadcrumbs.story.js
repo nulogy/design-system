e574006281ee2c1a1a5ec60136e955e444c3379c
Marianne Borowiak <marianneb@nulogy.com>
2020-04-13 13:56:56 -0400

    bread crumb text example

--- components/src/Breadcrumbs/Breadcrumbs.story.js
+++ components/src/Breadcrumbs/Breadcrumbs.story.js
@@ -2,10 +2,19 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Breadcrumbs } from "./index";
 import { Link } from "../Link";
+import { Text } from "../Type";
 
-storiesOf("Breadcrumbs", module).add("Breadcrumbs", () => (
-  <Breadcrumbs>
-    <Link href="/">Home</Link>
-    <Link href="/Tenants">Tenants</Link>
-  </Breadcrumbs>
-));
+storiesOf("Breadcrumbs", module)
+  .add("Breadcrumbs", () => (
+    <Breadcrumbs>
+      <Link href="/">Home</Link>
+      <Link href="/Tenants">Tenants</Link>
+    </Breadcrumbs>
+  ))
+  .add("without link", () => (
+    <Breadcrumbs>
+      <Link href="/">Home</Link>
+      <Link href="/Tenants">Tenants</Link>
+      <Text>Current Tenant</Text>
+    </Breadcrumbs>
+  ));
