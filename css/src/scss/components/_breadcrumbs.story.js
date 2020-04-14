e6a9744a8c049d804008462dec92b35b097280f7
Marianne Borowiak <marboro92@gmail.com>
2020-04-14 13:27:43 -0400

    feat: add css breadcrumb (#627)

---
+++ css/src/scss/components/_breadcrumbs.story.js
@@ -0,0 +1,18 @@
+/* global document */
+import { storiesOf } from "@storybook/html";
+
+storiesOf("Components|Breadcrumbs", module).add(
+  "Breadcrumbs",
+  () => `
+    <nav class="Breadcrumbs">
+      <ol>
+        <li class="Breadcrumbs__item"><a href="/">Home</a></li>
+        <li><i class="material-icons">keyboard_arrow_right</i><li>
+        <li class="Breadcrumbs__item"><a class="Breadcrumbs__item"href="/Tenants">Tenants</a></li>
+        <li><i class="material-icons">keyboard_arrow_right</i><li>
+        <li class="Breadcrumbs__item">Current Tenant<li>
+        <li><i class="material-icons">keyboard_arrow_right</i><li>
+      </ol>
+    </nav>
+`
+);
