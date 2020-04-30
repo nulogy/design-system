5dd2ed934aa936d3cda7581d2468adac5ae7a5eb
Marianne Borowiak <marianneb@nulogy.com>
2020-04-30 13:47:23 -0400

    add comments and babel runtime optimization

--- components/rollup.config.js
+++ components/rollup.config.js
@@ -1,12 +1,11 @@
-import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import replace from "rollup-plugin-replace";
 
 import packageJson from "./package.json";
 
-const EXTENSIONS = [".js", ".jsx"];
+/* Rollup outputs module and main bundles for the @nulogy/components package */
 
 const GLOBALS = {
   react: "React",
@@ -18,21 +17,32 @@ const GLOBALS = {
 const externals = Object.keys(GLOBALS);
 
 const CORE_PLUGINS = [
+  /* commonJS: convert CommonJS modules to ES6,
+  so they can be included in a Rollup bundle */
   commonjs({
+    /* include: include all items in node_modules folders (in entire monorepo) */
     include: [/node_modules/],
+    /* namedExports: sometimes commonjs can't resolve named exports from certain libraries, 
+    ex: import {exportName} from "package-name"; => exportName module not found
+    in those cases, it needs to be added as ["package-name"]: "exportName" here */
     namedExports: {
-      "../node_modules/debounce/index.js": ["debounce"],
-      "../node_modules/react-windowed-select/lib/index.js": ["components"]
+      debounce: ["debounce"],
+      "react-windowed-select": ["components"]
     }
   }),
+  /* babel: transiles the bundle according to babel.config */
   babel({
-    runtimeHelpers: true,
-    exclude: "./node_modules/**/*",
-    extensions: EXTENSIONS
+    /* babelHelpers: runtime tells babel to import from @babel/runtime instead of duplicated them */
+    babelHelpers: "runtime",
+    /* exclude: globs to exclude */
+    exclude: ["./node_modules/**/*", "../node_modules/**/*"],
+    /* exclude: files to be transpiled by babel */
+    extensions: [".js", ".jsx"]
   }),
-  json(),
+  /* replace: replaces strings when bundling */
   replace({
     exclude: /node_modules/,
+    /* ENV is replaced by environment setting */
     ENV: JSON.stringify(process.env.NODE_ENV || "development")
   })
 ];
@@ -40,15 +50,20 @@ const CORE_PLUGINS = [
 const ENTRY_FILE = "src/index.js";
 
 const mainBundles = {
+  /* input: main export file */
   input: ENTRY_FILE,
+  /* external: external dependencies, all peerDependencies */
   external: externals,
   output: [
+    // UMD format for compatibility with most script loaders
     {
       file: packageJson.main,
       name: "NDSComponents",
       format: "umd",
+      // globals: global variable names of external dependencies
       globals: GLOBALS
     },
+    // ES module format for package.module field, auto-imports and optimal tree-shaking
     {
       file: packageJson.module,
       format: "es",
@@ -56,9 +71,14 @@ const mainBundles = {
     }
   ],
   plugins: [
+    // resolve: resolves node_modules imports
     resolve({
+      /* mainFields: specifies the which package fields to look for first
+      package.module and then if it's not specified look at
+      package.main */
       mainFields: ["module", "main"],
-      extensions: EXTENSIONS
+      /* extensions: specifies the file extensions to accept as imports */
+      extensions: [".js", ".jsx"]
     }),
     ...CORE_PLUGINS
   ]
