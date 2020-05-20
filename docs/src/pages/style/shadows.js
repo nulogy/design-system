9ade8e8ca8960c4b7e6da63ec2c09523e50ae9f4
Matt Dunn <mattd@nulogy.com>
2020-05-20 13:37:31 -0400

    adds more information to shadows page

--- docs/src/pages/style/shadows.js
+++ docs/src/pages/style/shadows.js
@@ -6,9 +6,11 @@ import {
   ListItem,
   Link,
   SectionTitle,
-  Title
+  Title,
+  Text
 } from "@nulogy/components";
 import { Layout, Intro, IntroText, DocSection } from "../../components";
+import theme from "../../../../components/src/theme";
 
 export default () => (
   <Layout>
@@ -25,15 +27,54 @@ export default () => (
     <DocSection>
       <Box bg="lightGrey" p="x6">
         <Box p="x2" bg="white" boxShadow="small" mb="x2">
-          Small
+          <Text fontWeight="bold">Small</Text>
+          <Text fontFamily="mono" fontSize="smaller">
+            {theme.shadows.small}
+          </Text>
         </Box>
         <Box py="x4" px="x2" bg="white" boxShadow="medium" mb="x2">
-          Medium
+          <Text fontWeight="bold">Medium</Text>
+          <Text fontFamily="mono" fontSize="smaller">
+            {theme.shadows.medium}
+          </Text>
         </Box>
         <Box py="x8" px="x2" bg="white" boxShadow="large">
-          Large
+          <Text fontWeight="bold">Large</Text>
+          <Text fontFamily="mono" fontSize="smaller">
+            {theme.shadows.large}
+          </Text>
         </Box>
       </Box>
+    </DocSection>
+
+    <DocSection>
+      <SectionTitle>States</SectionTitle>
+      <Box p="x2" bg="white" boxShadow="focus" mb="x2" borderRadius="medium">
+        <Text fontWeight="bold">Focus</Text>
+        <Text fontFamily="mono" fontSize="smaller">
+          {theme.shadows.focus}
+        </Text>
+      </Box>
+      <Box p="x2" bg="white" boxShadow="error" mb="x2" borderRadius="medium">
+        <Text fontWeight="bold">Error</Text>
+        <Text fontFamily="mono" fontSize="smaller">
+          {theme.shadows.error}
+        </Text>
+      </Box>
+    </DocSection>
+
+    <DocSection>
+      <SectionTitle>Usage</SectionTitle>
+      <Text>
+        Shadows can be added to the{" "}
+        <Link href="/components/box">Box component</Link> using the
+        <Text inline fontFamily="mono">
+          boxShadow
+        </Text>{" "}
+        prop. Focus and Error states come built-in to inputs already, and all of
+        our shadow values are stored in the <Link href="/theme/">theme</Link>{" "}
+        for referencing anywhere else.
+      </Text>
     </DocSection>
 
     <DocSection>
