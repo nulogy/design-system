48a8ef0affa4fa3d24497218169a214113ae0ca5
Marianne Borowiak <marianneb@nulogy.com>
2020-06-03 15:44:04 -0400

    add overlay css component

---
+++ css/src/scss/components/_overlay.story.js
@@ -0,0 +1,20 @@
+/* global document */
+import { storiesOf } from "@storybook/html";
+
+storiesOf("Components|Overlay", module)
+  .add(
+    "Overlay",
+    () => `
+    <p>Some content behind the overlay</p>
+    <div class="Overlay">
+    </div>
+`
+  )
+  .add(
+    "Light Overlay",
+    () => `
+    <p>Some content behind the overlay</p>
+    <div class="Overlay Overlay--light">
+    </div>
+`
+  );
