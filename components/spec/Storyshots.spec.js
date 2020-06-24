0a736acd9ced2b0e9504ffa88757435808326d1e
Marianne Borowiak <marianneb@nulogy.com>
2020-06-24 11:45:30 -0400

    show styles in storyshots

--- components/spec/Storyshots.spec.js
+++ components/spec/Storyshots.spec.js
@@ -1,17 +1,23 @@
 import initStoryshots, { multiSnapshotWithOptions } from "@storybook/addon-storyshots";
 import { render } from "enzyme";
 import { createSerializer } from "enzyme-to-json";
+import { styleSheetSerializer } from "jest-styled-components/serializer";
 import "jest-styled-components";
+import { addSerializer } from "jest-specific-snapshot";
+
+addSerializer(styleSheetSerializer);
 
 // mock createPortal since it is used by react-modal and not supported by the test renderer
-jest.mock("react-dom", () => ({
-  createPortal: node => node
-}));
+jest.mock("react-dom", () => {
+  const original = jest.requireActual("react-dom");
+  return {
+    ...original,
+    createPortal: node => node
+  };
+});
 
 initStoryshots({
   storyNameRegex: /^((?!.*?SkipStoryshot).)*$/,
   snapshotSerializers: [createSerializer()],
-  test: multiSnapshotWithOptions({
-    renderer: render
-  })
+  test: multiSnapshotWithOptions()
 });
