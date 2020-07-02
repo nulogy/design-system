d756ec37253940d74fab53ff8e47d7d737887c81
Marianne Borowiak <marianneb@nulogy.com>
2020-07-02 10:59:23 -0400

    convert box to typescript

--- packages/components/src/Box/Box.tsx
+++ packages/components/src/Box/Box.tsx
@@ -1,9 +1,44 @@
 import styled from "styled-components";
-import { color, space, layout, border, boxShadow, textAlign, order, flexGrow, position } from "styled-system";
+import {
+  color,
+  space,
+  layout,
+  border,
+  boxShadow,
+  textAlign,
+  order,
+  flexGrow,
+  position,
+  ColorProps,
+  SpaceProps,
+  LayoutProps,
+  BoxShadowProps,
+  TextAlignProps,
+  OrderProps,
+  FlexGrowProps,
+  PositionProps
+} from "styled-system";
 import theme from "../theme";
 
-const Box: React.SFC<any> = styled.div(color, space, layout, border, boxShadow, textAlign, order, flexGrow, position);
-Box.defaultProps = {
-  theme
-};
+type BoxProps = ColorProps &
+  SpaceProps &
+  LayoutProps &
+  BoxShadowProps &
+  TextAlignProps &
+  OrderProps &
+  FlexGrowProps &
+  PositionProps;
+
+const Box: React.SFC<BoxProps> = styled.div(
+  color,
+  space,
+  layout,
+  border,
+  boxShadow,
+  textAlign,
+  order,
+  flexGrow,
+  position
+);
+
 export default Box;
