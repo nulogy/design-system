2fa83a0a96ff2e0fbca738e450185f52605463c0
Marianne Borowiak <marianneb@nulogy.com>
2020-07-16 13:11:17 -0400

    add deprecated component warning

---
+++ components/src/utils/DeprecatedComponent.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+export const Deprecated = (Component, message = "") => props => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      console.error("Deprecated NDS Component:", message);
+    }
+  });
+
+  return <Component {...props} />;
+};
