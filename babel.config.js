60d92299ecaae8488be34d492f5c26fbba665342
Marianne Borowiak <marianneb@nulogy.com>
2020-07-22 15:21:25 -0400

    move docs to be built from root and root dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  babelrcRoots: [
-    "css",
-    "tokens",
-    "components",
-    "docs"
-  ]
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { browsers: "defaults" },
+        useBuiltIns: "entry"
+      }
+    ],
+    "@babel/preset-react"
+  ],
+  plugins: [
+    "babel-plugin-styled-components",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-runtime"
+  ],
+  babelrcRoots: ["docs"]
 };
