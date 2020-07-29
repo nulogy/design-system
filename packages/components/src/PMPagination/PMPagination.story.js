5669f1346274c9c23e33c5843ecd74993ca4d835
Marianne Borowiak <marianneb@nulogy.com>
2020-07-29 16:22:25 -0400

    fix!: remove pm components

--- packages/components/src/PMPagination/PMPagination.story.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { PMPagination } from ".";
-
-storiesOf("PM/PMPagination", module).add("default", () => (
-  <PMPagination currentPage={1} totalPages={4} nextLabel="Next &#8594;" previousLabel="&#8592; Previous" />
-));
