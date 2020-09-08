014edbd1328d485ecc26254e8c6bd5bc541992bb
Marianne Borowiak <marianneb@nulogy.com>
2020-09-08 14:22:33 -0400

    add babel plugins

--- packages/components/babel.config.js
+++ packages/components/babel.config.js
@@ -10,7 +10,14 @@ module.exports = {
     "@babel/preset-react",
     "@babel/preset-typescript"
   ],
-  plugins: ["babel-plugin-styled-components", "inline-json-import", "@babel/plugin-transform-runtime"],
+  plugins: [
+    "babel-plugin-styled-components",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-private-methods",
+    "@babel/plugin-proposal-private-property-in-object",
+    "inline-json-import",
+    "@babel/plugin-transform-runtime"
+  ],
   env: {
     test: {
       plugins: ["babel-plugin-styled-components", "require-context-hook"]
