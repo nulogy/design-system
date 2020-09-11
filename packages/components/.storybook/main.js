1ae9cfa7c984c7083f64e77ba5213c861bb83fbe
Marianne Borowiak <marianneb@nulogy.com>
2020-09-11 14:15:50 -0400

    fix storysource not loading by moving its settings under addons

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -3,7 +3,14 @@ module.exports = {
   addons: [
     "@storybook/addon-knobs",
     "@storybook/addon-viewport",
-    "@storybook/addon-storysource",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        rule: {
+          test: [/\.story\.js?$/]
+        }
+      }
+    },
     "@storybook/addon-a11y",
     "@storybook/addon-actions",
     "storybook-addon-performance"
