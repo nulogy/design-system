314c03b048d10788c9bebf6e6c2bdc2f2800c658
Marianne Borowiak <marianneb@nulogy.com>
2020-09-16 13:57:37 -0400

    update table spec

--- packages/components/cypress/integration/components/Table.spec.js
+++ packages/components/cypress/integration/components/Table.spec.js
@@ -160,7 +160,7 @@ describe("Table", () => {
   describe("with filtering and pagination", () => {
     const filterIinput = () => cy.get('input[name="Name"]');
     beforeEach(() => {
-      cy.renderFromStorybook("table--with-filtering-and-pagination-skip-storyshot");
+      cy.renderFromStorybook("table--with-filtering-and-pagination");
     });
     it("filters down to fewer pages", () => {
       paginationButtons(3).should("exist");
