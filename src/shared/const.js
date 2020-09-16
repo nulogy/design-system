dbc2c9de0c782ef7fc8b98e83007dabaae8bde91
Matt Dunn <mattd@nulogy.com>
2020-09-16 14:08:15 -0400

    chore: updates storybook url

--- src/shared/const.js
+++ src/shared/const.js
@@ -1,3 +1,3 @@
-export const STORYBOOK_URL = "https://storybook.nulogy.design/?path=/story/";
+export const STORYBOOK_URL = `https://master--5f60c6c285eaad0022dce67f.chromatic.com/?path=/story/`;
 export const STORYBOOK_COMPONENT_URL = `${STORYBOOK_URL}components-`;
 export const STORYBOOK_PAGE_URL = `${STORYBOOK_URL}pages-`;
