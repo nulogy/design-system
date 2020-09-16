b0dfd362567b7cbee9f74f3cdb232940e67709e1
Matt Dunn <mattd@nulogy.com>
2020-09-16 15:21:46 -0400

    removes docs site from repo

--- src/shared/const.js
+++
@@ -1,3 +0,0 @@
-export const STORYBOOK_URL = "https://storybook.nulogy.design/?path=/story/";
-export const STORYBOOK_COMPONENT_URL = `${STORYBOOK_URL}components-`;
-export const STORYBOOK_PAGE_URL = `${STORYBOOK_URL}pages-`;
