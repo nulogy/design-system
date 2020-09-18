212fa4eafe98ccf9f38d7f8059c7bb95da43b076
Marianne Borowiak <marianneb@nulogy.com>
2020-09-18 11:31:51 -0400

    add register

---
+++ packages/components/.storybook/nds-theme/register.js
@@ -0,0 +1,22 @@
+// .storybook/my-addon/register.js
+
+import React from "react";
+import { addons, types } from "@storybook/addons";
+import { AddonPanel } from "@storybook/components";
+
+const ADDON_ID = "ndsThemeAddon";
+const PANEL_ID = `${ADDON_ID}/panel`;
+
+// give a unique name for the panel
+const MyPanel = () => <div>MyAddon</div>;
+addons.register(ADDON_ID, api => {
+  addons.add(PANEL_ID, {
+    type: types.PANEL,
+    title: "My Addon",
+    render: ({ active, key }) => (
+      <AddonPanel active={active} key={key}>
+        <MyPanel />
+      </AddonPanel>
+    )
+  });
+});
