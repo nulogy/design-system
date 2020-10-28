bc16563daec4c7d5960f0a3ac1e3e50ea629c6fd
Marianne Borowiak <marianneb@nulogy.com>
2020-10-28 13:51:05 -0400

    add story tests

--- src/Select/Select.spec.js
+++ src/Select/Select.spec.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { Select } from ".";
-import { selectOption } from "./Select.spec-utils";
+import { fireEvent } from "@testing-library/react";
 import { renderWithNDSProvider } from "../NDSProvider/renderWithNDSProvider.spec-utils";
+import { selectOption, openDropdown } from "./Select.spec-utils";
+import {
+  UsingRefToControlFocus,
+  WithAnOptionSelected,
+  WithState,
+} from "./Select.story";
+import { Select } from ".";
 
 describe("select", () => {
   it("returns the selected item on change", () => {
@@ -36,6 +42,15 @@ describe("select", () => {
     expect(container).toHaveTextContent("Two");
     selectOption("Three", container, queryByText);
     expect(container).toHaveTextContent("Three");
+  });
+  describe("with state", () => {
+    it("clears the selected option", () => {
+      const { container, queryByText } = renderWithNDSProvider(<WithState />);
+      selectOption("Open", container, queryByText);
+      expect(container).toHaveTextContent("Open");
+      fireEvent.click(queryByText("Clear selection"));
+      expect(container).not.toHaveTextContent("Open");
+    });
   });
 });
 
@@ -72,5 +87,25 @@ describe("multi select", () => {
 
     expect(container).toHaveTextContent("One");
     expect(container).toHaveTextContent("Two");
+  });
+  describe("with ref", () => {
+    it("can set the focus", () => {
+      const { container, queryByText } = renderWithNDSProvider(
+        <UsingRefToControlFocus />
+      );
+      expect(container.querySelectorAll("input")[0]).not.toHaveFocus();
+      fireEvent.click(queryByText("Focus the Input"));
+      expect(container.querySelectorAll("input")[0]).toHaveFocus();
+    });
+  });
+  describe("with close menu turned off", () => {
+    it("close menu stays open", () => {
+      const { container, queryByText } = renderWithNDSProvider(
+        <UsingRefToControlFocus />
+      );
+      expect(container.querySelectorAll("input")[0]).not.toHaveFocus();
+      fireEvent.click(queryByText("Focus the Input"));
+      expect(container.querySelectorAll("input")[0]).toHaveFocus();
+    });
   });
 });
