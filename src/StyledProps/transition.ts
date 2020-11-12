d5c02f2d6adc33ee7a9faf24942eb9552b694b48
Marianne Borowiak <marianneb@nulogy.com>
2020-11-12 15:05:46 -0500

    set props to optional

--- src/StyledProps/transition.ts
+++ src/StyledProps/transition.ts
@@ -1,11 +1,11 @@
 import { system } from "styled-system";
 
 export type TransitionProps = {
-  transition: string,
-  transitionProperty: string;
-  transitonDuration: string;
-  transitionTimingFunction: string;
-  transitionDelay: string;
+  transition?: string,
+  transitionProperty?: string;
+  transitonDuration?: string;
+  transitionTimingFunction?: string;
+  transitionDelay?: string;
 }
 
 export const transition = system({
