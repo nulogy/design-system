6c0a30e63b9005ebf946418a1ff1d80e35dbd45e
Marianne Borowiak <marianneb@nulogy.com>
2021-01-19 15:01:38 -0500

    remove uneccessary fragment

--- src/Layout/Page.tsx
+++ src/Layout/Page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Box } from "../Box";
 import { Flex } from "../Flex";
 import { Heading1 } from "../Type";
-import { FlexProps } from '../Flex/Flex';
+import { FlexProps } from "../Flex/Flex";
 
 type PageProps = FlexProps & {
   breadcrumbs?: React.ReactNode;
@@ -19,24 +19,22 @@ export const Page = ({
   headerContent,
   ...props
 }: PageProps) => (
-  <>
-    <Flex flexDirection="column" py="x3" px="x3" {...props}>
-      {breadcrumbs}
-      <Flex alignItems="center">
-        {title && (
-          <Heading1 mb="x6" mt="x2">
-            {title}
-          </Heading1>
-        )}
-        {headerContent && (
-          <Box mb="x6" mt="x2" flexGrow={1} ml="x1">
-            {headerContent}
-          </Box>
-        )}
-      </Flex>
-      {children}
+  <Flex flexDirection="column" py="x3" px="x3" {...props}>
+    {breadcrumbs}
+    <Flex alignItems="center">
+      {title && (
+        <Heading1 mb="x6" mt="x2">
+          {title}
+        </Heading1>
+      )}
+      {headerContent && (
+        <Box mb="x6" mt="x2" flexGrow={1} ml="x1">
+          {headerContent}
+        </Box>
+      )}
     </Flex>
-  </>
+    {children}
+  </Flex>
 );
 
 export default Page;
