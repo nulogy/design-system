482ae2e0c6870b8d96bad07afdc01b769672df02
Marianne Borowiak <marianneb@nulogy.com>
2021-03-12 11:26:01 -0500

    fix: Simplify rollup config and remove modulesOnly check that triggers unresolved dependencies error

--- rollup.config.js
+++ rollup.config.js
@@ -3,106 +3,51 @@ import babel from "@rollup/plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import replace from "rollup-plugin-replace";
 import typescript from "rollup-plugin-typescript2";
-
-/* Rollup outputs module and main bundles for the @nulogy/components package */
-import packageJson from "./package.json";
+import pkg from "./package.json";
 
 const PEER_DEPENDENCIES = {
   react: "React",
   "styled-components": "styled",
+  "@nulogy/icons": "icons",
+  "react-dom": "reactDom",
 };
 
 const GLOBALS = {
   ...PEER_DEPENDENCIES,
-  "prop-types": "PropTypes",
-  "react-windowed-select": "components",
   "@babel/runtime/helpers/typeof": "typeof",
   "@babel/runtime/helpers/defineProperty": "defineProperty",
   "@babel/runtime/helpers/classCallCheck": "classCallCheck",
   "@babel/runtime/helpers/createClass": "createClass",
   "@babel/runtime/helpers/slicedToArray": "slicedToArray",
   "@babel/runtime/helpers/objectWithoutProperties": "objectWithoutProperties",
-  "object-assign": "assign",
   "@babel/runtime/helpers/objectWithoutPropertiesLoose":
     "objectWithoutPropertiesLoose",
   "@babel/runtime/helpers/extends": "extends",
   "@babel/runtime/helpers/assertThisInitialized": "assertThisInitialized",
   "@babel/runtime/helpers/inheritsLoose": "inheritsLoose",
-  "deep-equal": "deepEqual",
-  "create-react-context": "createReactContext",
-  warning: "warning",
-  exenv: "exenv",
-  classnames: "t",
-  "react-input-autosize": "AutosizeInput",
-  "html-parse-stringify2": "HTML",
-  "smoothscroll-polyfill": "smoothscroll",
-  "react-fast-compare": "isEqual",
-  "path-to-regexp": "pathToRegexp",
-  "react-is": "reactIs",
-  "react-dom": "reactDom",
 };
 
-const externals = Object.keys(GLOBALS);
+const EXTENSIONS = [".js", ".jsx", ".ts", ".tsx", ".mjs"];
 
-const EXTENSIONS = [".js", ".jsx", "ts", ".tsx", ".mjs"];
-
-const CORE_PLUGINS = [
-  /* typescript: see tsconfig.json for settings */
-  typescript(),
-  /* commonJS: convert CommonJS modules to ES6,
-  so they can be included in a Rollup bundle */
-  commonjs({
-    /* include: include all items in node_modules folders (in entire monorepo) */
-    include: [/node_modules/],
-    /* namedExports: sometimes commonjs can't resolve named exports from certain libraries, 
-    ex: import {exportName} from "package-name"; => exportName module not found
-    in those cases, it needs to be added as ["package-name"]: "exportName" here */
-    namedExports: {
-      debounce: ["debounce"],
-      "react-windowed-select": ["components"],
-    },
-  }),
-  /* babel: transiles the bundle according to babel.config */
-  babel({
-    /* babelHelpers: runtime tells babel to import from @babel/runtime instead of duplicated them */
-    babelHelpers: "runtime",
-    /* exclude: globs to exclude */
-    exclude: ["./node_modules/**/*"],
-    /* exclude: files to be transpiled by babel */
-    extensions: EXTENSIONS,
-  }),
-  /* replace: replaces strings when bundling */
-  replace({
-    exclude: /node_modules/,
-    /* ENV is replaced by environment setting */
-    ENV: JSON.stringify(process.env.NODE_ENV || "development"),
-  }),
-];
-
-const ENTRY_FILE = "src/index.ts";
-
-const mainBundles = {
-  /* input: main export file */
-  input: ENTRY_FILE,
-  /* external: external dependencies, all peerDependencies */
-  external: externals,
+export default {
+  input: "src/index.ts",
   output: [
-    // UMD format for compatibility with most script loaders
     {
-      file: packageJson.main,
+      file: pkg.main,
       name: "NDSComponents",
       format: "umd",
-      // globals: global variable names of external dependencies
       globals: GLOBALS,
     },
-    // ES module format for package.module field, auto-imports and optimal tree-shaking
     {
-      file: packageJson.module,
+      file: pkg.module,
       format: "es",
       globals: GLOBALS,
     },
   ],
+  external: Object.keys(PEER_DEPENDENCIES),
   plugins: [
+    /* typescript: see tsconfig.json for settings */
+    typescript(),
     // resolve: resolves node_modules imports
     resolve({
       /* mainFields: specifies the which package fields to look for first
@@ -110,12 +55,38 @@ const mainBundles = {
       package.main */
       mainFields: ["module", "main"],
       /* modulesOnly: inspect resolved files are es2015 modules */
-      modulesOnly: true,
+      // modulesOnly: true,
       /* extensions: specifies the file extensions to accept as imports */
       extensions: EXTENSIONS,
     }),
-    ...CORE_PLUGINS,
+    /* commonJS: convert CommonJS modules to ES6,
+    so they can be included in a Rollup bundle */
+    commonjs({
+      exclude: ["src/**"],
+      /* include: include all items in node_modules folders (in entire monorepo) */
+      include: ["./node_modules/**"],
+      /* namedExports: sometimes commonjs can't resolve named exports from certain libraries, 
+    ex: import {exportName} from "package-name"; => exportName module not found
+    in those cases, it needs to be added as ["package-name"]: "exportName" here */
+      namedExports: {
+        debounce: ["debounce"],
+        "react-windowed-select": ["components"],
+        "body-scroll-lock": ["disableBodyScroll", "clearAllBodyScrollLocks"],
+        "react-is": ["isValidElementType"]
+      },
+    }),
+    /* babel: transiles the bundle according to babel.config */
+    babel({
+      /* babelHelpers: runtime tells babel to import from @babel/runtime instead of duplicated them */
+      babelHelpers: "runtime",
+      /* exclude: globs to exclude */
+      exclude: "./node_modules/**",
+    }),
+    /* replace: replaces strings when bundling */
+    replace({
+      exclude: /node_modules/,
+      /* ENV is replaced by environment setting */
+      ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+    }),
   ],
 };
-
-export default [mainBundles];
