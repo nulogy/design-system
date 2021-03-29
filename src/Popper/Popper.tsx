1777e15c64e994f503d0ea89e974d15ccdaa9641
Marianne Borowiak <marianneb@nulogy.com>
2021-03-29 16:29:32 -0400

    remove forward ref from Popper

--- src/Popper/Popper.tsx
+++ src/Popper/Popper.tsx
@@ -23,147 +23,142 @@ type PopperProps = {
   openAriaLabel?: string;
   closeAriaLabel?: string;
 };
-const Popper: React.SFC<PopperProps> = React.forwardRef(
-  (
-    {
-      placement,
-      defaultOpen,
-      id,
-      showDelay,
-      hideDelay,
-      trigger,
-      children,
-      openOnClick,
-      openOnHover,
-      modifiers,
-      backgroundColor,
-      borderColor,
-      showArrow,
-      openAriaLabel,
-      closeAriaLabel,
-    },
-    popperRef
-  ) => {
-    let timeoutID;
-    const [isOpen, setIsOpen] = useState(defaultOpen);
-    const [referenceElement, setReferenceElement] = useState(null);
-    const [popperElement, setPopperElement] = useState(null);
-    const [arrowElement, setArrowElement] = useState(null);
-    const { styles, attributes } = usePopper(referenceElement, popperElement, {
-      placement,
-      modifiers: [
-        { name: "offset", options: { offset: [0, 4] } },
-        {
-          name: "arrow",
-          options: {
-            element: arrowElement,
-            padding: 10, // for offset of the arrow from the border-radius only
-          },
+const Popper: React.SFC<PopperProps> = ({
+  placement,
+  defaultOpen,
+  id,
+  showDelay,
+  hideDelay,
+  trigger,
+  children,
+  openOnClick,
+  openOnHover,
+  modifiers,
+  backgroundColor,
+  borderColor,
+  showArrow,
+  openAriaLabel,
+  closeAriaLabel,
+}) => {
+  let timeoutID;
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement,
+    modifiers: [
+      { name: "offset", options: { offset: [0, 4] } },
+      {
+        name: "arrow",
+        options: {
+          element: arrowElement,
+          padding: 10, // for offset of the arrow from the border-radius only
         },
-        ...modifiers,
-      ],
-    });
-    const conditionallyApplyDelay = (fnc, delay, skipDelay = true) => {
-      if (!skipDelay) {
-        timeoutID = setTimeout(fnc, delay);
-      } else {
-        fnc();
+      },
+      ...modifiers,
+    ],
+  });
+  const conditionallyApplyDelay = (fnc, delay, skipDelay = true) => {
+    if (!skipDelay) {
+      timeoutID = setTimeout(fnc, delay);
+    } else {
+      fnc();
+    }
+  };
+  const setPopUpState = (nextIsOpenState, skipDelay) => {
+    clearTimeout(timeoutID);
+    conditionallyApplyDelay(
+      () => setIsOpen(nextIsOpenState),
+      nextIsOpenState ? showDelay : hideDelay,
+      skipDelay
+    );
+  };
+  const closePopUp = (skipDelay) => {
+    setPopUpState(false, skipDelay);
+  };
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      switch (event.keyCode) {
+        case keyCodes.ESC:
+          closePopUp();
+          break;
+        default:
+          break;
       }
     };
-    const setPopUpState = (nextIsOpenState, skipDelay) => {
+    document.addEventListener("keydown", handleKeyDown);
+    const cleanup = () => {
+      document.removeEventListener("keydown", handleKeyDown);
       clearTimeout(timeoutID);
-      conditionallyApplyDelay(
-        () => setIsOpen(nextIsOpenState),
-        nextIsOpenState ? showDelay : hideDelay,
-        skipDelay
-      );
     };
-    const closePopUp = (skipDelay) => {
-      setPopUpState(false, skipDelay);
-    };
-    useEffect(() => {
-      const handleKeyDown = (event) => {
-        switch (event.keyCode) {
-          case keyCodes.ESC:
-            closePopUp();
-            break;
-          default:
-            break;
-        }
-      };
-      document.addEventListener("keydown", handleKeyDown);
-      const cleanup = () => {
-        document.removeEventListener("keydown", handleKeyDown);
-        clearTimeout(timeoutID);
-      };
-      return cleanup;
-    }, []);
-    const openPopUp = (skipDelay) => {
-      setPopUpState(true, skipDelay);
-    };
-    const onClickEventHandlers = openOnClick
-      ? {
-          onMouseDown: () => {
-            if (isOpen) {
-              closePopUp(false);
-            } else {
-              openPopUp(false);
-            }
-          },
-        }
-      : null;
-    const onHoverHandlers = openOnHover
-      ? {
-          onMouseEnter: () => openPopUp(false),
-          onMouseLeave: () => closePopUp(false),
-        }
-      : null;
-    const eventHandlers = {
-      onFocus: () => openPopUp(false),
-      onBlur: () => {
-        closePopUp(false);
-      },
-      ...onHoverHandlers,
-      ...onClickEventHandlers,
-    };
-    const { t } = useTranslation();
-    const openLabel = openAriaLabel || t("open");
-    const closeLabel = closeAriaLabel || t("close");
-    return (
-      <>
-        <trigger.type
-          {...trigger.props}
-          ref={setReferenceElement}
-          aria-haspopup={true}
-          aria-expanded={isOpen}
-          aria-describedby={id}
-          aria-label={isOpen ? closeLabel : openLabel}
-          {...eventHandlers}
-        />
+    return cleanup;
+  }, []);
+  const openPopUp = (skipDelay) => {
+    setPopUpState(true, skipDelay);
+  };
+  const onClickEventHandlers = openOnClick
+    ? {
+        onMouseDown: () => {
+          if (isOpen) {
+            closePopUp(false);
+          } else {
+            openPopUp(false);
+          }
+        },
+      }
+    : null;
+  const onHoverHandlers = openOnHover
+    ? {
+        onMouseEnter: () => openPopUp(false),
+        onMouseLeave: () => closePopUp(false),
+      }
+    : null;
+  const eventHandlers = {
+    onFocus: () => openPopUp(false),
+    onBlur: () => {
+      closePopUp(false);
+    },
+    ...onHoverHandlers,
+    ...onClickEventHandlers,
+  };
+  const { t } = useTranslation();
+  const openLabel = openAriaLabel || t("open");
+  const closeLabel = closeAriaLabel || t("close");
+  return (
+    <>
+      <trigger.type
+        {...trigger.props}
+        ref={setReferenceElement}
+        aria-haspopup={true}
+        aria-expanded={isOpen}
+        aria-describedby={id}
+        aria-label={isOpen ? closeLabel : openLabel}
+        {...eventHandlers}
+      />
 
-        <Box
-          ref={setPopperElement}
-          style={styles.popper}
-          display={isOpen ? "block" : "none"}
-          position="relative"
-          {...attributes.popper}
-        >
-          {children}
-          {showArrow && (
-            <PopperArrow
-              key="popper-arrow"
-              placement={placement}
-              ref={setArrowElement}
-              style={styles.arrow}
-              backgroundColor={backgroundColor}
-              borderColor={borderColor}
-            />
-          )}
-        </Box>
-      </>
-    );
-  }
-);
+      <Box
+        ref={setPopperElement}
+        style={styles.popper}
+        display={isOpen ? "block" : "none"}
+        position="relative"
+        {...attributes.popper}
+      >
+        {children}
+        {showArrow && (
+          <PopperArrow
+            key="popper-arrow"
+            placement={placement}
+            ref={setArrowElement}
+            style={styles.arrow}
+            backgroundColor={backgroundColor}
+            borderColor={borderColor}
+          />
+        )}
+      </Box>
+    </>
+  );
+};
 Popper.defaultProps = {
   showDelay: "100",
   hideDelay: "350",
