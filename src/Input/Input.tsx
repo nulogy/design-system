ef6035d9b50937a27f830b67dae0b7d76ce69a30
Marianne Borowiak <marianneb@nulogy.com>
2021-04-12 11:36:44 -0400

    fix: Minimally fix the input issue by getting rid of suspected circular dependency

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,9 +1,9 @@
 import React, { forwardRef } from "react";
 import propTypes from "@styled-system/prop-types";
 import { getSubset, omitSubset } from "../utils/subset";
-import { Field } from "../Form";
-import { InputField, InputFieldProps } from "./InputField";
+import Field from "../Form/Field";
 import { InlineValidation } from "../Validation";
+import { InputField, InputFieldProps } from "./InputField";
 
 type InputProps = InputFieldProps & {
   errorMessage?: string;
@@ -16,14 +16,20 @@ const Input: React.SFC<InputProps> = forwardRef<HTMLInputElement, InputProps>(
   ({ errorMessage, errorList, className, ...props }, ref) => {
     const spaceProps = getSubset(props, propTypes.space);
     const restProps = omitSubset(props, propTypes.space);
-    return <Field className={className} {...spaceProps}>
-      <InputField {...restProps} error={!!(errorMessage || errorList)} ref={ref} />
-      <InlineValidation
-        mt="x1"
-        errorMessage={errorMessage}
-        errorList={errorList}
-      />
-    </Field>
+    return (
+      <Field className={className} {...spaceProps}>
+        <InputField
+          {...restProps}
+          error={!!(errorMessage || errorList)}
+          ref={ref}
+        />
+        <InlineValidation
+          mt="x1"
+          errorMessage={errorMessage}
+          errorList={errorList}
+        />
+      </Field>
+    );
   }
 );
 
