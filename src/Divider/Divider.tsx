cce60deede750f615990fa23ae3528f44b924c0f
Marianne Borowiak <marianneb@nulogy.com>
2021-04-15 17:05:49 -0400

    feat: add a horizontal <Divider/> component (#887)

---
+++ src/Divider/Divider.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box } from "../Box";
+import { BoxProps } from "../Box/Box";
+
+type DividerProps = BoxProps;
+
+const Divider = ({ borderColor = "lightGrey", ...props }: DividerProps) => (
+  <Box
+    as="hr"
+    borderTop="1px solid"
+    borderColor={borderColor}
+    {...props}
+  />
+);
+
+export default Divider;
