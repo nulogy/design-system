8e12012111f4dd7f9be23a40d561972319871974
Haider Alshamma <haidera@nulogy.com>
2021-07-21 11:11:14 -0400

    DO-352: Adds AsyncSelect Multiselect component e2e tests

---
+++ cypress/integration/components/AsyncSelect.spec.js
@@ -0,0 +1,47 @@
+describe("AsyncSelect", () => {
+  const getSelectComponent = () => cy.get("[data-testid='select-container']");
+  const getDropdownMenu = () => cy.get("[data-testid='select-dropdown']");
+  const getMultiselect = () => getSelectComponent();
+  const getClearButton = () => cy.get("[data-testid='select-clear']");
+
+  describe("Multiselect", () => {
+    beforeEach(() => {
+      cy.renderFromStorybook("asyncselect--multiselect");
+    });
+
+    it("can select multiple values", () => {
+      getMultiselect().click();
+
+      cy.focused().type("cana");
+      cy.wait(200);
+      cy.focused().type("{enter}");
+
+      cy.focused().type("austra");
+      cy.wait(200);
+      cy.focused().type("{enter}");
+
+      getMultiselect().contains("Canada");
+      getMultiselect().contains("Australia");
+    });
+
+    it("clears all selected values", () => {
+      getMultiselect().click();
+
+      cy.focused().type("cana");
+      cy.wait(200);
+      cy.focused().type("{enter}");
+
+      getClearButton().click();
+
+      getMultiselect().contains("Please select a countries");
+    });
+
+    it("does not show the dropdown arrow", () => {
+      getDropdownMenu().should("not.exist");
+    });
+
+    it("does not show the clear button when no values are selected", () => {
+      getClearButton().should("not.exist");
+    });
+  });
+});
