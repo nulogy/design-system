4a8781245e06c951d09a0d1609d3ae2927284522
Matt Dunn <mattd@nulogy.com>
2021-09-24 09:34:31 -0400

    extract components from Breadcrumbs

---
+++ src/Breadcrumbs/BreadcrumbsListItem.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import { space, color, flexbox, layout } from "styled-system";
+
+const BreadcrumbsListItem: React.SFC<any> = styled.li(
+  ({ theme }) => ({
+    margin: 0,
+    padding: 0,
+    listStyle: "none",
+    display: "inline-flex",
+    alignSelf: "center",
+    color: theme.colors.darkGrey,
+    "a:visited": {
+      color: theme.colors.darkBlue,
+    },
+  }),
+  space,
+  layout,
+  color,
+  flexbox
+);
+
+export default BreadcrumbsListItem;
