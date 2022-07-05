0758d92e40a62fd52a42f9a2296e0ad4665ae310
Sean Kirby <seank@nulogy.com>
2022-07-05 10:54:17 -0400

    feat: allow NavBarBackground to be customized

--- src/BrandedNavBar/NavBarBackground.tsx
+++ src/BrandedNavBar/NavBarBackground.tsx
@@ -1,23 +1,21 @@
 import styled, { CSSObject } from "styled-components";
 import { DefaultNDSThemeType } from "../theme.type";
 import { Flex } from "../Flex";
+import { addStyledProps, StyledProps } from "../StyledProps";
 
 export type NavBarBackgroundProps = {
-  backgroundColor?: string;
   theme?: DefaultNDSThemeType;
-  height: string;
-};
+} & StyledProps;
 
 const NavBarBackground = styled(Flex)(
-  ({ height, backgroundColor, theme }: NavBarBackgroundProps): CSSObject => ({
-    background: backgroundColor,
+  ({ theme }: NavBarBackgroundProps): CSSObject => ({
     padding: `0 ${theme.space.x3}`,
     borderBottom: `1px solid ${theme.colors.lightGrey}`,
     alignItems: "center",
-    height: height,
     zIndex: theme.zIndices.navBar,
     position: "relative",
-  })
+  }),
+  addStyledProps
 );
 
 export default NavBarBackground;
