3725c7d4db0f5f97984e5209943aa6ad9aaa816e
Haider Alshamma <haidera@nulogy.com>
2022-08-23 09:39:22 -0400

    Adjust specs to assert on node existence in the DOM instead of CSS property value

--- cypress/integration/components/Toast.spec.js
+++ cypress/integration/components/Toast.spec.js
@@ -8,30 +8,30 @@ describe("Toast", () => {
     });
 
     it("is hidden by default", () => {
-      toast().should("have.css", "opacity", "0");
+      toast().should("not.exist");
     });
 
     it("shows and hides alert when triggered", () => {
       button().click();
       cy.isInViewport(TOAST_SELECTOR);
-      toast().should("have.css", "opacity", "1");
+      toast().should("exist");
       cy.wait(4000);
-      toast().should("have.css", "opacity", "0");
+      toast().should("not.exist");
     });
 
     it("continues to show the alert when hovered over", () => {
       button().click();
-      toast().should("have.css", "opacity", "1");
+      toast().should("exist");
       toast().trigger("mouseover");
       cy.wait(4000);
-      toast().should("have.css", "opacity", "1");
+      toast().should("exist");
     });
     it("hides the alert on mouseleave", () => {
       button().click();
-      toast().should("have.css", "opacity", "1");
+      toast().should("exist");
       toast().trigger("mouseover");
       cy.wait(4000);
-      toast().should("have.css", "opacity", "1");
+      toast().should("exist");
       toast().trigger("mouseover");
       cy.wait(2000);
     });
@@ -43,22 +43,22 @@ describe("Toast", () => {
     });
 
     it("is hidden by default", () => {
-      toast().should("have.css", "opacity", "0");
+      toast().should("not.exist");
     });
 
     it("shows the alert when triggered and does not automatically hide it", () => {
       button().click();
       cy.isInViewport(TOAST_SELECTOR);
-      toast().should("have.css", "opacity", "1");
+      toast().should("exist");
       cy.wait(4000);
-      toast().should("have.css", "opacity", "1");
+      toast().should("exist");
     });
     it("hides the alert when close button is clicked", () => {
       button().click();
-      toast().should("have.css", "opacity", "1");
+      toast().should("exist");
       cy.get(CLOSE_BTN_SELECTOR).click();
       cy.wait(4000);
-      toast().should("have.css", "opacity", "0");
+      toast().should("not.exist");
     });
   });
 });
