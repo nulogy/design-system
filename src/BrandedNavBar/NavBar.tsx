6e28631837fbf76c8d43b59f71d9b5199a15854b
petror-nulogy <petror@nulogy.dev>
2022-11-29 23:13:25 +0200

    chore: remove useless code from NavBar

--- src/BrandedNavBar/NavBar.tsx
+++ src/BrandedNavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import React, { useLayoutEffect, useState } from "react";
 import styled, { useTheme, CSSObject } from "styled-components";
-import ReactResizeDetector, { useResizeDetector } from "react-resize-detector";
+import { useResizeDetector } from "react-resize-detector";
 import { useTranslation } from "react-i18next";
 import {
   color,
@@ -15,40 +15,33 @@ import {
   layout,
   compose,
 } from "styled-system";
-import {
-  Box,
-  Branding,
-  BrandLogoContainer,
-  DesktopMenu,
-  EnvironmentBanner,
-  Flex,
-  Icon,
-  Link,
-  NavBarBackground,
-  SmallNavBar,
-  ThemeType,
-} from "../index";
+import { Box, DesktopMenu, EnvironmentBanner, Flex, Icon, NavBarBackground, SmallNavBar } from "../index";
 import { MenuIcon } from "../NavBar/NavBar";
-import { NulogyLogoContainer } from "./NulogyLogoContainer";
+import { DefaultNDSThemeType } from "../theme.type";
 
 export type StyleProps = SpaceProps & OverflowProps & LayoutProps & TypographyProps & ColorProps;
 
 export const stylePropsToCss: (props: StyleProps) => CSSObject = compose(space, overflow, layout, typography, color);
 
 export type NavBarWithResizablePrimaryMenuProps = {
-  menuData?: any;
+  menuData?: {
+    primaryMenu: MenuParentItem[];
+    secondaryMenu: MenuParentItem[];
+  };
   subtext?: string;
   environment?: string;
   brandingLinkHref?: string;
   className?: string;
   breakpointUpper?: number | string;
   defaultOpen?: boolean;
+  logo?: (brandingLinkHref: string, width: number) => React.ReactElement;
 };
-
+// TODO: Should we have some default Nulogy logo, in case if logo props was not provided?
 const NavBarWithResizablePrimaryMenu: React.FC<NavBarWithResizablePrimaryMenuProps> = ({
   menuData,
   breakpointUpper = "medium",
   brandingLinkHref = "/",
+  logo,
   ...props
 }) => {
   const primaryMenu = filterDisabledMenuItems(menuData.primaryMenu);
@@ -62,7 +55,7 @@ const NavBarWithResizablePrimaryMenu: React.FC<NavBarWithResizablePrimaryMenuPro
         breakpointUpper={breakpointUpper}
         width={width}
         menuData={{ primaryMenu, secondaryMenu }}
-        logo={<ProgressiveNulogyLogo width={width} brandingLinkHref={brandingLinkHref} />}
+        logo={logo && logo(brandingLinkHref, width)}
         {...props}
       />
     </div>
@@ -178,9 +171,11 @@ const filterItems = (menuItems) => {
 };
 
 function filterDisabledMenuItems(menuItems) {
+  console.log({ menuItems });
   return menuItems
     .map((item) => {
       if (item.items) {
+        console.log({ items: item.items });
         const itemsWithSubMenuFiltered = item.items.map((subMenuItem) => {
           if (subMenuItem.items && subMenuItem.items.length > 0) {
             return { ...subMenuItem, items: filterItems(subMenuItem.items) };
@@ -204,9 +199,19 @@ function filterDisabledMenuItems(menuItems) {
 
 const HAMBURGER_BREAKPOINT = 1024;
 const MIN_MENU_SPACING = 64;
-const NAVBAR_HEIGHT = "56px";
+export const NAVBAR_HEIGHT = "56px";
 const LARGEST_MENU_ELEM_WIDTH = 201; // width of the largest primary menu element in French
 const BACKGROUND_PADDING = 48; // width of the padding at the edges of the NavBarBackground
+
+// NDS should really export this instead of us having to re-create it here
+type NewMenuButtonProps = React.ComponentPropsWithRef<"button"> &
+  StyleProps & {
+    color?: string;
+    disabled?: boolean;
+    theme?: DefaultNDSThemeType;
+    hoverColor?: string;
+    bgHoverColor?: string;
+  };
 
 const NewMenuButton: React.FC<NewMenuButtonProps> = styled.button(
   ({ theme }) => ({
@@ -242,17 +247,6 @@ const NewMenuButton: React.FC<NewMenuButtonProps> = styled.button(
   }),
   stylePropsToCss
 );
-NewMenuButton.displayName = "NewMenuButton";
-
-// NDS should really export this instead of us having to re-create it here
-export type NewMenuButtonProps = React.ComponentPropsWithRef<"button"> &
-  StyleProps & {
-    color?: string;
-    disabled?: boolean;
-    theme?: ThemeType;
-    hoverColor?: string;
-    bgHoverColor?: string;
-  };
 
 type NewMenuButtonWithChevronProps = NewMenuButtonProps & {
   icon: string;
@@ -280,28 +274,6 @@ const StyledNewMenuButton = styled(NewMenuButton)(({ theme }) => ({
     fill: theme.colors.darkBlue,
   },
 }));
-
-type ProgressiveNulogyLogoProps = {
-  brandingLinkHref: string;
-  width: number;
-};
-
-function ProgressiveNulogyLogo({ brandingLinkHref, width }: ProgressiveNulogyLogoProps) {
-  const { t } = useTranslation();
-  const logoType = width >= 1920 ? "wordmark" : "lettermark";
-  return (
-    <Link
-      aria-label={t("Home")}
-      as={Link}
-      to={brandingLinkHref}
-      underline={false}
-      lineHeight="1"
-      style={{ display: "block" }}
-    >
-      <Branding size="medium" logoType={logoType} logoColor="blue" />
-    </Link>
-  );
-}
 
 interface UseMoreMenuSizeProps {
   maxMoreMenuSize: number;
@@ -332,225 +304,40 @@ function useMoreMenuSize({ maxMoreMenuSize, distance, growThreshold, shrinkThres
   return moreMenuSize;
 }
 
-// type BaseNavBarProps = {
-//   menuData?: any;
-//   className?: string;
-//   breakpointUpper?: number | string;
-//   environment?: "training" | "development" | undefined;
-//   subtext?: string;
-//   brandingLinkHref?: string;
-//   logoSrc?: string;
-// };
+interface MenuParentItem {
+  name: string;
+  key?: string;
+  items?: MenuItem[];
+  trigger?: (props: any) => React.ReactElement;
+  render?: (config: { size: string | number; layer: string }) => React.ReactElement;
+}
 
-// const BaseNavBar = ({ environment, breakpointUpper = "medium", ...props }: BaseNavBarProps) => {
-//   const { breakpoints } = useTheme();
-//   return (
-//     <ReactResizeDetector handleWidth>
-//       <SelectNavBarBasedOnWidth
-//         breakpointUpper={breakpoints[breakpointUpper] || breakpointUpper}
-//         {...props}
-//         environment={environment}
-//       />
-//     </ReactResizeDetector>
-//   );
-// };
-
-// const NavBar = styled(BaseNavBar)({});
-
-// export default NavBar;
+interface MenuItem {
+  name: string;
+  to?: string;
+  href?: string;
+  as?: React.ReactElement;
+  isEnabled?: boolean;
+  items?: MenuItem[];
+}
 
 export interface NewNavBarProps {
-  masterDataClient: ApolloClient<object>;
+  primaryMenu: MenuParentItem[];
+  secondaryMenu: MenuParentItem[];
   defaultOpen?: boolean;
-  width?: number;
+  environment?: string;
 }
 
-export default function NewNavBar({ currentUser, masterDataClient, defaultOpen, width }: NewNavBarProps) {
-  const [triggeredErrorToast, setTriggeredErrorToast] = useState(false);
-  const { t } = useTranslation();
-  const session = new SessionHelper();
-  const { organization } = currentUser;
-
-  const { loading: orderManagementSettingsLoading, data: orderManagementSettingsData } = useQuery(
-    orderManagementEnabledQuery,
-    {
-      client: masterDataClient,
-    }
-  );
-  const orderManagementMenuItems = buildOrderManagementMenuItems(
-    organization,
-    orderManagementSettingsLoading,
-    orderManagementSettingsData,
-    t
-  );
-  const primaryMenu = [
-    {
-      name: t("Order management"),
-      trigger: (props) => <ProgressiveMenuButton {...props}>{t("Order management")}</ProgressiveMenuButton>,
-      items: orderManagementMenuItems,
-    },
-    {
-      name: t("Analytics"),
-      trigger: (props) => <ProgressiveMenuButton {...props}>{t("Analytics")}</ProgressiveMenuButton>,
-      items: [
-        {
-          name: t("Historical orders"),
-          to: MENU_ENDPOINTS.HISTORICAL_ORDERS,
-          as: RouterLink,
-          isEnabled: organization.enableOrderTracking,
-        },
-        {
-          name: t("Scorecards"),
-          to: MENU_ENDPOINTS.SCORECARDS,
-          as: RouterLink,
-          isEnabled: organization.enableScorecards,
-        },
-      ],
-    },
-    {
-      name: t("Inventory Management"),
-      trigger: (props) => <ProgressiveMenuButton {...props}>{t("Inventory management")}</ProgressiveMenuButton>,
-      items: [
-        {
-          name: t("Materials overview"),
-          to: MENU_ENDPOINTS.MATERIALS_OVERVIEW,
-          as: RouterLink,
-          isEnabled: organization.enableMaterialsOverview,
-        },
-        {
-          name: t("Inventory reconciliation"),
-          to: MENU_ENDPOINTS.INVENTORY_RECONCILIATION,
-          as: RouterLink,
-          isEnabled: currentUser.inventoryReconciliationEnabled,
-        },
-      ],
-    },
-    {
-      render: ({ size, layer }) => {
-        return (
-          <ProgressiveMenuLink to={MENU_ENDPOINTS.INVOICES} size={size} layer={layer}>
-            {t("Invoices")}
-          </ProgressiveMenuLink>
-        );
-      },
-      key: "invoices",
-      isEnabled: Boolean(organization.enableInvoices && currentUser.invoicesEnabled),
-    },
-    {
-      render: ({ size, layer }) => (
-        <ProgressiveMenuLink to={MENU_ENDPOINTS.ITEMS} size={size} layer={layer}>
-          {t("Items")}
-        </ProgressiveMenuLink>
-      ),
-      key: "items",
-    },
-    {
-      render: ({ size, layer }) => (
-        <ProgressiveMenuLink to={MENU_ENDPOINTS.IMPORTS_EXPORTS} size={size} layer={layer}>
-          {t("Imports and exports")}
-        </ProgressiveMenuLink>
-      ),
-      key: "importsAndExports",
-    },
-  ];
-  const handleError = () => setTriggeredErrorToast(true);
-
-  const adminOrgSwitcher = {
-    key: "admin org switcher",
-    trigger: () => (
-      <NewMenuButtonWithChevron icon="downArrow" aria-label={t("admin org switcher")}>
-        <Icon icon="building" />
-      </NewMenuButtonWithChevron>
-    ),
-    items: [
-      {
-        key: "organization-item-1",
-        render: () => (
-          <ResponsiveForm>
-            <AdminOrgSwitcher {...{ currentUser, masterDataClient }} />
-          </ResponsiveForm>
-        ),
-      },
-    ],
-  };
-
-  let secondaryMenu;
-  // users usually have a bigger screen, so avoid jitter when the width gets picked up.
-  const showingDesktopNav = width === undefined ? true : width > HAMBURGER_BREAKPOINT;
-  if (showingDesktopNav) {
-    secondaryMenu = [
-      {
-        name: "Guided tours",
-        render: () => <GuidedTours>{t("Guided tours")}</GuidedTours>,
-      },
-      {
-        name: "Help desk",
-        render: () => <Helpdesk errorHandler={handleError}>{t("HelpDesk")}</Helpdesk>,
-      },
-      {
-        name: "Help menu separator",
-        render: () => <VerticalDivider />,
-      },
-      {
-        ...adminOrgSwitcher,
-        isEnabled: currentUser.isAdmin,
-      },
-      {
-        name: "Admin org switcher separator",
-        render: () => <VerticalDivider />,
-        isEnabled: currentUser.isAdmin,
-      },
-    ];
-  } else {
-    secondaryMenu = [];
-  }
-  secondaryMenu = [
-    ...secondaryMenu,
-    {
-      name: "User Menu",
-      render: ({ size }) => {
-        return size === "medium" ? (
-          <Flex>
-            <UserMenuLogo currentUser={currentUser} masterDataClient={masterDataClient} />
-            <UserMenuModalButton {...{ currentUser, masterDataClient }} />
-          </Flex>
-        ) : (
-          <UserMenuMobile {...{ currentUser, masterDataClient }} />
-        );
-      },
-    },
-  ];
-
-  return (
-    <>
-      <NavBarWithResizablePrimaryMenu
-        menuData={{ primaryMenu, secondaryMenu }}
-        breakpointUpper={HAMBURGER_BREAKPOINT + 1}
-        environment={mapURLToBannerName(document.URL)}
-        defaultOpen={defaultOpen}
-      />
-      <NetworkErrorToast triggered={triggeredErrorToast} onHide={() => setTriggeredErrorToast(false)} />
-      <Toast type="success" triggered={session.getOrganizationName()} onHidden={() => session.clearOrganizationName()}>
-        {t(`Switched to ${session.getOrganizationName()}`)}
-      </Toast>
-    </>
-  );
-}
-
-function GuidedTours(props) {
-  return <NewMenuButton id="guided-tours" {...props} />;
-}
-
-function Helpdesk({ errorHandler, ...props }) {
-  return <NewMenuButton id="help-desk" onClick={() => displayZendeskWidget(errorHandler)} {...props} />;
-}
-Helpdesk.propTypes = {
-  errorHandler: PropTypes.func,
-};
-
-const ResponsiveForm = styled(Form)(({ theme }) => ({
-  padding: `${theme.space.x1} ${theme.space.x2}`,
-}));
+// export default function NewNavBar({ primaryMenu, secondaryMenu, defaultOpen, environment }: NewNavBarProps) {
+//   return (
+//     <NavBarWithResizablePrimaryMenu
+//       menuData={{ primaryMenu, secondaryMenu }}
+//       breakpointUpper={HAMBURGER_BREAKPOINT + 1}
+//       environment={environment}
+//       defaultOpen={defaultOpen}
+//     />
+//   );
+// }
 
 type ProgressiveMenuButtonProps = {
   size: string;
@@ -558,31 +345,3 @@ type ProgressiveMenuButtonProps = {
   defaultRender: () => React.ReactElement;
   children: React.ReactNode;
 };
-
-const ProgressiveMenuButton = React.forwardRef(
-  ({ size, layer, defaultRender, ...props }: ProgressiveMenuButtonProps, ref) => {
-    if (layer === 0) {
-      return size === "small" ? (
-        <NewMenuText pl="x3" mb="x1" {...props} />
-      ) : (
-        <NewMenuButtonWithChevron ref={ref} icon="downArrow" {...props} />
-      );
-    }
-    return React.cloneElement(defaultRender(), { ref, ...props });
-  }
-);
-ProgressiveMenuButton.displayName = "ProgressiveMenuButton";
-
-type ProgressiveMenuLinkProps = {
-  size: string;
-  layer: number;
-  to: string;
-  children: React.ReactNode;
-};
-function ProgressiveMenuLink({ size, layer, ...props }: ProgressiveMenuLinkProps) {
-  const marginBottom = size === "small" ? "x1" : "";
-  const indent = size === "small" ? "x3" : "";
-  const DesktopLink = layer === 0 ? NewMenuLink : DropdownLink;
-  const LinkElem = size === "small" ? NewMenuHamburgerLink : DesktopLink;
-  return <LinkElem as={RouterLink} mb={marginBottom} pl={indent} {...props} />;
-}
