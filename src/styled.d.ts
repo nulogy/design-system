2494d56900d6c8b5eb4ebf275877add4cfec6878
petror-nulogy <petror@nulogy.dev>
2022-11-29 23:14:10 +0200

    chore: fixing types issue

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,9 +1,11 @@
 import {} from "styled-components";
 import theme from "./theme";
+// import { ThemeType } from "./theme.type";
 
 declare module "styled-components" {
   type Theme = typeof theme;
   export interface DefaultTheme extends Theme {}
+  // export interface DefaultTheme extends ThemeType {}
 }
 
 declare module "styled-system";
