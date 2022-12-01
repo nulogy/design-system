4ee53953e6002150f0ce36ad3847913256e46ed2
Haider Alshamma <haidera@nulogy.com>
2022-12-01 11:37:08 -0500

    fix: add the gap properties to the Flex component

--- src/Flex/Flex.tsx
+++ src/Flex/Flex.tsx
@@ -1,14 +1,33 @@
+import React, { CSSProperties } from "react";
 import styled from "styled-components";
-import { flexbox } from "styled-system";
+import { flexbox, system } from "styled-system";
 import { Box } from "../Box";
 import { BoxProps } from "../Box/Box";
 
-export type FlexProps = BoxProps;
+export type FlexProps = BoxProps & {
+  gap?: CSSProperties["gap"];
+  rowGap?: CSSProperties["rowGap"];
+  columnGap?: CSSProperties["columnGap"];
+};
 
-const Flex: React.FunctionComponent<FlexProps> = styled(Box)(
+const Flex: React.FC<FlexProps> = styled(Box)(
   {
     display: "flex",
   },
+  system({
+    gap: {
+      property: "gap",
+      scale: "space",
+    },
+    rowGap: {
+      property: "rowGap",
+      scale: "space",
+    },
+    columnGap: {
+      property: "columnGap",
+      scale: "space",
+    },
+  }),
   flexbox
 );
 
