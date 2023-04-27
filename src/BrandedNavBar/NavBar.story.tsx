2d3a93ac65a409bef8d37f43edf77031656831c2
Haider Alshamma <haidera@nulogy.com>
2023-04-27 16:51:38 -0400

    Fix storybook errors

--- src/BrandedNavBar/NavBar.story.tsx
+++ src/BrandedNavBar/NavBar.story.tsx
@@ -12,7 +12,7 @@ import { BrandedNavBar as NDSBrandedNavBar } from "./index";
 
 const sampleLogo = "http://pigment.github.io/fake-logos/logos/vector/color/auto-speed.svg";
 
-export const ResetStorybookView = styled.div({
+const ResetStorybookView = styled.div({
   position: "absolute",
   top: 0,
   left: 0,
@@ -132,11 +132,10 @@ const secondaryMenuWithIcon = [
   },
 ];
 
-export const menuData = { primaryMenu, secondaryMenu }
+const menuData = { primaryMenu, secondaryMenu };
 export default {
   title: "Components/BrandedNavBar",
 };
-
 
 export const _BrandedNavBar = () => <BrandedNavBar menuData={{ primaryMenu, secondaryMenu }} />;
 
