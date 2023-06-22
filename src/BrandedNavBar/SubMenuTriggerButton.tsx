fe2afc05b136887da01f650ffc296f6d56c86a11
Haider Alshamma <haidera@nulogy.com>
2023-06-22 11:18:53 -0400

    fix: remove absolute positioning of icon

--- src/BrandedNavBar/SubMenuTriggerButton.tsx
+++ src/BrandedNavBar/SubMenuTriggerButton.tsx
@@ -2,26 +2,32 @@ import styled, { CSSObject, StyledComponentPropsWithRef } from "styled-component
 import React from "react";
 import { DropdownButton } from "../DropdownMenu";
 import { Icon } from "../Icon";
+import { Flex } from "../Flex";
 
-const StyledButton: StyledComponentPropsWithRef<any> = styled(DropdownButton)(
-  ({ isOpen, theme }: any): CSSObject => ({
-    position: "relative",
+const StyledButton = styled(DropdownButton)<{ isOpen: boolean }>(
+  ({ isOpen, theme }): CSSObject => ({
+    padding: `${theme.space.x1} ${theme.space.x1} ${theme.space.x1} 12px`,
     backgroundColor: isOpen ? theme.colors.lightBlue : "transparent",
     color: isOpen ? theme.colors.darkBlue : theme.colors.darkGrey,
   })
 );
+
 type SubMenuTriggerButtonProps = React.ComponentPropsWithRef<"button"> & {
   name?: string;
   isOpen: boolean;
 };
 
 const SubMenuTriggerButton = React.forwardRef<HTMLButtonElement, SubMenuTriggerButtonProps>(
-  ({ name, isOpen, ...props }, ref) => (
-    <StyledButton isOpen={isOpen} ref={ref} {...props}>
-      {name}
-      <Icon style={{ position: "absolute", top: "10px" }} icon="rightArrow" size="20px" p="2px" />
-    </StyledButton>
-  )
+  ({ name, isOpen, ...props }, ref) => {
+    return (
+      <StyledButton isOpen={isOpen} ref={ref} {...props}>
+        <Flex gap="half" justifyContent="space-between" alignItems="center">
+          {name}
+          <Icon icon="rightArrow" size="20px" />
+        </Flex>
+      </StyledButton>
+    );
+  }
 );
 
 SubMenuTriggerButton.displayName = "SubMenuTriggerButton";
