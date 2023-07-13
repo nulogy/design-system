831ee087ca5acec5955bbc2042f6d42c44ee0330
Haider Alshamma <haidera@nulogy.com>
2023-07-13 16:50:37 -0400

    fix: upgrade enzyme-adapter-react

--- spec/support/specHelper.js
+++ spec/support/specHelper.js
@@ -1,5 +1,5 @@
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import "@testing-library/jest-dom/extend-expect";
 
 Enzyme.configure({ adapter: new Adapter() });
