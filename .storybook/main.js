1cb7d420f24ffbd0a04b2ce871e2be98af39c172
Haider Alshamma <haidera@nulogy.com>
2024-02-13 11:19:17 -0500

    chore: allow storybook to load esmodules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,17 @@
 module.exports = {
   stories: ["../src/**/*.story.tsx", "../src/**/*.story.js"],
+  webpackFinal: async (config) => {
+    // Resolve error when webpack-ing storybook:
+    // Can't import the named export 'Children' from non EcmaScript module (only
+    // default export is available)
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+
+    return config;
+  },
   addons: [
     "@storybook/addon-knobs",
     "@storybook/addon-viewport",
