cc35d4baab69b86134bf3d9aac77b79dc2d769c2
Haider Alshamma <haidera@nulogy.com>
2024-10-30 15:27:47 -0400

    feat: support tablet and phone media queries

---
+++ src/NDSProvider/GlobalStylesComposer.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { DefaultNDSThemeType } from "../theme.type";
+import Reset from "./Reset";
+import ModalStyleOverride from "./ModalStyleOverride";
+import GlobalStyles from "./GlobalStyles";
+
+type GlobalStylesComposerProps = {
+  theme?: DefaultNDSThemeType;
+  locale?: string;
+  disableGlobalStyles?: boolean;
+  children?: React.ReactNode;
+};
+
+export default function GlobalStylesComposer({
+  theme,
+  locale,
+  disableGlobalStyles,
+  children,
+}: GlobalStylesComposerProps) {
+  if (disableGlobalStyles) {
+    return <>{children}</>;
+  }
+
+  return (
+    <>
+      <Reset />
+      <ModalStyleOverride theme={theme} locale={locale} />
+      <GlobalStyles theme={theme} locale={locale}>
+        {children}
+      </GlobalStyles>
+    </>
+  );
+}
