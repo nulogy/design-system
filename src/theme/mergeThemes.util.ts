ffe95a722b22224c31e5d9e32de8ca03a238e77c
Haider Alshamma <haidera@nulogy.com>
2024-11-12 15:42:19 -0500

    feat: add futureFlags

---
+++ src/theme/mergeThemes.util.ts
@@ -0,0 +1,15 @@
+import { DefaultNDSThemeType, ThemeType } from "./theme.type";
+
+export const mergeThemes = (theme: DefaultNDSThemeType, customTheme: ThemeType): DefaultNDSThemeType =>
+  Object.keys(theme).reduce((mergedTheme, group) => {
+    if (customTheme && mergedTheme[group] && customTheme[group]) {
+      return {
+        ...mergedTheme,
+        [group]: {
+          ...mergedTheme[group],
+          ...customTheme[group],
+        },
+      };
+    }
+    return mergedTheme;
+  }, theme);
