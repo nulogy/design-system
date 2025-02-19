4f0c03b68f1449cea204b99edbd66738ffa90192
Haider Alshamma <haidera@nulogy.com>
2025-02-19 16:54:26 -0500

    refactor: move files and components around

---
+++ src/DescriptionList/index.ts
@@ -0,0 +1,5 @@
+export type { DescriptionListProps } from "./lib/types";
+export { default as DescriptionList } from "./DescriptionList";
+export { DescriptionGroup } from "./DescriptionList.parts";
+export { DescriptionTerm } from "./DescriptionList.parts";
+export { DescriptionDetails } from "./DescriptionList.parts";
