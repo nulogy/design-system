ac4fe0100c8d1627b527ae7268f7c47cb350ecef
Haider Alshamma <haidera@nulogy.com>
2025-04-01 15:15:25 -0400

    restore code from branch

---
+++ src/Navigation/icons/Customlogo.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function CustomLogo() {
+  return (
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_5422_14897)">
+        <path d="M26.7278 39.3779H35.9574L13.0939 0.160156H3.86426L26.7278 39.3779Z" fill="#7476ED" />
+        <path
+          d="M17.835 0.160158L39.875 37.9653V0.160156H33.9106V12.096L26.9521 0.160156L17.835 0.160158Z"
+          fill="#E56F8C"
+        />
+        <path d="M0 1.47314V39.3778H5.96441V27.442L12.9228 39.3778H22.0981L0 1.47314Z" fill="#64C2DB" />
+      </g>
+      <defs>
+        <clipPath id="clip0_5422_14897">
+          <rect width="40" height="40" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
