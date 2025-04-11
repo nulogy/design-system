e8b79d3cb41c2d8e3c54a8906b205168041180d5
Nikola Pejcic <nikolap@nulogy.com>
2025-04-11 13:00:35 -0400

    Fixes the line height of requirement text and hint icon size

--- src/FieldLabel/RequirementText.tsx
+++ src/FieldLabel/RequirementText.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Text } from "../Type";
 import { TextProps } from "../Type/Text";
 
-const RequirementText = (props: TextProps) => <Text inline ml="x1" fontSize="smaller" color="darkGrey" {...props} />;
+const RequirementText = (props: TextProps) => (
+  <Text inline ml="half" fontSize="smaller" lineHeight="smallerText" color="darkGrey" {...props} />
+);
 
 export default RequirementText;
