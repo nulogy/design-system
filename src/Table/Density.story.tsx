e24a8fc2cb931a6a6b48fb7c1fe3f27f7e2b271e
Nikola Pejcic <nikolap@nulogy.com>
2025-04-11 14:35:06 -0400

    Adds density stories

---
+++ src/Table/Density.story.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Columns } from "./Table.types";
+import { Table } from ".";
+
+const columns: Columns<{}> = [
+  { label: "Date", dataKey: "date" },
+  { label: "Expected Quantity", dataKey: "expectedQuantity" },
+  { label: "Actual Quantity", dataKey: "actualQuantity", align: "right" },
+];
+
+const rowData = [
+  {
+    date: "2019-10-01",
+    expectedQuantity: "2,025 eaches",
+    actualQuantity: "1,800 eaches",
+    id: "r1",
+  },
+  {
+    date: "2019-10-02",
+    expectedQuantity: "2,475 eaches",
+    actualQuantity: "2,250 eaches",
+    id: "r2",
+  },
+  {
+    date: "2019-10-03",
+    expectedQuantity: "2,475 eaches",
+    actualQuantity: "1,425 eaches",
+    id: "r3",
+  },
+  {
+    date: "2019-10-04",
+    expectedQuantity: "2,475 eaches",
+    actualQuantity: "675 eaches",
+    id: "r4",
+  },
+  {
+    date: "2019-10-07",
+    expectedQuantity: "2,475 eaches",
+    actualQuantity: "1,575 eaches",
+    id: "r5",
+  },
+  {
+    date: "2019-10-22",
+    expectedQuantity: "1,725 eaches",
+    actualQuantity: "-",
+    id: "r7",
+  },
+  {
+    date: "2019-10-23",
+    expectedQuantity: "2,475 eaches",
+    actualQuantity: "-",
+    id: "r8",
+  },
+  {
+    date: "2019-10-24",
+    expectedQuantity: "2,475 eaches",
+    actualQuantity: "-",
+    id: "r9",
+  },
+];
+
+export default {
+  title: "Components/Table/Density"
+};
+
+export const Default = () => (
+  <Table
+    columns={columns}
+    rows={rowData}
+  />
+);
+
+Default.story = {
+    name: "Default",
+  };
+
+  export const Compact = () => (
+    <Table
+      columns={columns}
+      rows={rowData}
+      compact
+    />
+  );
+  
+  Compact.story = {
+      name: "Compact",
+    };
+
+
