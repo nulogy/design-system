458897c81c7bd295655bf82964168ee0ef955e3b
Haider Alshamma <haidera@nulogy.com>
2025-05-07 17:41:07 -0400

    feat: add AppSwitcher story

---
+++ src/Navigation/stories/Navigation.appSwitcher.story.tsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import Navigation from "../Navigation";
+import { ApplicationFrame, Page } from "../../Layout";
+import { Checkbox } from "../../Checkbox";
+import { Text } from "../../Type";
+import { StatusIndicator } from "../../StatusIndicator";
+import { InlineFlex } from "../../Flex";
+import { Icon } from "../../Icon";
+import { Code } from "../../utils/story/code";
+
+export default {
+  title: "Components/Navigation/App Switcher",
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export const AllApps = () => {
+  return (
+    <Navigation
+      appSwitcher={{
+        apps: {
+          "production-scheduling": {
+            url: "https://nulogy.com/",
+          },
+          "supplier-collaboration": {
+            url: "https://nulogy.com/",
+          },
+          "digital-quality-inspection": {
+            url: "https://nulogy.com/",
+          },
+          "shop-floor": {
+            url: "https://nulogy.com/",
+          },
+          "smart-factory": {
+            url: "https://nulogy.com/",
+          },
+          connections: {
+            url: "https://nulogy.com/",
+          },
+        },
+      }}
+    />
+  );
+};
+
+export const OnlySelectApps = () => {
+  return (
+    <Navigation
+      appSwitcher={{
+        apps: {
+          "production-scheduling": {
+            url: "https://nulogy.com/",
+          },
+          "digital-quality-inspection": {
+            url: "https://nulogy.com/",
+          },
+          "shop-floor": {
+            url: "https://nulogy.com/",
+          },
+        },
+      }}
+    />
+  );
+};
+
+export const WithConditionallyVisibleApps = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  return (
+    <ApplicationFrame
+      navBar={
+        <Navigation
+          appSwitcher={{
+            apps: {
+              "production-scheduling": {
+                url: "https://nulogy.com/",
+              },
+              "digital-quality-inspection": {
+                url: "https://nulogy.com/",
+              },
+              "shop-floor": {
+                url: "https://nulogy.com/",
+              },
+              "smart-factory": {
+                url: "https://nulogy.com/",
+                visible: isAdmin,
+              },
+            },
+          }}
+        />
+      }
+    >
+      <Page fullHeight>
+        <Checkbox labelText="User is admin" checked={isAdmin} onChange={() => setIsAdmin(!isAdmin)} />
+        <Text fontSize="small" color="darkGrey">
+          In this example, the Smart Factory app is only visible to <strong>admins</strong>. The app switcher is
+          conditionally rendered based on the user&apos;s role.
+        </Text>
+      </Page>
+    </ApplicationFrame>
+  );
+};
+
+export const WithAnIndicator = () => {
+  return (
+    <ApplicationFrame
+      navBar={
+        <Navigation
+          appSwitcher={{
+            apps: {
+              "production-scheduling": {
+                url: "https://nulogy.com/",
+              },
+              "smart-factory": {
+                url: "https://nulogy.com/",
+                indicator: <StatusIndicator type="informative">new</StatusIndicator>,
+              },
+              "shop-floor": {
+                url: "https://nulogy.com/",
+                indicator: (
+                  <InlineFlex alignItems="center" gap="x0_25" bg="lightRed" pl="x0_5" pr="x1" borderRadius="rounded">
+                    <Icon icon="error" size="x2" color="red" />
+                    <Text
+                      fontSize="smaller"
+                      letterSpacing="0.05em"
+                      textTransform="uppercase"
+                      fontWeight="medium"
+                      color="red"
+                    >
+                      Not available
+                    </Text>
+                  </InlineFlex>
+                ),
+              },
+            },
+          }}
+        />
+      }
+    >
+      <Page fullHeight>
+        <Text fontSize="small" color="darkGrey">
+          In this example, the Smart Factory app is using a <Code>StatusIndicator</Code>, The Shop Floor app is using a
+          custom one.
+        </Text>
+      </Page>
+    </ApplicationFrame>
+  );
+};
