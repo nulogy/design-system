6302d265dd3092a9f184d2d9e475e891c4e5a545
Haider Alshamma <haidera@nulogy.com>
2025-05-23 14:35:24 -0400

    feat: export the Navigation component and its props

--- src/Navigation/index.ts
+++ src/Navigation/index.ts
@@ -1,1 +1,4 @@
 export { default as Navigation } from "./Navigation";
+export type { NavigationProps } from "./Navigation";
+export type { MenuItems, MenuItem, UserMenu, UserMenuItem, UserMenuInfo } from "./types";
+export type { AppSwitcherConfig } from "./components/AppSwitcher/NulogyAppSwitcher";
