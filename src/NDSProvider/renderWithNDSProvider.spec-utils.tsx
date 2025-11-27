67da608b578d64d694c595a937b1cce5e755a616
James Herdman <jamesherdman@nulogy.com>
2025-11-27 11:36:12 -0500

    fix: Add missing type annotation

--- src/NDSProvider/renderWithNDSProvider.spec-utils.tsx
+++ src/NDSProvider/renderWithNDSProvider.spec-utils.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { type ReactNode } from "react";
 import { render } from "@testing-library/react";
 import NDSProvider from "./NDSProvider";
 
-export const renderWithNDSProvider = (component, locale = "en_US") =>
+export const renderWithNDSProvider = (component: ReactNode, locale = "en_US") =>
   render(<NDSProvider locale={locale}>{component}</NDSProvider>);
