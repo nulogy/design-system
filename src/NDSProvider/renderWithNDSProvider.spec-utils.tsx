d1d87c2344e599038be4042cfb19c00908754708
James Herdman <jamesherdman@nulogy.com>
2025-12-01 14:40:38 -0500

    task: Use pnpm instead of Yarn

--- src/NDSProvider/renderWithNDSProvider.spec-utils.tsx
+++ src/NDSProvider/renderWithNDSProvider.spec-utils.tsx
@@ -1,6 +1,7 @@
 import React, { type ReactNode } from "react";
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import NDSProvider from "./NDSProvider";
 
-export const renderWithNDSProvider = (component: ReactNode, locale = "en_US") =>
-  render(<NDSProvider locale={locale}>{component}</NDSProvider>);
+export const renderWithNDSProvider = (component: ReactNode, locale = "en_US"): RenderResult => {
+  return render(<NDSProvider locale={locale}>{component}</NDSProvider>);
+}
